a2087c3ee086581616edddc81c30216aa076021a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:57 +0000

    Add package jfjson/0.0.2/jfjson-0.0.2.tar.gz

---
+++ packages/jfjson/jfjson-0.0.2.tar.gz/jfjson-0.0.2/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jfjson",
+    version="0.0.2",
+    author="Tobin Yehle",
+    author_email="tobin@yehle.us",
+    description="Automatic json parsing and serialing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tyehle/jfjson",
+    packages=["jfjson"],
+    package_dir={"": "src"},
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+
+        "Operating System :: OS Independent",
+
+        "Topic :: Software Development",
+
+        "Intended Audience :: Developers",
+
+        "Typing :: Typed",
+    ],
+)
