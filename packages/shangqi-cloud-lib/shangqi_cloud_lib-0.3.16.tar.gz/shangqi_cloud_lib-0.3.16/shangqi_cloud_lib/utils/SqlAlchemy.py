7fcc1c7a2405b3ea5df6fb9c8c33895906b8663d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:47 +0000

    Add package shangqi-cloud-lib/0.3.16/shangqi_cloud_lib-0.3.16.tar.gz

---
+++ packages/shangqi-cloud-lib/shangqi_cloud_lib-0.3.16.tar.gz/shangqi_cloud_lib-0.3.16/shangqi_cloud_lib/utils/SqlAlchemy.py
@@ -0,0 +1,26 @@
+def alchemy_default_to_dict(params, data):
+    data_list = []
+    key_list = []
+    for arg in params:
+        if "(" in arg and ")" in arg:
+            key_list.append(str(arg).split(".")[-1][:-1])
+        else:
+            key_list.append(str(arg).split(".")[-1])
+    if isinstance(data, list):
+        for d in data:
+            dict_data = dict(zip(key_list, d))
+            data_list.append(dict_data)
+        return data_list
+    else:
+        if data:
+            return dict(zip(key_list, data))
+        else:
+            return {}
+
+
+def sqlalchemy_paging(Query, limit_number, offset_number):
+    data_list = Query.limit(limit_number).offset(offset_number).all()
+    data_count = 0
+    if offset_number == 0:
+        data_count = Query.count()
+    return {"count": data_count, "dataSource": data_list}
