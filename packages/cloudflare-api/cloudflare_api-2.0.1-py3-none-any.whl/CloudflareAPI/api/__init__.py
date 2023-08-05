e3c8a5da2654b5f8ca94edfad67e44f8899a7795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:32 +0000

    Add package cloudflare-api/2.0.1/cloudflare_api-2.0.1-py3-none-any.whl

---
+++ packages/cloudflare-api/cloudflare_api-2.0.1-py3-none-any.whl/CloudflareAPI/api/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from CloudflareAPI.api.user import User
+from CloudflareAPI.api.worker import Worker
+from CloudflareAPI.api.storage import Storage
+from CloudflareAPI.api.account import Account
---
+++ packages/cloudflare-api/cloudflare_api-2.0.1-py3-none-any.whl/CloudflareAPI/core/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from CloudflareAPI.core.base import CFBase
+from CloudflareAPI.core.network import Request
+from CloudflareAPI.core.configuration import config
