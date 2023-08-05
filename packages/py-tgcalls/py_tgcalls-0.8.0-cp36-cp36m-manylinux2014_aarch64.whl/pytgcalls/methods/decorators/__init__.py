80cc18aeccd34c01b3d22295f66c12bfaee67a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:38 +0000

    Add package py-tgcalls/0.8.0/py_tgcalls-0.8.0-cp36-cp36m-manylinux2014_aarch64.whl

---
+++ packages/py-tgcalls/py_tgcalls-0.8.0-cp36-cp36m-manylinux2014_aarch64.whl/pytgcalls/methods/decorators/__init__.py
@@ -0,0 +1,17 @@
+from .on_closed_voice_chat import OnClosedVoiceChat
+from .on_group_call_invite import OnGroupCallInvite
+from .on_kicked import OnKicked
+from .on_left import OnLeft
+from .on_raw_update import OnRawUpdate
+from .on_stream_end import OnStreamEnd
+
+
+class Decorators(
+    OnClosedVoiceChat,
+    OnGroupCallInvite,
+    OnKicked,
+    OnLeft,
+    OnRawUpdate,
+    OnStreamEnd,
+):
+    pass
