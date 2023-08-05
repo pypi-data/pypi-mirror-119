195b42a54608b5493e235bce55b437a130dd1665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:50 +0000

    Add package conga-office/0.2.0/conga_office-0.2.0-py3-none-any.whl

---
+++ packages/conga-office/conga_office-0.2.0-py3-none-any.whl/conga_office/config/metadata.py
@@ -0,0 +1,10 @@
+
+valid_speed_modes =\
+    {
+        "normal": 1.0,
+        "slow": 2.0,
+        "turbo": 0.1
+    }
+teams_search_key = 'teams'
+bs_generator_api = 'https://corporatebs-generator.sameerkumar.website/'
+waiting_intervals = 2
