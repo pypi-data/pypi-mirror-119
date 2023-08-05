40841b046fef7f69bb6ef414ad872894976c94a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package wikiscraper/1.0.0/wikiscraper-1.0.0.tar.gz

---
+++ packages/wikiscraper/wikiscraper-1.0.0.tar.gz/wikiscraper-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Easy scraper that extracts data from Wikipedia articles thanks to its URL slug'
+long_description = (Path(__file__).parent / "README.md").read_text()
+
+# Setting up
+setup(
+    name="wikiscraper",
+    version=VERSION,
+    author="Alexandre Meyer",
+    author_email="contact@alexandremeyer.fr",
+    description=DESCRIPTION,
+    url='https://github.com/Alexandre333/easy_wiki',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['requests', 'beautifulsoup4'],
+    keywords=['python', 'web scraping', 'wikipedia', 'slug'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
