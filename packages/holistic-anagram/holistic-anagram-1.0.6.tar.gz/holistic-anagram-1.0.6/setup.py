3bf0f2fc7fa3503d1d60d2003d182f53a4769e28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:22 +0000

    Add package holistic-anagram/1.0.6/holistic-anagram-1.0.6.tar.gz

---
+++ packages/holistic-anagram/holistic-anagram-1.0.6.tar.gz/holistic-anagram-1.0.6/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup
+
+def readme():
+    
+    with open("README.md") as f:
+        README = f.read()
+        
+    return README
+
+
+setup(
+    
+    name = "holistic-anagram",
+    version="1.0.6",
+    description = "A python package for anagrams",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/MichaelQuant4R/holistic-anagram",
+    author="Michael S. Russell",
+    author_email="michael.r2014@yahoo.co.uk",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9"
+    ],
+    package_data = {"holistic_anagram":["english.txt"]},
+    packages = ["holistic_anagram"],
+    package_dir ={"holistic_anagram":"holistic_anagram"},
+    include_package_data = True,
+    install_requires=[]
+
+)
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
