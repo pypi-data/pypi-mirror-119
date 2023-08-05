22a5ec9e31cda1d3e3aa54b9e2955b777c4d1890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:33 +0000

    Add package cloudflare-api/2.0.2/cloudflare-api-2.0.2.tar.gz

---
+++ packages/cloudflare-api/cloudflare-api-2.0.2.tar.gz/cloudflare-api-2.0.2/CloudflareAPI/utils.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import time
+import requests
+from typing import Any, Optional
+from CloudflareAPI.exceptions import APIError
+
+
+class Fetch:
+    def __init__(self, url: str) -> None:
+        self.base_url = url
+
+    def __call__(self, endpoint: str) -> Optional[str]:
+        url = self.base_url + endpoint
+        print("\rFetching URL:", url, end="")
+        response = requests.get(url, timeout=4)
+        if not response.ok:
+            print("Waiting for response...", end="")
+            while not response.ok:
+                response = requests.get(url, timeout=4)
+                time.sleep(1)
+        print(" [OK]")
+        return response.text or None
+
+
+def wait_result(func, *args, **kargs) -> Any:
+    while True:
+        try:
+            result = func(*args, **kargs)
+            return result
+        except APIError:
+            print("\rWaiting for result...", end="")
+            time.sleep(0.1)
+            continue
