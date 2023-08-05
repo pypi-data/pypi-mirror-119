f207e1fd8d0a8037aa2542a90191201668d91c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:07 +0000

    Add package kmm/0.0.1/kmm-0.0.1-py3-none-any.whl

---
+++ packages/kmm/kmm-0.0.1-py3-none-any.whl/kmm/__init__.py
@@ -0,0 +1,1 @@
+from kmm.read import read
---
+++ packages/kmm/kmm-0.0.1-py3-none-any.whl/kmm/kmm.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+
+def kmm(path: Path):
+    return pd.read_csv(
+        path,
+        sep="\t",
+        encoding="latin1",
+        names=[
+            "track_section",
+            "kilometer",
+            "meter",
+            "track_lane",
+            "1?",
+            "2?",
+            "3?",
+            "4?",
+            "5?",
+            "sweref99_tm_x",
+            "sweref99_tm_y",
+            "8?",
+            "9?"
+        ],
+        dtype=dict(
+            track_section=str,
+            kilometer=np.int32,
+            meter=np.int32,
+            track_lane=str,
+            sweref99_tm_x=np.float32,
+            sweref99_tm_y=np.float32,
+        ),
+    )
