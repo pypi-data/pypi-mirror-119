140027c53d50448073bae51f8c29b71fb236ed63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:57 +0000

    Add package giving/0.1.3/giving-0.1.3.tar.gz

---
+++ packages/giving/giving-0.1.3.tar.gz/giving-0.1.3/giving/__init__.py
@@ -0,0 +1,14 @@
+from . import operators as op
+from .core import (
+    Given,
+    Giver,
+    accumulate,
+    give,
+    given,
+    giver,
+    make_give,
+    register_special,
+    resolve,
+)
+from .obs import ObservableProxy, prox
+from .version import version
