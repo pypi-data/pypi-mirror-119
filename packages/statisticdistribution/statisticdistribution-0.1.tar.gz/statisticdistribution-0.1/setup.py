be5b67e7f2cac2bf72b5cab480e38df880fddcac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:42 +0000

    Add package statisticdistribution/0.1/statisticdistribution-0.1.tar.gz

---
+++ packages/statisticdistribution/statisticdistribution-0.1.tar.gz/statisticdistribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='statisticdistribution',
+      author= 'Luter .C. Aondona',
+      email = 'luteraondona@gmail.com',
+      version='0.1',
+      description='Gaussian and binomial distributions',
+      packages=['statisticdistribution'],
+      zip_safe=False)
