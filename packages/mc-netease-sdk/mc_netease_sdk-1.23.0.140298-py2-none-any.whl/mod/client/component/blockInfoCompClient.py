da6115d6340521d3541f62f92fd4a62b84c08d56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:00 +0000

    Add package mc-netease-sdk/1.23.0.140298/mc_netease_sdk-1.23.0.140298-py2-none-any.whl

---
+++ packages/mc-netease-sdk/mc_netease_sdk-1.23.0.140298-py2-none-any.whl/mod/client/component/blockInfoCompClient.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from typing import Union
+from mod.common.component.baseComponent import BaseComponent
+from typing import Tuple
+
+class BlockInfoComponentClient(BaseComponent):
+    def GetBlock(self, pos):
+        # type: (Tuple[float,float,float]) -> Tuple[str,int]
+        """
+        获取某一位置的block
+        """
+        pass
+
+    def GetTopBlockHeight(self, pos):
+        # type: (Tuple[int,int]) -> Union[int,None]
+        """
+        获取当前维度某一位置最高的非空气方块的高度
+        """
+        pass
+
+    def ChangeBlockTextures(self, blockName, tileName, texturePath):
+        # type: (str, str, str) -> bool
+        """
+        替换方块贴图
+        """
+        pass
+
+    def GetDestroyTotalTime(self, blockName, itemName=None):
+        # type: (str, str) -> float
+        """
+        获取使用物品破坏方块需要的时间
+        """
+        pass
+
