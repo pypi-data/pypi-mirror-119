a62fc13642fdc4fd2e1bb71d96de57964bae41c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:31 +0000

    Add package microservice-template-core/2.2.27/microservice-template-core-2.2.27.tar.gz

---
+++ packages/microservice-template-core/microservice-template-core-2.2.27.tar.gz/microservice-template-core-2.2.27/microservice_template_core/tools/kafka_confluent_consumer.py
@@ -0,0 +1,29 @@
+from microservice_template_core.tools.logger import get_logger
+from microservice_template_core.settings import ServiceConfig, KafkaConfig
+from confluent_kafka import Consumer
+
+logger = get_logger()
+
+
+class KafkaConsumeMessages(object):
+    def __init__(self, kafka_topic):
+        self.kafka_topic = kafka_topic
+
+    def start_consumer(self):
+        """
+        Start consumer
+        """
+
+        logger.debug(f"Initializing Kafka Confluent Consumer - {ServiceConfig.SERVICE_NAME}. Topic - {self.kafka_topic}")
+
+        consumer = Consumer(
+            {
+                'bootstrap.servers': KafkaConfig.KAFKA_SERVERS,
+                'group.id': ServiceConfig.SERVICE_NAME,
+                'auto.offset.reset': 'latest',
+            }
+        )
+
+        consumer.subscribe([self.kafka_topic])
+
+        return consumer
