6c63a4a51ce12931ffb83fd34e382572c1e92d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package musictune/0.0.5/musictune-0.0.5.tar.gz

---
+++ packages/musictune/musictune-0.0.5.tar.gz/musictune-0.0.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="musictune",
+    version="0.0.5",
+    author="vibujithan",
+    author_email="author@example.com",
+    description="Parallel image processing package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vibujithan/musictune",
+    project_urls={
+        "Bug Tracker": "https://github.com/vibujithan/musictune/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    package_data={
+        "musictune": ["data/**", "data/PSF/**"],
+        "": ["examples/**"]
+    },
+    scripts=['bin/pzf2zarr.py', 'bin/start_cluster.py',
+             'bin/temp.py'
+             ]
+)
