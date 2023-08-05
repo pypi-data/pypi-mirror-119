77a4ee0816daafa87427c85db38de04e964f2902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:20 +0000

    Add package shareberry/0.0.6/shareberry-0.0.6-py2.py3-none-any.whl

---
+++ packages/shareberry/shareberry-0.0.6-py2.py3-none-any.whl/hivebase/chains.py
@@ -0,0 +1,18 @@
+default_prefix = "SHARE"
+
+known_chains = {
+    "Shareberry-Testnet": {
+        "chain_id": "a3ee3a34dd82dcd8e4f9ed14202c6d35734ea4c8d9c0e9daee48ec859d135b6c",
+        "prefix": "SHARE",
+        "steem_symbol": "SHTO",
+        "sbd_symbol": "UNDEFINED",
+        "vests_symbol": "SHPO",
+    },
+    "Shareberry": {
+        "chain_id": "c1f3fe6f266b42af3a15b4dc8709075759c07c6831cece731ddd6559fcd8b98b",
+        "prefix": "SHARE",
+        "steem_symbol": "SHTO",
+        "sbd_symbol": "UNDEFINED",
+        "vests_symbol": "SHPO",
+    },
+}
