aa4e9039c46a73734249059deb7d0be796f36062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:37 +0000

    Add package botd/61/botd-61.tar.gz

---
+++ packages/botd/botd-61.tar.gz/botd-61/setup.py
@@ -0,0 +1,46 @@
+# This file is placed in the Public Domain.
+
+from setuptools import setup
+
+def read():
+    return open("README.rst", "r").read()
+
+setup(
+    name="botd",
+    version="61",
+    url="https://github.com/bthate/botd",
+    author="Bart Thate",
+    author_email="bthate67@gmail.com",
+    description="24/7 channel daemon",
+    long_description=read(),
+    license="Public Domain",
+    packages=["bot"],
+    include_package_data=True,
+    data_files=[('share/doc/botd', [
+                                    'docs/botdline.png',
+                                    'docs/botdsmile.png',
+                                    'docs/botdscreen.png',
+                                    'docs/conf.py',
+                                    'docs/index.rst',
+                                    'docs/aasource.rst',
+                                    'docs/_templates/base.rst',
+                                    'docs/_templates/class.rst',
+                                    'docs/_templates/layout.html',
+                                    'docs/_templates/module.rst',
+                                    'files/bot.1.md',
+                                    'files/botd.8.md'
+                                   ]),
+                ("etc/rc.d/", ["files/botd"]),
+                ("share/doc/botd/", ["files/botd.8.md"]),
+                ("lib/systemd/system", ["files/botd.service"]),
+                ("man/man1", ["files/bot.1.gz"]),
+                ("man/man8", ["files/botctl.8.gz", "files/botd.8.gz"])],
+    scripts=["bin/bot", "bin/botctl", "bin/botd"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: Public Domain",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ]
+)
