b54735f3361310e32d0510bfea265dcc966013f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:32 +0000

    Add package zen-publish/0.0.21/zen-publish-0.0.21.tar.gz

---
+++ packages/zen-publish/zen-publish-0.0.21.tar.gz/zen-publish-0.0.21/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+def read_text(file_name):
+    return open(file_name).read()
+
+setup(
+    name="zen-publish",  # Replace with your own username
+    version="0.0.21",
+    author="Dr. P. B. Patel",
+    author_email="customer.support@zenreportz.atlassian.net",
+    description="Package for Zen Reports",
+    long_description= read_text("README.md"),
+    long_description_content_type="text/markdown",
+    url="",
+   packages=['zen_publish', "zen_publish.core", "zen_publish.support"],  #same as name
+    # packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'click', 'oyaml', 'requests', 
+    ],
+    python_requires='>=3',
+    dependency_links=['https://github.com/Zen-Reportz/ZenPublish/tree/master/python-package'],
+    license='Apache License',
+    entry_points={
+        "console_scripts": [
+            "zen=zen_publish:cli"
+        ]
+    },
+)
