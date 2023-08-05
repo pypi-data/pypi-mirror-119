b01e009565c40c8e1151e3abdb562138a0b0bddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:44 +0000

    Add package ryroraf-distributions/0.1/ryroraf_distributions-0.1.tar.gz

---
+++ packages/ryroraf-distributions/ryroraf_distributions-0.1.tar.gz/ryroraf_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ryroraf_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ryroraf_distributions'],
+      zip_safe=False)
