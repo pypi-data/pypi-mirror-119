99171daea12a4d6a43c961814e506d77871632cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:12 +0000

    Add package text-explainability/0.3.7/text_explainability-0.3.7-py3-none-any.whl

---
+++ packages/text-explainability/text_explainability-0.3.7-py3-none-any.whl/text_explainability/__init__.py
@@ -0,0 +1,3 @@
+from text_explainability.global_explanation import TokenFrequency, TokenInformation
+from text_explainability.local_explanation import LIME, KernelSHAP, Anchor, LocalTree
+from text_explainability.utils import default_tokenizer, default_detokenizer
