75837875656f2aebf284eb58cf94d71f2d22fec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:03 +0000

    Add package cqTools/0.0.6/cqTools-0.0.6-py3-none-any.whl

---
+++ packages/cqTools/cqTools-0.0.6-py3-none-any.whl/cqTools/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+__all__ = ["utils"]
+from cqTools.StudentSearch.utils.login import ReportLogin
+
+class studentSearch(ReportLogin):
+    def __init__(self) -> None:
+        """查找学生信息
+        """
+        super(studentSearch, self).__init__()
+
+    def login(self, cardNo:str, password:str) -> dict:
+        """获取登录token
+        """
+        return super().login(cardNo=cardNo, password=password)
+
+    def resetPassword(self, cardNo:str) -> bool:
+        """重置密码
+        """
+        return super().resetPassword(cardNo=cardNo)
+
+    def setPassword(self, cardNo:str, password:str, newPassword:str) -> bool:
+        """修改密码
+        """
+        return super().setPassword(cardNo=cardNo, password=password, newPassword=newPassword)
