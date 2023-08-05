8984d4f13daa16f04e2649a98b7a7822f0bcb818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:40 +0000

    Add package cicdtest/0.84/cicdtest-0.84-py3-none-any.whl

---
+++ packages/cicdtest/cicdtest-0.84-py3-none-any.whl/buildpan/setting.py
@@ -0,0 +1,9 @@
+info = {
+    "HOST" : "35.225.89.124",
+    "PUSH_COMMIT_URL" : "http://35.225.89.124/push_commit",
+    "FETCH_LOG_URL" : "http://35.225.89.124/fetch_log",
+    "TIME" : "45",
+    "key" : "HB2xRCZSyKYMcTS9rm",
+    "secret_key" : "pQA5FMShe6deqr2b8Eq3GfDBmhEnn77C",
+    "version": "0.84"
+}
