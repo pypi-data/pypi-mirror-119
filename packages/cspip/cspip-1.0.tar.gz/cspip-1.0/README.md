34181dd2db3c4b3b59d288d52f977ff7f396e461
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:52 +0000

    Add package cspip/1.0/cspip-1.0.tar.gz

---
+++ packages/cspip/cspip-1.0.tar.gz/cspip-1.0/README.md
@@ -0,0 +1,5 @@
+### 测试-cspip
+
+#### 安装
+
+> pip install cspip
---
+++ packages/cspip/cspip-1.0.tar.gz/cspip-1.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   setup.py
+@Time    :   2021/09/08 10:51:41
+@Author  :   jixn
+@Version :   1.0
+'''
+
+
+import setuptools
+ 
+ 
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+ 
+ 
+setuptools.setup(
+    name='cspip',  # 模块名称
+    version="1.0",  # 当前版本
+    author="jixn",  # 作者
+    author_email="jixn@foxmail.com",  # 作者邮箱
+    description="这只是一个测试",  # 模块简介
+    long_description=long_description,  # 模块详细介绍
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="https://github.com/zhangyafeii/timer",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=["requests"],
+    python_requires='>=3.7',
+)
