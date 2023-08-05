19446c17fd6365659fa5b83a622fd5e954bdeb1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:03 +0000

    Add package chemical-QBD/0.0.32/chemical_QBD-0.0.32-py3-none-any.whl

---
+++ packages/chemical-QBD/chemical_QBD-0.0.32-py3-none-any.whl/chemical_QBD/const.py
@@ -0,0 +1,20 @@
+"""
+https://pubchem.ncbi.nlm.nih.gov/periodic-table/#view=list
+"""
+import json
+# import os
+  
+# path = os.path.join(os.path.curdir, 'elementary__data.json')
+file = open('./elementary__data.json')
+data = json.load(file)
+file.close()
+
+CHEMICAL__ELEMENTS = [element['Cell'][1] for element in data['Table']['Row']]
+CHEMICAL__ELEMENTS__1 = [i for i in CHEMICAL__ELEMENTS if len(i) == 1]
+CHEMICAL__ELEMENTS__2 = [i for i in CHEMICAL__ELEMENTS if len(i) == 2]
+
+CHEMICAL__ELEMENTS__CONFIGURATION = {}
+for element in data['Table']['Row']:
+    CHEMICAL__ELEMENTS__CONFIGURATION[element['Cell'][1]] = element['Cell'][5]
+
+
