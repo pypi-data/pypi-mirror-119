63b62de9cff4c15cca0ebfd8f94e60a56f1f8427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:36 +0000

    Add package gppt/1.3/gppt-1.3-py3-none-any.whl

---
+++ packages/gppt/gppt-1.3-py3-none-any.whl/gppt/__init__.py
@@ -0,0 +1,4 @@
+from .auth import PixivAuth  # noqa: F401
+from .selenium import (AUTH_TOKEN_URL, CALLBACK_URI, CLIENT_ID,  # noqa: F401
+                       CLIENT_SECRET, LOGIN_URL, REDIRECT_URI, REQUESTS_KWARGS,
+                       USER_AGENT, GetPixivToken)
