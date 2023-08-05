eaf06a84aeecfd779760a2d18cacffd1b4dc38b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:55 +0000

    Add package georgea-distributions/0.1/georgea_distributions-0.1.tar.gz

---
+++ packages/georgea-distributions/georgea_distributions-0.1.tar.gz/georgea_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='georgea_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['georgea_distributions'],
+      zip_safe=False)
