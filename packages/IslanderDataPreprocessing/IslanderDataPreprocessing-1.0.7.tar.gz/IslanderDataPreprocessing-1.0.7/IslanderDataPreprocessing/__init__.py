e8cfeecc00b3bb79459c6ddd10bb451d608762c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:09 +0000

    Add package IslanderDataPreprocessing/1.0.7/IslanderDataPreprocessing-1.0.7.tar.gz

---
+++ packages/IslanderDataPreprocessing/IslanderDataPreprocessing-1.0.7.tar.gz/IslanderDataPreprocessing-1.0.7/IslanderDataPreprocessing/__init__.py
@@ -0,0 +1,6 @@
+from .database import DataBase
+from .datadiscovery import DataDiscovery
+from .DataVisulization import DataVisulization
+from .datacorrelation import DataCorrelation
+from .encoder2 import Encoder
+from .missingvalues import MissingValues
