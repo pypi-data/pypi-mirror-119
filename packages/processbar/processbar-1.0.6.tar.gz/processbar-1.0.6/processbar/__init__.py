fe188cb11884969d28c9f6ba04b8983d4fd1921c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:35 +0000

    Add package processbar/1.0.6/processbar-1.0.6.tar.gz

---
+++ packages/processbar/processbar-1.0.6.tar.gz/processbar-1.0.6/processbar/__init__.py
@@ -0,0 +1,11 @@
+# Author     : lin le
+# CreateTime : 2021/8/31 16:20
+# Email      : linle861021@163.com
+
+import sys
+
+__version__ = '1.0.3'
+if sys.version < '3.6':
+    raise RuntimeError('python version required 3.6+')
+
+from .simpleprocessbar import SimpleProcessbar
