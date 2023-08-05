abdd316b8b4784f0bbc179d887aa2a9bdbc6d494
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package signal-envelope/1.3/signal-envelope-1.3.tar.gz

---
+++ packages/signal-envelope/signal-envelope-1.3.tar.gz/signal-envelope-1.3/README.md
@@ -0,0 +1,24 @@
+[![DOI](https://zenodo.org/badge/297107471.svg)](https://zenodo.org/badge/latestdoi/297107471)
+
+# Quickstart
+Module to extract the envelope of signals.
+
+Interactive visualizations [here](https://envelope.netlify.app/).
+
+An exe for Win64 machines can be found in the releases section. For usage info run the exe with the -h flag.
+A Python module implementing the same functionality can be found [here](https://pypi.org/project/signal-envelope/).
+A number of test wav files can be found at the /test_samples folder.
+
+
+# Documentation
+
+## [C++](https://tesserato.github.io/envelope/html/index.html)
+
+## [Python](https://pypi.org/project/signal-envelope/)
+
+
+# Used Libraries
+
+[Libsndfile](http://www.mega-nerd.com/libsndfile/)
+[Boost](https://www.boost.org/)
+[Intel Math Kernel Library](https://software.intel.com/content/www/us/en/develop/tools/oneapi/components/onemkl.html#gs.9syxj0)
---
+++ packages/signal-envelope/signal-envelope-1.3.tar.gz/signal-envelope-1.3/setup.py
@@ -0,0 +1,36 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README_pypi.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="signal-envelope",
+    version="1.3",
+    description="Extracts the temporal envelope of a digital signal",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tesserato/envelope",
+    author="Carlos Tarjano",
+    author_email="tesserato@hotmail.com",
+    license="MIT", # <| <| <| <|
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=["signal_envelope"],
+    include_package_data=True,
+    package_data={'signal-envelope': ['signal_envelope/*.dll']},
+    # package_data={"dlls": ["..\signal_envelope\DLL.dll", "..\signal_envelope\libsndfile-1.dll"]},
+    # data_files=[("", "\DLL.dll"), ("","\libsndfile-1.dll")],
+    install_requires=["numpy"],
+    # entry_points={
+    #     "console_scripts": [
+    #         "realpython=reader.__main__:main",
+    #     ]
+    # },
+)
