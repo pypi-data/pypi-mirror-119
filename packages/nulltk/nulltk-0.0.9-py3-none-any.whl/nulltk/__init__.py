eb9f60ffe9464a1547d0267f5a26db4b1eded5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:45 +0000

    Add package nulltk/0.0.9/nulltk-0.0.9-py3-none-any.whl

---
+++ packages/nulltk/nulltk-0.0.9-py3-none-any.whl/nulltk/__init__.py
@@ -0,0 +1,21 @@
+# include default tkinter symbols
+from tkinter import *
+
+# widgets
+from .tkroot import Tk
+from .floatingwindow import FloatingWindow
+from .graph import Graph
+from .image import Image
+from .tabbedframe import TabbedFrame
+from .overlaywindow import OverlayWindow
+from .input import Slider, Dial
+from .timeplot import TimePlot
+from .text import Text
+from .radialgauge import RadialGauge
+
+# appliers
+from .mixins import Reactive
+from .style import Style
+
+# misc
+from .color import Color
