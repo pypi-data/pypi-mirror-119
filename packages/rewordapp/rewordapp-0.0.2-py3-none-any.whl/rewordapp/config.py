19395dfac46ba8a554e0b1e54a78bea07c98bb9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:05 +0000

    Add package rewordapp/0.0.2/rewordapp-0.0.2-py3-none-any.whl

---
+++ packages/rewordapp/rewordapp-0.0.2-py3-none-any.whl/rewordapp/config.py
@@ -0,0 +1,11 @@
+"""Module containing the attributes for rewordapp."""
+
+__version__ = '0.0.2'
+version = __version__
+__edition__ = 'Community'
+edition = __edition__
+
+__all__ = [
+    'version',
+    'edition'
+]
