66008e94ee3706952e016ba55c6214d76c4006a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package LibreGaming/0.0.3/LibreGaming-0.0.3-py3-none-any.whl

---
+++ packages/LibreGaming/LibreGaming-0.0.3-py3-none-any.whl/LibreGaming/getPackageManager.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ -x "$(command -v dnf)" ]; then echo "dnf"
+elif [ -x "$(command -v yay)" ]; then echo "yay"
+elif [ -x "$(command -v paru)" ]; then echo "paru"
+elif [ -x "$(command -v pacman)" ]; then echo "pacman"
+elif [ -x "$(command -v apt)" ]; then echo "apt"
+else echo "FAILED TO INSTALL PACKAGE: Package manager not found."; fi
