cf2e50b17d67d62616a51d6ff461c255b43d285d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:48 +0000

    Add package pydmt/0.0.79/pydmt-0.0.79-py3-none-any.whl

---
+++ packages/pydmt/pydmt-0.0.79-py3-none-any.whl/pydmt/main.py
@@ -0,0 +1,70 @@
+import sys
+import os
+import pathlib
+
+import pylogconf.core
+from pytconf import register_endpoint, register_main, config_arg_parse_and_launch
+
+from pydmt.features.documentation import Documentation
+from pydmt.core.pydmt import PyDMT
+from pydmt.features.templating import Templating
+from pydmt.static import APP_NAME, VERSION_STR, DESCRIPTION
+
+
+def add_to_path():
+    add_import_of_cwd = True
+    add_import_of_home = True
+    add_import_of_shared = True
+    if add_import_of_shared:
+        folder = "/etc/pydmt"
+        if folder not in sys.path:
+            sys.path.insert(0, folder)
+    if add_import_of_home:
+        folder = os.path.join(str(pathlib.Path.home()), ".config/pydmt")
+        if folder not in sys.path:
+            sys.path.insert(0, folder)
+    if add_import_of_cwd:
+        if "" not in sys.path:
+            sys.path.insert(0, "")
+
+
+@register_endpoint(
+    description="Build the project",
+)
+def build():
+    # add_to_path()
+
+    pylogconf.core.setup()
+    p = PyDMT()
+
+    # add templating support
+    f = Templating()
+    f.setup(p)
+
+    # add sphinx support
+    f = Documentation()
+    f.setup(p)
+
+    stats = p.build_all()
+    sys.exit(stats.get_os_error_code())
+
+
+@register_endpoint(
+    description="Clean all generated files"
+)
+def clean() -> None:
+    pass
+
+
+@register_main(
+    app_name=APP_NAME,
+    version=VERSION_STR,
+    main_description=DESCRIPTION,
+)
+def main():
+    pylogconf.core.setup()
+    config_arg_parse_and_launch()
+
+
+if __name__ == '__main__':
+    main()
