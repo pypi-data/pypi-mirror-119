8a08d53abf0b55e021417cabc4318b9133828a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:20 +0000

    Add package py-assembly-payments/0.11.0/py-assembly-payments-0.11.0.tar.gz

---
+++ packages/py-assembly-payments/py-assembly-payments-0.11.0.tar.gz/py-assembly-payments-0.11.0/assembly_payments/exceptions.py
@@ -0,0 +1,27 @@
+
+class PyAssemblyPaymentsException(Exception):
+    pass
+
+
+class PyAssemblyPaymentsNotImplementedException(PyAssemblyPaymentsException):
+    pass
+
+
+class PyAssemblyPaymentsBadRequest(PyAssemblyPaymentsException):
+    pass
+
+
+class PyAssemblyPaymentsForbidden(PyAssemblyPaymentsException):
+    pass
+
+
+class PyAssemblyPaymentsNotFound(PyAssemblyPaymentsException):
+    pass
+
+
+class PyAssemblyPaymentsConflict(PyAssemblyPaymentsException):
+    pass
+
+
+class PyAssemblyPaymentsUnprocessableEntity(PyAssemblyPaymentsException):
+    pass
