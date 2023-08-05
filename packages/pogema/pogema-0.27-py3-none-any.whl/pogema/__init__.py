644c7e0bb821c12337ddc453d04239bb1d89fa41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:21 +0000

    Add package pogema/0.27/pogema-0.27-py3-none-any.whl

---
+++ packages/pogema/pogema-0.27-py3-none-any.whl/pogema/__init__.py
@@ -0,0 +1,27 @@
+from gym import register
+from pogema.grid_config import GridConfig
+
+__version__ = '0.27'
+
+__all__ = [
+    'GridConfig',
+]
+
+register(
+    id="SingleAgentPogema-v0",
+    entry_point="pogema.envs:SingleAgentPogema",
+    max_episode_steps=256,
+)
+
+register(
+    id="Pogesa-v0",
+    entry_point="pogema.envs:Pogesa",
+    max_episode_steps=256,
+)
+
+register(
+    id="StochasticPogesa-v0",
+    entry_point="pogema.envs:StochasticPogesa",
+    max_episode_steps=256,
+)
+
