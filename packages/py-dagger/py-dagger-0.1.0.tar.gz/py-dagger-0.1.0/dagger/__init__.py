31a81dd3531eefa85957cd0977ad559745fde102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:55 +0000

    Add package py-dagger/0.1.0/py-dagger-0.1.0.tar.gz

---
+++ packages/py-dagger/py-dagger-0.1.0.tar.gz/py-dagger-0.1.0/dagger/__init__.py
@@ -0,0 +1,18 @@
+"""Define sophisticated workflows/pipelines as Directed Acyclic Graphs (DAGs) and execute them with different runtimes, either locally or remotely."""
+import logging
+import os
+
+import dagger.dsl as dsl  # noqa
+from dagger.dag import DAG  # noqa
+from dagger.task import Task  # noqa
+
+# This will be replaced at package publication time by the latest git tag
+__version__ = "$(git describe --tags --abbrev=0)"
+
+
+LOG_FORMAT = "%(asctime)s %(levelname)s:%(name)s:%(message)s"
+logging.basicConfig(
+    level=os.environ.get("LOG_LEVEL", "INFO"),
+    format=LOG_FORMAT,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
