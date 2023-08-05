827bac7e90d3938272a97b018cf3a0d56bcdac80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:28 +0000

    Add package BinoGausDist/1.0/BinoGausDist-1.0.tar.gz

---
+++ packages/BinoGausDist/BinoGausDist-1.0.tar.gz/BinoGausDist-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='BinoGausDist',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['BinoGausDist'],
+      author = 'Ajitesh Gupta',
+      author_email = 'ajiteshgupta13@gmail.com',
+      zip_safe=False)
