c817c941d6abf61c909d0b5650f3077e5633aca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:12 +0000

    Add package mynomial-probability/0.1/mynomial_probability-0.1.tar.gz

---
+++ packages/mynomial-probability/mynomial_probability-0.1.tar.gz/mynomial_probability-0.1/README.md
@@ -0,0 +1,10 @@
+# mynomial-distribution package
+
+Summary of the package
+
+# Files
+
+Explanation of files in the package
+
+# Installation
+
---
+++ packages/mynomial-probability/mynomial_probability-0.1.tar.gz/mynomial_probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='mynomial_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mynomial_probability'],
+      author = 'SpecialK',
+      author_email = 'blackhole@gmail.com',
+    zip_safe=False)
+    
