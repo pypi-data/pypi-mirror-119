dc8eefaa31c424689466a6733c4320240722f341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package dungeonsheets/0.17.1/dungeonsheets-0.17.1-py3-none-any.whl

---
+++ packages/dungeonsheets/dungeonsheets-0.17.1-py3-none-any.whl/dungeonsheets/mechanics.py
@@ -0,0 +1,18 @@
+"""Convenience module holding base classes for the various kinds of
+game mechanics."""
+
+from dungeonsheets.content import Content
+from dungeonsheets.spells import Spell
+from dungeonsheets.features import Feature
+from dungeonsheets.infusions import Infusion
+from dungeonsheets.weapons import (
+    Weapon,
+    MeleeWeapon,
+    RangedWeapon,
+    SimpleWeapon,
+    MartialWeapon,
+)
+from dungeonsheets.armor import Armor, Shield
+from dungeonsheets.magic_items import MagicItem
+from dungeonsheets.monsters import Monster
+from dungeonsheets.stats import Ability
