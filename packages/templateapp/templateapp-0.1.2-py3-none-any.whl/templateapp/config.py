e6c01061b7902b359fe893ca7e06dc324788f364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:28 +0000

    Add package templateapp/0.1.2/templateapp-0.1.2-py3-none-any.whl

---
+++ packages/templateapp/templateapp-0.1.2-py3-none-any.whl/templateapp/config.py
@@ -0,0 +1,11 @@
+"""Module containing the attributes for templateapp."""
+
+__version__ = '0.1.2'
+version = __version__
+__edition__ = 'Community'
+edition = __edition__
+
+__all__ = [
+    'version',
+    'edition'
+]
