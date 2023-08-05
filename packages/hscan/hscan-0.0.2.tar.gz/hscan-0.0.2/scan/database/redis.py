641db75f87d4a29a34ae2fcd66f7d6d91aaedf7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:06 +0000

    Add package hscan/0.0.2/hscan-0.0.2.tar.gz

---
+++ packages/hscan/hscan-0.0.2.tar.gz/hscan-0.0.2/scan/database/redis.py
@@ -0,0 +1,20 @@
+import aioredis
+
+
+class Redis:
+    def __init__(self, **kwargs):
+        self.host = kwargs.get('host') or 'localhost'
+        self.port = kwargs.get('port') or 6379
+        self.password = kwargs.get('password')
+        self.db = kwargs.get('db') or 1
+        self.pool_size = kwargs.get('pool_size') or 10
+        self.user = kwargs.get('user')
+
+    async def get_redis(self):
+        pool = aioredis.ConnectionPool.from_url(f"redis://{self.host}", port=int(self.port), username=self.user, password=self.password,
+                                                db=int(self.db), max_connections=int(self.pool_size))
+        redis = aioredis.Redis(connection_pool=pool)
+        return redis
+
+
+__all__ = Redis
