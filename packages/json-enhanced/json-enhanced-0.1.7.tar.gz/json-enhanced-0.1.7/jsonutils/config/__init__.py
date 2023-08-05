fd0a72bb911d7842b3a0c23392d42523a4c4467c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:50 +0000

    Add package json-enhanced/0.1.7/json-enhanced-0.1.7.tar.gz

---
+++ packages/json-enhanced/json-enhanced-0.1.7.tar.gz/json-enhanced-0.1.7/jsonutils/config/__init__.py
@@ -0,0 +1,8 @@
+from .completion import autocomplete_only_nodes
+from .locals import decimal_separator, thousands_separator
+from .queries import (
+    clever_parsing,
+    include_parents,
+    query_exceptions,
+    recursive_queries,
+)
