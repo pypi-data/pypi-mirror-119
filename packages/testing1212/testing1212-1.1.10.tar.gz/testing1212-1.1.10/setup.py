f4379bf8ac97fbfe1af9227394aafaa95731e1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:22 +0000

    Add package testing1212/1.1.10/testing1212-1.1.10.tar.gz

---
+++ packages/testing1212/testing1212-1.1.10.tar.gz/testing1212-1.1.10/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+import os
+
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    author="Rahul Kumar",
+    author_email="rahul@trell.in",
+    name='testing1212',
+    description='Utility code',
+    version="v1.1.10",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://gitlab.com/Rahultrel/testing',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6.9",
+    install_requires=['requests'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+
+    ],
+)
