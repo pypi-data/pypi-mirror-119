9b8bda3f7636e7f261c2a4b6ffa7d28490fe1d07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:49 +0000

    Add package smack-talk-steve/0.0.3/smack_talk_steve-0.0.3-py3-none-any.whl

---
+++ packages/smack-talk-steve/smack_talk_steve-0.0.3-py3-none-any.whl/steve/insults.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def speak():
+
+	return np.random.choice(("shut your mouth before i smack it", "you're a clown", "you smell", "dummy", "moron", "shut up", "no one likes you"))
