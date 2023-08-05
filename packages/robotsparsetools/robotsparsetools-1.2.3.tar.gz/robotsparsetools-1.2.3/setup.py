9b31a131abf319687672961f7f189b80d9199f36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:54 +0000

    Add package robotsparsetools/1.2.3/robotsparsetools-1.2.3.tar.gz

---
+++ packages/robotsparsetools/robotsparsetools-1.2.3.tar.gz/robotsparsetools-1.2.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("./README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="robotsparsetools",
+    description="Parse robots.txt",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version="1.2.3",
+    author="minomushi",
+    author_email="mino3@cocoro.uk",
+    url="https://github.com/mino-38/robotsparsetools",
+    packages=["robotsparsetools"],
+    keywords="robots.txt parser",
+    licence="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3 :: Only"
+    ]
+)
