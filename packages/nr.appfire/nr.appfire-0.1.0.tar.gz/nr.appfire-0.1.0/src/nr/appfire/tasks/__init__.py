db2d85d51714a2ac89844e4eb95bbb61c814e903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:46 +0000

    Add package nr.appfire/0.1.0/nr.appfire-0.1.0.tar.gz

---
+++ packages/nr.appfire/nr.appfire-0.1.0.tar.gz/nr.appfire-0.1.0/src/nr/appfire/tasks/__init__.py
@@ -0,0 +1,8 @@
+
+"""
+The `nr.appfire.tasks` package provides an easy-to-use framework for managing background tasks in a
+Python application.
+"""
+
+from .task import Task, TaskStatus, TaskCallback
+from .task_manager import TaskManager
