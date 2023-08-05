59fccceb001376a2725155b81e228ba8d13e23fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:03 +0000

    Add package cspip/1.1/cspip-1.1.tar.gz

---
+++ packages/cspip/cspip-1.1.tar.gz/cspip-1.1/cspip/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py
+@Time    :   2021/09/08 11:13:21
+@Author  :   jixn
+@Version :   1.0
+'''
+
+
+from jixnprint import csjixn
---
+++ packages/cspip/cspip-1.1.tar.gz/cspip-1.1/cspip/jixnprint.py
@@ -0,0 +1,10 @@
+import requests
+
+class csjixn:
+	def name1():
+		print("jixn1344")
+
+	def name2():
+		r = requests.get("http://www.baidu.com")
+		print(r)
+		return r
