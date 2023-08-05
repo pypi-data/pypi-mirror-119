c67d4a81430008206f45b9bccda8735a85358866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:43 +0000

    Add package limes-inventory/0.4.dev1/limes-inventory-0.4.dev1.tar.gz

---
+++ packages/limes-inventory/limes-inventory-0.4.dev1.tar.gz/limes-inventory-0.4.dev1/src/limes_common/config.py
@@ -0,0 +1,31 @@
+from pytz import timezone
+
+_DEBUG = True
+
+TIME_ZONE = timezone('Canada/Pacific')
+ENCODING = 'utf-8'
+if _DEBUG:
+    HTTP_TIMEOUT = 10
+else:
+    HTTP_TIMEOUT = 5
+
+CSRF_NAME = 'X-CSRFToken'
+
+# ELAB_URL = 'https://us.elabjournal.com/'
+ELAB_URL = 'https://elab.msl.ubc.ca/'
+ELAB_API = '%sapi/v1/' % ELAB_URL
+
+SERVER_API_VER = 'd1'
+if _DEBUG:
+    SERVER_BIND = '127.0.0.1:8001'
+else:
+    SERVER_BIND = 'sh-lims.microbiology.ubc.ca:8001'
+SERVER_URL = 'http://%s/api/%s/' % (SERVER_BIND, SERVER_API_VER)
+SERVER_DB_PATH = 'limes_server/db'
+
+# PROVIDER_STATICS_PATH = 'limes_core/example_static_providers.json'
+PROVIDER_STATICS_PATH = 'limes_core/static_providers.json'
+PROVIDER_DEFAULT_TRANSACTION_TIMEOUT = 5
+PROVIDER_DEFAULT_CONNECTION_TIMEOUT = 60
+
+ELAB_CACHE = 'elab_cache.npy'
