df831a2eac751037a06b8581f45c854b9c6eff2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:53 +0000

    Add package odoo12-addon-hr-employee-multidepartment/12.0.0.0.5/odoo12-addon-hr_employee_multidepartment-12.0.0.0.5.tar.gz

---
+++ packages/odoo12-addon-hr-employee-multidepartment/odoo12-addon-hr_employee_multidepartment-12.0.0.0.5.tar.gz/odoo12-addon-hr_employee_multidepartment-12.0.0.0.5/odoo/addons/hr_employee_multidepartment/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': "HR Employee Multidepartment",
+    'version': '12.0.0.0.5',
+    'depends': [
+        'hr',
+        'hr_holidays',
+    ],
+    'author': """Som Energia SCCL,
+    Odoo Community Association (OCA)
+    """,
+    'website': 'https://github.com/OCA/hr',
+    'category': 'Human Resources',
+    'summary': """
+    Add multidepartment feature.
+    """,
+    'license': 'AGPL-3',
+    'demo': [
+    ],
+    'data': [
+        'views/hr_employee_views.xml',
+        'views/hr_leave_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
