09d4e29fffdea6fdf4a79693de0cc221c5141edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:19 +0000

    Add package cdk8s-aws-load-balancer-controller/2.3.6/cdk8s_aws_load_balancer_controller-2.3.6-py3-none-any.whl

---
+++ packages/cdk8s-aws-load-balancer-controller/cdk8s_aws_load_balancer_controller-2.3.6-py3-none-any.whl/cdk8s_aws_load_balancer_controller/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_iam._jsii
+import aws_cdk.core._jsii
+import cdk8s._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk8s-aws-load-balancer-controller",
+    "2.3.6",
+    __name__[0:-6],
+    "cdk8s-aws-load-balancer-controller@2.3.6.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
