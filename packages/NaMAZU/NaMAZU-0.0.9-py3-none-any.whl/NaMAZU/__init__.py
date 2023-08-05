3405760edfb51a0ef303f08e16978f98151bcc35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:42 +0000

    Add package NaMAZU/0.0.9/NaMAZU-0.0.9-py3-none-any.whl

---
+++ packages/NaMAZU/NaMAZU-0.0.9-py3-none-any.whl/NaMAZU/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.9"
+
+import NaMAZU.functional
+import NaMAZU.lightning_wingman
+
+# rm -rf build dist NaMAZU.egg-info
+# python setup.py bdist_wheel
+# twine upload dist/*
---
+++ packages/NaMAZU/NaMAZU-0.0.9-py3-none-any.whl/NaMAZU/functional/__init__.py
@@ -0,0 +1,13 @@
+from .image_control import *
+from .file_control import *
+
+__all__ = [
+    "img_to_npy",
+    "npy_to_png",
+    "split_image",
+    "apply_to_all",
+    "collect_file_pathes_by_ext",
+    "zip_files",
+    "change_frame_rates_in",
+]
+
