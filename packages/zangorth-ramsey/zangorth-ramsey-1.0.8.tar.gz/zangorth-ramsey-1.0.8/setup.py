9ea2fe863f4e29773dd9f6bcfbf9073955ace6e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:44 +0000

    Add package zangorth-ramsey/1.0.8/zangorth-ramsey-1.0.8.tar.gz

---
+++ packages/zangorth-ramsey/zangorth-ramsey-1.0.8.tar.gz/zangorth-ramsey-1.0.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+      name='zangorth-ramsey',
+      version='1.0.8',
+      description='Helper Functions for Ramsey Project',
+      author='Zangorth',
+      packages=['ramsey'],
+      install_requires=open(r'C:\Users\Samuel\Google Drive\Portfolio\Ramsey\helper_functions\requirements.txt', 'r').read().split('\n')[0:-1]
+      )
