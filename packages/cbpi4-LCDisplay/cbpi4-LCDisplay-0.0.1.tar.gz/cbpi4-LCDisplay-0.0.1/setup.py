a470fcb6a73f9266aae184f77a029da57dfc2bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:58 +0000

    Add package cbpi4-LCDisplay/0.0.1/cbpi4-LCDisplay-0.0.1.tar.gz

---
+++ packages/cbpi4-LCDisplay/cbpi4-LCDisplay-0.0.1.tar.gz/cbpi4-LCDisplay-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='cbpi4-LCDisplay',
+      version='0.0.1',
+      description='CraftBeerPi4 LCD Plugin',
+      author='Jan Battermann',
+      author_email='jan.battermann@t-online.de',
+      url='https://github.com/JamFfm/cbpi4-LCDisplay',
+      license='GPLv3',
+      packages=find_packages(),
+      include_package_data=True,
+      package_data={
+          # If any package contains *.txt or *.rst files, include them:
+          '': ['*.txt', '*.rst', '*.yaml'],
+          'cbpi4-LCDisplay': ['*', '*.txt', '*.rst', '*.yaml']},
+      # packages=['cbpi4-LCDisplay'],
+      install_requires=[
+          'cbpi>=4.0.0.33',
+          'smbus2',
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+      )
