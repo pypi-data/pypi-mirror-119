d9369c96aa7f79d7bb56daf7ab790826acbcb3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:35 +0000

    Add package hamlet/9.5.1.dev6/hamlet-9.5.1.dev6.tar.gz

---
+++ packages/hamlet/hamlet-9.5.1.dev6.tar.gz/hamlet-9.5.1.dev6/hamlet/command/component/__init__.py
@@ -0,0 +1,20 @@
+from hamlet.command import root as cli
+
+from hamlet.command.component.describe_occurrence import (
+    describe_occurrence as describe_occurrence_group,
+)
+from hamlet.command.component.list_occurrences import (
+    list_occurrences as list_occurrences_command,
+)
+
+
+@cli.group("component", context_settings=dict(max_content_width=240))
+def component_group():
+    """
+    Provides information on the components used in a hamlet
+    """
+    pass
+
+
+component_group.add_command(describe_occurrence_group)
+component_group.add_command(list_occurrences_command)
