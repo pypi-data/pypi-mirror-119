c43e3efde23cf9078c630a2191b576c53904df56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:58 +0000

    Add package pfhedge/0.13.0/pfhedge-0.13.0-py3-none-any.whl

---
+++ packages/pfhedge/pfhedge-0.13.0-py3-none-any.whl/pfhedge/features/_getter.py
@@ -0,0 +1,55 @@
+from typing import Callable
+from typing import Mapping
+from typing import Union
+
+from ._base import Feature
+from .features import Empty
+from .features import ExpiryTime
+from .features import LogMoneyness
+from .features import MaxLogMoneyness
+from .features import MaxMoneyness
+from .features import Moneyness
+from .features import PrevHedge
+from .features import TimeToMaturity
+from .features import Variance
+from .features import Volatility
+from .features import Zeros
+
+FEATURES = [
+    Empty,
+    ExpiryTime,
+    TimeToMaturity,
+    LogMoneyness,
+    MaxLogMoneyness,
+    MaxMoneyness,
+    Moneyness,
+    PrevHedge,
+    Variance,
+    Volatility,
+    Zeros,
+]
+
+DICT_FEATURES = {str(f()): f for f in FEATURES}
+
+
+def get_feature(feature: Union[str, Feature]) -> Feature:
+    """Get feature from name.
+
+    Args:
+        name (str): Name of feature.
+
+    Returns:
+        Feature
+    """
+    dict_features: Mapping[str, Callable[[], Feature]] = DICT_FEATURES
+
+    if isinstance(feature, str):
+        if feature not in dict_features:
+            raise ValueError(
+                f"{feature} is not a valid value. "
+                "Use sorted(pfhedge.features.FEATURES) to get valid options."
+            )
+        feature = dict_features[feature]()
+    elif not isinstance(feature, Feature):
+        raise TypeError(f"{feature} is not an instance of Feature.")
+    return feature
