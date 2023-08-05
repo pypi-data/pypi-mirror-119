a43fe1354f6334bb599e011e226bcdee0a6aba7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:16 +0000

    Add package raser/2.0.2/raser-2.0.2.tar.gz

---
+++ packages/raser/raser-2.0.2.tar.gz/raser-2.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup,find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="raser",
+    version="2.0.2",
+    author="Xin Shi",
+    author_email="xin.shi@outlook.com",
+    description="Simulate timing resolution, TCT, TPA-TCT of 2D&3D Si&SiC detector",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dt-np/raser",
+    packages=find_packages(),
+    classifiers=[
+				"Programming Language :: Python :: 3",
+				"License :: OSI Approved :: MIT License",
+				"Operating System :: OS Independent",
+    			]
+)
