a3c9965b2a53b3e252b7546993ed354722ae38ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:57 +0000

    Add package dlapp/0.2.0/dlapp-0.2.0.tar.gz

---
+++ packages/dlapp/dlapp-0.2.0.tar.gz/dlapp-0.2.0/dlapp/config.py
@@ -0,0 +1,11 @@
+"""Module containing the attributes for connectorapp."""
+
+__version__ = '0.2.0'
+version = __version__
+__edition__ = 'Community'
+edition = __edition__
+
+__all__ = [
+    'version',
+    'edition'
+]
