de16c510900fc930110a2280d8cf1434d8a8cf32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package extattr/1.0/extattr-1.0.tar.gz

---
+++ packages/extattr/extattr-1.0.tar.gz/extattr-1.0/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='extattr',
+    packages=['extattr'],
+    version='1.0',
+    license='MIT',
+    description='Extra Attributes For Python',
+    author='gncdev',
+    author_email='info@teamwtss.com',
+    url='https://github.com/gncdev/extattr',
+    keywords=['Code', 'Python', 'extra', 'ext', 'attr', 'attributes', 'final', "etc", "Programming"],
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
