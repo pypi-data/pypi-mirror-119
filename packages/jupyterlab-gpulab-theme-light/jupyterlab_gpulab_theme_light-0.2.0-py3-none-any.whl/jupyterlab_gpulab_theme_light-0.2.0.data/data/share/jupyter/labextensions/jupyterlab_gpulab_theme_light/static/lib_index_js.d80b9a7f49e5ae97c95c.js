91cc167bba77b71af184064dda4fcced8274f798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:08 +0000

    Add package jupyterlab-gpulab-theme-light/0.2.0/jupyterlab_gpulab_theme_light-0.2.0-py3-none-any.whl

---
+++ packages/jupyterlab-gpulab-theme-light/jupyterlab_gpulab_theme_light-0.2.0-py3-none-any.whl/jupyterlab_gpulab_theme_light-0.2.0.data/data/share/jupyter/labextensions/jupyterlab_gpulab_theme_light/static/lib_index_js.d80b9a7f49e5ae97c95c.js
@@ -0,0 +1,41 @@
+(self["webpackChunkjupyterlab_gpulab_theme_light"] = self["webpackChunkjupyterlab_gpulab_theme_light"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__);
+
+/**
+ * Initialization data for the jupyterlab_gpulab_theme_light extension.
+ */
+const extension = {
+    id: 'jupyterlab_gpulab_theme_light',
+    requires: [_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__.IThemeManager],
+    autoStart: true,
+    activate: (app, manager) => {
+        console.log('JupyterLab extension jupyterlab_gpulab_theme_light is activated!');
+        const style = 'jupyterlab_gpulab_theme_light/index.css';
+        manager.register({
+            name: 'GPULab Light',
+            isLight: true,
+            load: () => manager.loadCSS(style),
+            unload: () => Promise.resolve(undefined)
+        });
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (extension);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.d80b9a7f49e5ae97c95c.js.map
