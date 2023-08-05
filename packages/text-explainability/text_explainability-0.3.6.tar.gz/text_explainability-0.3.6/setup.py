368d6fa139c334f2e4799548e9087097c4b51574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:09:37 +0000

    Add package text-explainability/0.3.6/text_explainability-0.3.6.tar.gz

---
+++ packages/text-explainability/text_explainability-0.3.6.tar.gz/text_explainability-0.3.6/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+from os import path
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup( # type: ignore
+    name = 'text_explainability',
+    version = '0.3.6',
+    description = 'Generic explainability architecture for text machine learning models',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = 'Marcel Robeer',
+    author_email = 'm.j.robeer@uu.nl',
+    license = 'GNU LGPL v3',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence'
+    ],
+    url='https://git.science.uu.nl/m.j.robeer/text_explainability',
+    packages = setuptools.find_packages(), # type : ignore
+    install_requires = [
+        'instancelib>=0.3.2.0',
+        'numpy>=1.19.5'
+        'python-i18n>=0.3.9',
+        'scikit-learn>=0.24.1',
+    ],
+    python_requires = '>=3.8'
+)
