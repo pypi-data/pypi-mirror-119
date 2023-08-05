eda4191b63d896d941bc34b6e2baf91dec05742b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:08 +0000

    Add package pdfmacha/1.0/pdfmacha-1.0.tar.gz

---
+++ packages/pdfmacha/pdfmacha-1.0.tar.gz/pdfmacha-1.0/pdfmacha/pdf2image.py
@@ -0,0 +1,18 @@
+""" 
+One line description.
+
+A more detailed explanation.
+"""
+
+
+def convert(path):
+    """ 
+    Convert given pdf to text.
+
+    Parameters:
+    path (str): The path to a PDF file.
+
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("pdf2text", path)
