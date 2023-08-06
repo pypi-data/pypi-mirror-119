f224e9cf6e5353c6111fda8e380f8649980e87c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:48 +0000

    Add package asynciotimemachine/0.2.0/asynciotimemachine-0.2.0.tar.gz

---
+++ packages/asynciotimemachine/asynciotimemachine-0.2.0.tar.gz/asynciotimemachine-0.2.0/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+]
+
+setup_requirements = [
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest',
+]
+
+setup(
+    name='asynciotimemachine',
+    version='0.2.0',
+    description="Monkey-patches asyncio.AbstractEventLoop so as to make it "
+                "think it lives in the future.",
+    long_description=readme + '\n\n' + history,
+    author="Eugene M. Kim",
+    author_email='astralblue@gmail.com',
+    url='https://github.com/astralblue/asynciotimemachine',
+    py_modules=['asynciotimemachine'],
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='asynciotimemachine',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
