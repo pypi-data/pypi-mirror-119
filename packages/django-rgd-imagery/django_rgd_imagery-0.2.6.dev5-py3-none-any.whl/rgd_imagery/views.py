01dc772b66cf7b9bd63c195b7f04106aa16eaf2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:28 +0000

    Add package django-rgd-imagery/0.2.6.dev5/django_rgd_imagery-0.2.6.dev5-py3-none-any.whl

---
+++ packages/django-rgd-imagery/django_rgd_imagery-0.2.6.dev5-py3-none-any.whl/rgd_imagery/views.py
@@ -0,0 +1,18 @@
+from rgd.views import PermissionDetailView, SpatialEntriesListView, _SpatialDetailView
+
+from . import filters, models
+
+
+class ImageSetDetailView(PermissionDetailView):
+    model = models.ImageSet
+
+
+class RasterMetaEntriesListView(SpatialEntriesListView):
+    model = models.RasterMeta
+    filter = filters.RasterMetaFilter
+    context_object_name = 'spatial_entries'
+    template_name = 'rgd_imagery/rastermeta_list.html'
+
+
+class RasterDetailView(_SpatialDetailView):
+    model = models.RasterMeta
