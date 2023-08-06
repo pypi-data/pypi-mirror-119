6923faa83580dd7eae3033f2a88d7c4041e02ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:46 +0000

    Add package orffinder/1.6/orffinder-1.6.tar.gz

---
+++ packages/orffinder/orffinder-1.6.tar.gz/orffinder-1.6/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="orffinder",
+    version="1.6",
+    author="ChocoParrot",
+    author_email="lachocoparrot@gmail.com",
+    description="Open Reading Frame finder natively coded in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chokyotager/ORFFinder",
+    project_urls={
+        "Bug Tracker": "https://github.com/Chokyotager/ORFFinder/issues",
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+    install_requires=[
+        "biopython>=1.79"
+    ],
+    scripts=[
+        "src/cline_tools/orffinder-to-gtf",
+        "src/cline_tools/orffinder-to-sequence",
+        "src/cline_tools/orffinder-to-gff3"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6"
+)
