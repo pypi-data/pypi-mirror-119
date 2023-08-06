29bae3427571e6b5c2e64a98f653fabdcf505714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:29 +0000

    Add package eric-tools/1.2.1/Eric_Tools-1.2.1-py2-none-any.whl

---
+++ packages/eric-tools/Eric_Tools-1.2.1-py2-none-any.whl/common/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*-
+'''
+@Author  : Eric
+@Time    : 2020-11-30 17:31
+@IDE     : PyCharm
+'''
+name = 'Eric-Tools'
+__title__ = 'tools'
+__description__ = 'Python HTTP for Humans.'
+__version__ = "1.2.1"
+__author__ = 'Eric'
+__doc__ = ["Python Daily Development Tools"]
+__url__ = "https://github.com/Eric-jxl/Tools"
+__license__ = "Apache"
+
+import exception_class
+import resize_image
+import logger
+import ip
+import encryption_classmethod
+import pgsql
+import sftp
+import readconfig
+import send_email
+import jwt_encrypt
+import convert_json
+import Abstract
+import decorator
