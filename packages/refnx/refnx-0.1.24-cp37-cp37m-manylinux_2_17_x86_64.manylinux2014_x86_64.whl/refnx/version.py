77a43b5137305c7b7a1e90d659243db98bbe3864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:59 +0000

    Add package refnx/0.1.24/refnx-0.1.24-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/refnx/refnx-0.1.24-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/refnx/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM REFNX SETUP.PY
+short_version = '0.1.24'
+version = '0.1.24'
+full_version = '0.1.24'
+git_revision = 'c3563237f45ccdd628ffb92678d472f5683a2a31'
+release = True
+if not release:
+    version = full_version
