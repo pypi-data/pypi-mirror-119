c8c9781e8e71aee42b7c2345e36c73f894aa9451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:59 +0000

    Add package napplib/0.8.20/napplib-0.8.20-py3-none-any.whl

---
+++ packages/napplib/napplib-0.8.20-py3-none-any.whl/napplib/vtex/v2/models/order.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import List
+from enum import Enum
+from typing import Optional
+
+class VtexOrderInvoiceType(Enum):
+    OUTPUT = "Output"
+    INPUT = "Input"
+
+@dataclass
+class VtexOrderInvoiceItem:
+    id: str
+    quantity: int
+    price: int
+
+@dataclass
+class VtexOrderInvoice:
+    type: VtexOrderInvoiceType
+    invoiceNumber: float
+    invoiceValue: int
+    items: List[VtexOrderInvoiceItem]
+    invoiceKey: Optional[str] = None
+    invoiceUrl: Optional[str] = None
+    issuanceDate: Optional[str] = None
+    
