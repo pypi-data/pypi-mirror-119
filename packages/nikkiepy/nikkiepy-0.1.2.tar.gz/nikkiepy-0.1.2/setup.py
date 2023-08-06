b5266249a0b912e9fd6df674a201c6aafd0f2073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:53 +0000

    Add package nikkiepy/0.1.2/nikkiepy-0.1.2.tar.gz

---
+++ packages/nikkiepy/nikkiepy-0.1.2.tar.gz/nikkiepy-0.1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", 'r', encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nikkiepy",
+    version="0.1.2",
+    author="NikkieDev#6322",
+    author_email="nikkieschaad@gmail.com",
+    description="Custom python library by: NikkieDev",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NikkieCodes/nikkiepy",
+    python_requires=">=3.7",
+    packages=setuptools.find_packages(where="nikkiepy")
+)
