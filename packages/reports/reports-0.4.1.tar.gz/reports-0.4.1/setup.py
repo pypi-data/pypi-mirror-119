fe985130f4e9fab21831f34a89d061819e10c51b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:53 +0000

    Add package reports/0.4.1/reports-0.4.1.tar.gz

---
+++ packages/reports/reports-0.4.1.tar.gz/reports-0.4.1/setup.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+__revision__ = "$Id$" # for the SVN Id
+import sys
+import os
+from setuptools import setup, find_packages
+import glob
+
+_MAJOR               = 0
+_MINOR               = 4
+_MICRO               = 1
+version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
+release              = '%d.%d' % (_MAJOR, _MINOR)
+
+metainfo = {
+    'authors': {"main": ("Thomas Cokelaer", "cokelaer@gmail.com")},
+    'version': version,
+    'license' : 'BSD3',
+    'download_url' : 'https://pypi.python.org/pypi/reports',
+    'url' : "https://github.com/cokelaer/reports/",
+    'description': "Quickly create HTML reports using a set of JINJA templates" ,
+    'platforms' : ['Linux', 'Unix', 'MacOsX', 'Windows'],
+    'keywords' : ['HTML', 'table', 'jinja', 'report', 'reports'],
+    'classifiers' : [
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          'Topic :: Scientific/Engineering :: Physics']
+    }
+
+requirements = open("requirements.txt").read().split()
+
+
+setup(
+    name             = "reports",
+    version          = version,
+    maintainer       = metainfo['authors']['main'][0],
+    maintainer_email = metainfo['authors']['main'][1],
+    author           = metainfo['authors']['main'][0],
+    author_email     = metainfo['authors']['main'][1],
+    long_description = open("README.rst").read(),
+    keywords         = metainfo['keywords'],
+    description      = metainfo['description'],
+    license          = metainfo['license'],
+    platforms        = metainfo['platforms'],
+    url              = metainfo['url'],
+    download_url     = metainfo['download_url'],
+    classifiers      = metainfo['classifiers'],
+
+    zip_safe=False,
+    # package installation
+    packages = ["reports", "reports.resources"],
+    include_package_data = True,
+    package_data = {"reports.resources": ['javascript/*', 'templates/generic/*', 'css/*']},
+
+    install_requires = requirements,
+)
