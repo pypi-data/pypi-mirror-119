0e179fbb13d1a02f3bfbc74401e84dfb2a3f5c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:56 +0000

    Add package delft-physics-lab/0.4/delft_physics_lab-0.4.tar.gz

---
+++ packages/delft-physics-lab/delft_physics_lab-0.4.tar.gz/delft_physics_lab-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'delft_physics_lab',
+  packages = find_packages(),
+  version = '0.4',
+  license='MIT',
+  description = 'delft physics lab',
+  install_requires=[
+    'numpy',
+    'scipy',
+    'matplotlib'
+  ],
+)
