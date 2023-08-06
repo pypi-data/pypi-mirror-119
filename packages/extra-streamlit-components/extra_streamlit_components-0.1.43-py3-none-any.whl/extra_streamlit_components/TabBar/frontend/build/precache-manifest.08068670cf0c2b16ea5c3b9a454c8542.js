6c2fe968f298053161f1d1ddf20cbf3041b7f60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:30 +0000

    Add package extra-streamlit-components/0.1.43/extra_streamlit_components-0.1.43-py3-none-any.whl

---
+++ packages/extra-streamlit-components/extra_streamlit_components-0.1.43-py3-none-any.whl/extra_streamlit_components/TabBar/frontend/build/precache-manifest.08068670cf0c2b16ea5c3b9a454c8542.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95314f54130b918af778f1dcd4708ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0313970224d8df9f9f07",
+    "url": "./static/js/2.c93cfe15.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.c93cfe15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8935a82e67c40cc4533c",
+    "url": "./static/js/main.e891e0d5.chunk.js"
+  },
+  {
+    "revision": "2f8f0d56246b8a0cd183",
+    "url": "./static/js/runtime-main.b0f65f68.js"
+  }
+]);
