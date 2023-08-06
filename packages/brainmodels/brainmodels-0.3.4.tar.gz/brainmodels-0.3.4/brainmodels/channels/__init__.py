6f60b9bc853ee4894cc040a91d052069eaa02758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:29 +0000

    Add package brainmodels/0.3.4/brainmodels-0.3.4.tar.gz

---
+++ packages/brainmodels/brainmodels-0.3.4.tar.gz/brainmodels-0.3.4/brainmodels/channels/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+
+from . import Ca
+from . import K
+from . import Na
+from . import other
+# from .Ca import *
+# from .K import *
+# from .other import *
+# from .Na import *
+
