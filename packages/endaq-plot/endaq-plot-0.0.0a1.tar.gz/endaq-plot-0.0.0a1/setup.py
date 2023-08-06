5447e88b3468ab628357e68f5227fd2187cd8175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:52 +0000

    Add package endaq-plot/0.0.0a1/endaq-plot-0.0.0a1.tar.gz

---
+++ packages/endaq-plot/endaq-plot-0.0.0a1.tar.gz/endaq-plot-0.0.0a1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name='endaq-plot',
+        version='0.0.0a1',
+        author='Mide Technology',
+        author_email='help@mide.com',
+        packages=[],
+        package_dir={'endaq-plot': './endaq'},
+        install_requires=[],
+)
