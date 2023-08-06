b2cd520e598ebe58329276b29d1f4f51f2f7029e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:25 +0000

    Add package physbiblio/1.8.2/physbiblio-1.8.2.tar.gz

---
+++ packages/physbiblio/physbiblio-1.8.2.tar.gz/physbiblio-1.8.2/physbiblio/version.py
@@ -0,0 +1,9 @@
+__version__ = "1.8.2"
+__version_date__ = "10/09/2021"
+
+__recent_changes__ = """<br>* copy name of directory for associated files from bibtex table<br>
+* fix bug with different type (int/str) of inspire id fields that made it to be updated every day<br>
+* useful new tab name when opening entries corresponding to cats/exps in new tab<br>
+* fix importing from inspire when bibtex contains latex accents such as '\"a'<br>
+* fixing bugs with 'Update bibtex' and 'Reload bibtex' from INSPIRE<br>
+"""
