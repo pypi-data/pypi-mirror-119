e170f414605e2c318baec9dbcd1fd97890291108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:45 +0000

    Add package xlsx-lib-infomoto/0.1.6/xlsx-lib-infomoto-0.1.6.tar.gz

---
+++ packages/xlsx-lib-infomoto/xlsx-lib-infomoto-0.1.6.tar.gz/xlsx-lib-infomoto-0.1.6/xlsx_lib/domain/tightening_torques/component.py
@@ -0,0 +1,12 @@
+from xlsx_lib.domain.json_serializable.custom_json_serializable import JSONSerializable
+from xlsx_lib.domain.tightening_torques.component_part import ComponentPart
+
+
+class Component(JSONSerializable):
+    name: str
+    parts: list[ComponentPart]
+
+    def __init__(self, name: str):
+        self.name = name
+        self.parts = list()
+
