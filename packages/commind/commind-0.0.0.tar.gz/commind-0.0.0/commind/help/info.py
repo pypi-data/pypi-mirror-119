d2c0f0caa370b4b9f59fa65ae104b846d65b7d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:43 +0000

    Add package commind/0.0.0/commind-0.0.0.tar.gz

---
+++ packages/commind/commind-0.0.0.tar.gz/commind-0.0.0/commind/help/info.py
@@ -0,0 +1,20 @@
+
+help_text = '''
+################################################
+PYCOMMIND
+-----------------------------------------------
+Control:
+\tCtrl-;\tFocus on the command line
+Commands:
+\t a:Add a node on the current node and enter it;
+\t n:Add a node on the current node but not enter it;
+\t d:Delete a node and its child nodes;
+\t s [text]:Set the text of the node;
+\t e:Enter the first child node of the node;
+\t l:Enter the next child node.
+The tool is still developing and you can use your screenshot tools to save the picture.
+################################################
+Have a good time!
+'''
+def help():
+    return help_text
