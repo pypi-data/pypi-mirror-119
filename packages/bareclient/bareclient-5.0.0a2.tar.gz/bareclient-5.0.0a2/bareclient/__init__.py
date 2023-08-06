9380ae3f70094465684e09e4db54c0ee9f3a02b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:59 +0000

    Add package bareclient/5.0.0a2/bareclient-5.0.0a2.tar.gz

---
+++ packages/bareclient/bareclient-5.0.0a2.tar.gz/bareclient-5.0.0a2/bareclient/__init__.py
@@ -0,0 +1,45 @@
+"""bareclient"""
+
+from .client import HttpClient
+from .session import HttpSession
+from .unbound_session import HttpUnboundSession
+from .helpers import (
+    request,
+    get,
+    get_text,
+    get_json,
+    post,
+    post_text,
+    post_json
+)
+from .constants import DEFAULT_PROTOCOLS
+from .middleware import HttpClientMiddlewareCallback, HttpClientCallback
+from .ssl_contexts import (
+    create_ssl_context,
+    create_ssl_context_with_cert_chain,
+    DEFAULT_CIPHERS,
+    DEFAULT_OPTIONS
+)
+from .types import Request, Response
+
+__all__ = [
+    'HttpClient',
+    'HttpSession',
+    'HttpUnboundSession',
+    'request',
+    'get',
+    'get_text',
+    'get_json',
+    'post',
+    'post_text',
+    'post_json',
+    'create_ssl_context',
+    'create_ssl_context_with_cert_chain',
+    'DEFAULT_PROTOCOLS',
+    'DEFAULT_CIPHERS',
+    'DEFAULT_OPTIONS',
+    'Request',
+    'Response',
+    'HttpClientMiddlewareCallback',
+    'HttpClientCallback'
+]
