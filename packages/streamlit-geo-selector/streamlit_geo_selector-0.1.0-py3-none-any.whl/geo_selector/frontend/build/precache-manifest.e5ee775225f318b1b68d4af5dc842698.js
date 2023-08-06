57d6a00421c645714a5f8374b3a1e5e8e8c9d331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:17 +0000

    Add package streamlit-geo-selector/0.1.0/streamlit_geo_selector-0.1.0-py3-none-any.whl

---
+++ packages/streamlit-geo-selector/streamlit_geo_selector-0.1.0-py3-none-any.whl/geo_selector/frontend/build/precache-manifest.e5ee775225f318b1b68d4af5dc842698.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3553b4a35c41c9ca24fb3a2a24d1bfe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c714b38ea4126498b0a2",
+    "url": "./static/css/2.e315e6af.chunk.css"
+  },
+  {
+    "revision": "c714b38ea4126498b0a2",
+    "url": "./static/js/2.06e1f43a.chunk.js"
+  },
+  {
+    "revision": "a9e2914a001214a89904da6e6fdabfd0",
+    "url": "./static/js/2.06e1f43a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdaf0707dc666f584d9d",
+    "url": "./static/js/main.ede1dee9.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  },
+  {
+    "revision": "79148a43587cbe5c43d24fa894a78ba3",
+    "url": "./static/media/markers-soft.79148a43.png"
+  },
+  {
+    "revision": "0f2c10250081cf1bd9a4d2250b80dba6",
+    "url": "./static/media/markers-soft@2x.0f2c1025.png"
+  }
+]);
