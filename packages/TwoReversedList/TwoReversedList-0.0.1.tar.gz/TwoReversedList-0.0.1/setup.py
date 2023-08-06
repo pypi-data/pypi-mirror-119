9c2034c9617265b80dd543d3e00461188b8c397a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:33 +0000

    Add package TwoReversedList/0.0.1/TwoReversedList-0.0.1.tar.gz

---
+++ packages/TwoReversedList/TwoReversedList-0.0.1.tar.gz/TwoReversedList-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "A module to reverse lists"
+
+# Setting up
+setup(
+    name="TwoReversedList",
+    version=VERSION,
+    author="Sahand Pourjavad",
+    author_email="<sahndpourjavad@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    keywords=["python", "Lists"],
+)
