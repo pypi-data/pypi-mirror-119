5403b5516c448b088a5c5db94b822fe5c7e0702b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:23 +0000

    Add package simpleSound/1.0.0b0/simpleSound-1.0.0b0.tar.gz

---
+++ packages/simpleSound/simpleSound-1.0.0b0.tar.gz/simpleSound-1.0.0b0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name = "simpleSound", version = "1.0.0b", author = "nekumelon", author_email = "nekumelon@gmail.com", packages = find_packages(), description = "A very very very simple sound playing library for windows.", url = "https://github.com/nekumelon/simpleSound", license = "MIT", install_requires = [""]);
