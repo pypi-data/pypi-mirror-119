b7890e6066b9888b9ab8e8468d15688f5bf4a09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:31 +0000

    Add package 2ppy/0.1.8/2ppy-0.1.8.tar.gz

---
+++ packages/2ppy/2ppy-0.1.8.tar.gz/2ppy-0.1.8/tuprolog/core/exception.py
@@ -0,0 +1,10 @@
+import logging
+import jpype
+import jpype.imports
+
+from it.unibo.tuprolog.core.exception import TuPrologException
+from it.unibo.tuprolog.core.exception import SubstitutionException
+from it.unibo.tuprolog.core.exception import SubstitutionApplicationException
+
+logging.debug("Loaded JVM classes from it.unibo.tuprolog.core.exception.*")
+
