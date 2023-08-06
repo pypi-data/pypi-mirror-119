9789e0ecaad295134f5577e4c5bb864e671259ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:29 +0000

    Add package pystyle/0.1/pystyle-0.1-py3-none-any.whl

---
+++ packages/pystyle/pystyle-0.1-py3-none-any.whl/pystyle/__init__.py
@@ -0,0 +1,11 @@
+# made by billythegoat356 and loTus01
+
+# https://github.com/billythegoat356 https://github.com/loTus04
+
+# Version : 0.1
+
+# <3
+
+
+import pyfade as fade
+import pycenter as center
