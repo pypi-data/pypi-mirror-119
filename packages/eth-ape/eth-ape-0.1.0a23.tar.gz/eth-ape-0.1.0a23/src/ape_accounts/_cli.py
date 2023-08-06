f6933fe8d932b8864775e1a7c35d4e0778fd53e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:18 +0000

    Add package eth-ape/0.1.0a23/eth-ape-0.1.0a23.tar.gz

---
+++ packages/eth-ape/eth-ape-0.1.0a23.tar.gz/eth-ape-0.1.0a23/src/ape_accounts/_cli.py
@@ -0,0 +1,115 @@
+import json
+from typing import List
+
+import click
+from eth_account import Account as EthAccount  # type: ignore
+from eth_utils import to_bytes
+
+from ape import accounts
+from ape.exceptions import AliasAlreadyInUseError
+from ape.utils import Abort, notify
+
+# NOTE: Must used the instantiated version of `AccountsContainer` in `accounts`
+container = accounts.containers["accounts"]
+
+
+class Alias(click.Choice):
+    """Wraps ``click.Choice`` to load account aliases for the active project at runtime."""
+
+    name = "alias"
+
+    def __init__(self):
+        # NOTE: we purposely skip the constructor of `Choice`
+        self.case_sensitive = False
+
+    @property
+    def choices(self) -> List[str]:  # type: ignore
+        # NOTE: This is a hack to lazy-load the aliases so CLI invocation works properly
+        return list(accounts.aliases)
+
+
+@click.group(short_help="Manage local accounts")
+def cli():
+    """
+    Command-line helper for managing local accounts. You can unlock local accounts from
+    scripts or the console using the accounts.load() method.
+    """
+
+
+# Different name because `list` is a keyword
+@cli.command(name="list", short_help="List available accounts")
+def _list():
+    if len(accounts) == 0:
+        notify("WARNING", "No accounts found.")
+        return
+
+    elif len(accounts) > 1:
+        click.echo(f"Found {len(accounts)} accounts:")
+
+    else:
+        click.echo("Found 1 account:")
+
+    for account in accounts:
+        alias_display = f" (alias: '{account.alias}')" if account.alias else ""
+        click.echo(f"  {account.address}{alias_display}")
+
+
+@cli.command(short_help="Create a new keyfile account with a random private key")
+@click.argument("alias")
+def generate(alias):
+    if alias in accounts.aliases:
+        raise AliasAlreadyInUseError(alias)
+
+    path = container.data_folder.joinpath(f"{alias}.json")
+    extra_entropy = click.prompt(
+        "Add extra entropy for key generation...",
+        hide_input=True,
+    )
+    account = EthAccount.create(extra_entropy)
+    passphrase = click.prompt(
+        "Create Passphrase",
+        hide_input=True,
+        confirmation_prompt=True,
+    )
+    path.write_text(json.dumps(EthAccount.encrypt(account.key, passphrase)))
+
+    notify("SUCCESS", f"A new account '{account.address}' has been added with the id '{alias}'")
+
+
+# Different name because `import` is a keyword
+@cli.command(name="import", short_help="Add a new keyfile account by entering a private key")
+@click.argument("alias")
+def _import(alias):
+    if alias in accounts.aliases:
+        raise AliasAlreadyInUseError(alias)
+
+    path = container.data_folder.joinpath(f"{alias}.json")
+    key = click.prompt("Enter Private Key", hide_input=True)
+    try:
+        account = EthAccount.from_key(to_bytes(hexstr=key))
+    except Exception as error:
+        raise Abort(f"Key can't be imported: {error}")
+    passphrase = click.prompt(
+        "Create Passphrase",
+        hide_input=True,
+        confirmation_prompt=True,
+    )
+    path.write_text(json.dumps(EthAccount.encrypt(account.key, passphrase)))
+
+    notify("SUCCESS", f"A new account '{account.address}' has been added with the id '{alias}'")
+
+
+@cli.command(short_help="Change the password of an existing account")
+@click.argument("alias", type=Alias())
+def change_password(alias):
+    account = accounts.load(alias)
+    account.change_password()
+    notify("SUCCESS", f"Password has been changed for account '{alias}'")
+
+
+@cli.command(short_help="Delete an existing account")
+@click.argument("alias", type=Alias())
+def delete(alias):
+    account = accounts.load(alias)
+    account.delete()
+    notify("SUCCESS", f"Account '{alias}' has been deleted")
---
+++ packages/eth-ape/eth-ape-0.1.0a23.tar.gz/eth-ape-0.1.0a23/src/ape_compile/_cli.py
@@ -0,0 +1,103 @@
+from itertools import chain
+from pathlib import Path
+
+import click
+
+from ape.utils import notify
+
+flatten = chain.from_iterable
+
+
+@click.command(short_help="Compile select contract source files")
+@click.argument(
+    "filepaths",
+    nargs=-1,
+    type=click.Path(exists=True, path_type=Path),
+)
+@click.option(
+    "-f",
+    "--force",
+    "use_cache",
+    flag_value=False,
+    default=True,
+    is_flag=True,
+    help="Force recompiling selected contracts",
+)
+@click.option(
+    "-s",
+    "--size",
+    "display_size",
+    default=False,
+    is_flag=True,
+    help="Show deployment bytecode size for all contracts",
+)
+def cli(filepaths, use_cache, display_size):
+    """
+    Compiles the manifest for this project and saves the results
+    back to the manifest.
+
+    Note that ape automatically recompiles any changed contracts each time
+    a project is loaded. You do not have to manually trigger a recompile.
+    """
+    # NOTE: Lazy load so that testing works properly
+    from ape import project
+
+    # Expand source tree based on selection
+    if not filepaths:
+        if not (project.path / "contracts").exists():
+            notify("WARNING", "No 'contracts/' directory detected")
+            return
+
+        # If no paths are specified, use all local project sources
+        contract_filepaths = project.sources
+
+    else:
+        # Expand any folder paths
+        expanded_filepaths = flatten([d.rglob("*.*") if d.is_dir() else [d] for d in filepaths])
+        # Filter by what's in our project's source tree
+        # NOTE: Make the paths absolute like `project.sources`
+        contract_filepaths = [
+            c.resolve() for c in expanded_filepaths if c.resolve() in project.sources
+        ]
+
+    if not contract_filepaths:
+        if filepaths:
+            selected_paths = "', '".join(str(p.resolve()) for p in filepaths)
+
+        else:
+            selected_paths = str(project.path / "contracts")
+
+        extensions = ", ".join(set(f.suffix for f in selected_paths))
+        notify("WARNING", f"No compilers detected for the following extensions: {extensions}")
+        return
+
+    # TODO: only compile selected contracts
+    contract_types = project.load_contracts(use_cache)
+
+    # Display bytecode size for *all* contract types (not just ones we compiled)
+    if display_size:
+        codesize = []
+        for contract in contract_types.values():
+            if not contract.deploymentBytecode:
+                continue  # Skip if not bytecode to display
+
+            bytecode = contract.deploymentBytecode.bytecode
+
+            if bytecode:
+                codesize.append((contract.contractName, len(bytecode) // 2))
+
+        if not codesize:
+            notify("INFO", "No contracts with bytecode to display")
+            return
+
+        click.echo()
+        click.echo("============ Deployment Bytecode Sizes ============")
+        indent = max(len(i[0]) for i in codesize)
+        for name, size in sorted(codesize, key=lambda k: k[1], reverse=True):
+            pct = size / 24577
+            # pct_color = color(next((i[1] for i in CODESIZE_COLORS if pct >= i[0]), ""))
+            # TODO Get colors fixed for bytecode size output
+            # click.echo(f"  {name:<{indent}}  -  {size:>6,}B  ({pct_color}{pct:.2%}{color})")
+            click.echo(f"  {name:<{indent}}  -  {size:>6,}B  ({pct:.2%})")
+
+        click.echo()
