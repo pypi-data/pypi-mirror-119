9addd2a88c662191e8212ef43c248e099f57c7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:51 +0000

    Add package pretty-tables/1.3.0/pretty-tables-1.3.0.tar.gz

---
+++ packages/pretty-tables/pretty-tables-1.3.0.tar.gz/pretty-tables-1.3.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+DEV_REQUIREMENTS = [
+    'coveralls == 3.*',
+    'flake8',
+    'pytest == 6.*',
+    'pytest-cov == 2.*',
+]
+
+setuptools.setup(
+    name='pretty-tables',
+    version='1.3.0',
+    description='Create pretty tables from headers and rows, perfect for console output.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/justintime50/pretty-tables',
+    author='Justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        'dev': DEV_REQUIREMENTS,
+    },
+    python_requires='>=3.7',
+)
