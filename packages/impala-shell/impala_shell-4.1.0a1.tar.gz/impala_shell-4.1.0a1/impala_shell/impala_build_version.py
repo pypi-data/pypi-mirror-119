5e726850e86105e4d1ee391c64973e996f10dcc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:44 +0000

    Add package impala-shell/4.1.0a1/impala_shell-4.1.0a1.tar.gz

---
+++ packages/impala-shell/impala_shell-4.1.0a1.tar.gz/impala_shell-4.1.0a1/impala_shell/impala_build_version.py
@@ -0,0 +1,26 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+def get_version():
+  return "4.1.0a1"
+
+def get_git_hash():
+  return "dc5af921cce6eb08ef32a10a15a1e998b967c129"
+
+def get_build_date():
+  return "Fri Sep  3 22:55:31 PDT 2021"
