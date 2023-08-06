0ba45b0f78e87ee23cac7a554aa2c89dffeeb95f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:04 +0000

    Add package purePyStatistics/0.1.5/purePyStatistics-0.1.5.tar.gz

---
+++ packages/purePyStatistics/purePyStatistics-0.1.5.tar.gz/purePyStatistics-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+version = input('What is the new version: ')
+setup(
+    name = 'purePyStatistics',
+    packages = ['programfiles'],
+    version = version,
+    license='MIT',
+    description = 'Basic Functions for Statistics',
+    author = 'Isaac To',
+    author_email = 'isaacto3890@gmail.com',
+    url = 'https://github.com/chisaku-dev/pyDataStats',
+    download_url = f'https://github.com/chisaku-dev/pyDataStats/archive/refs/tags/{version}.tar.gz',
+    keywords = ['statistics', 'data', 'probability'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
