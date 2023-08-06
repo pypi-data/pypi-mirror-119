d07bb525c72a43b1d11c19ee81b112d935ef3b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:42 +0000

    Add package static-data-dp/0.1.0/static_data_dp-0.1.0.tar.gz

---
+++ packages/static-data-dp/static_data_dp-0.1.0.tar.gz/static_data_dp-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="static_data_dp",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Library to easily handles ddragon ressources files for League of Legends, origined by Canisback, modified by dp0973",
+    author="dp0973",
+    author_email="yeong0973@gmail.com",
+    url="https://github.com/dp0973/static-data",
+    keywords=["Riot API", "python", "static-data"],
+    classifiers=[],
+    install_requires=["requests", "asyncio", "aiohttp"],
+)
