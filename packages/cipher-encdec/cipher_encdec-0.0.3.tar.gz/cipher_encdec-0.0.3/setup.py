380ba3adefa0a0b24766c9731cc61891c52f337b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:07 +0000

    Add package cipher-encdec/0.0.3/cipher_encdec-0.0.3.tar.gz

---
+++ packages/cipher-encdec/cipher_encdec-0.0.3.tar.gz/cipher_encdec-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Encodes and Decodes your Data'
+LONG_DESCRIPTION = 'A package that allows to Encode and Decode your Data the way you want.'
+
+# Setting up
+setup(
+    name="cipher_encdec",
+    version=VERSION,
+    author="Cipher_Secure",
+    # author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'cipher'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
