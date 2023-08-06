89eb62d4b24d69a57059dc238577840988a880a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:51 +0000

    Add package aws-ec2-pricing/0.1.8/aws-ec2-pricing-0.1.8.tar.gz

---
+++ packages/aws-ec2-pricing/aws-ec2-pricing-0.1.8.tar.gz/aws-ec2-pricing-0.1.8/ec2pricing/__init__.py
@@ -0,0 +1,5 @@
+from ec2pricing.main import main as cli, __version__
+
+
+if __name__ == "__main__":
+    cli()
---
+++ packages/aws-ec2-pricing/aws-ec2-pricing-0.1.8.tar.gz/aws-ec2-pricing-0.1.8/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ec2pricing']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.18.40,<2.0.0', 'click>=8.0.1,<9.0.0', 'prettytable>=2.2.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['ec2pricing = ec2pricing:cli']}
+
+setup_kwargs = {
+    'name': 'aws-ec2-pricing',
+    'version': '0.1.8',
+    'description': 'CLI tool to build SQLite table from AWS EC2 prices.',
+    'long_description': None,
+    'author': 'vduseev',
+    'author_email': 'vagiz@duseev.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
