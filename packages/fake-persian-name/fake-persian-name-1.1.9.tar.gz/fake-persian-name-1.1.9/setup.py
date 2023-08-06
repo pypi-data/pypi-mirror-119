b5d1123856018a4fb5a81d965ad409e54af8696c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:31 +0000

    Add package fake-persian-name/1.1.9/fake-persian-name-1.1.9.tar.gz

---
+++ packages/fake-persian-name/fake-persian-name-1.1.9.tar.gz/fake-persian-name-1.1.9/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+with open('./README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+  name='fake-persian-name',
+  packages=['fake_persian_name'],
+  version='1.1.9',
+  license='MIT',
+  description='A small Python library to generate fake Persian name.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  author='Erfan Rahmati',
+  author_email='ErfanRht2005@gmail.com',
+  url='https://github.com/ErfanRht/fake-persian-name',
+  download_url='https://github.com/ErfanRht/fake-persian-name/blob/master/dist/fake-persian-name-1.1.9.tar.gz',
+  keywords=['fake', 'persian', 'name', 'persian name', 'fake name', 'fake persian name'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+  install_requires=[
+    "requests"
+  ],
+  project_urls={
+        'Documentation': 'https://requests.readthedocs.io',
+        'Source': 'https://pypi.org/project/fake-persian-name/',
+    },
+  include_package_data=True,
+)
