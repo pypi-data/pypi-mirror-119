d917616a474302fe587e7f4ab5ab8dcdd7bebaf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:38 +0000

    Add package dagster-cloud/0.12.10/dagster_cloud-0.12.10.tar.gz

---
+++ packages/dagster-cloud/dagster_cloud-0.12.10.tar.gz/dagster_cloud-0.12.10/dagster_cloud/cli/__init__.py
@@ -0,0 +1,23 @@
+import click
+from dagster_cloud.daemon.cli import agent_cli
+from dagster_cloud.workspace.cli import workspace_cli
+
+
+def create_dagster_cloud_cli():
+    commands = {
+        "agent": agent_cli,
+        "workspace": workspace_cli,
+    }
+
+    @click.group(commands=commands)
+    def group():
+        "CLI tools for working with dagster-cloud."
+
+    return group
+
+
+cli = create_dagster_cloud_cli()
+
+
+def main():
+    cli(obj={})  # pylint:disable=E1123
