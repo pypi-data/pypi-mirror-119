38738a840a9848ece8b7c28325199166b38795ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:35 +0000

    Add package tempath/0.1.1/tempath-0.1.1-py3.9.egg

---
+++ packages/tempath/tempath-0.1.1-py3.9.egg/tempath/__init__.py
@@ -0,0 +1,5 @@
+from tempath.tempfiles import autofile, autodir, fillfile, filldir
+
+__version__ = "0.1.1"
+__author__ = "alexpdev"
+__all__ = ["autofile", "autodir", "fillfile", "filldir", "rmpath"]
