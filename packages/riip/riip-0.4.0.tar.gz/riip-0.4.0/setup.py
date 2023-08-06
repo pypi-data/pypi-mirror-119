5e7a6200dc1bc81b5ef5f8dbf0e970c4e3b1e74b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package riip/0.4.0/riip-0.4.0.tar.gz

---
+++ packages/riip/riip-0.4.0.tar.gz/riip-0.4.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+
+def get_install_requires():
+    with open("requirements.txt", "r") as f:
+        return [line.strip() for line in f.readlines() if not line.startswith("-")]
+
+
+setup(
+    name="riip",
+    version="0.4.0",
+    url="https://github.com/mnishida/RII_Pandas",
+    license="MIT",
+    author="Munehiro Nishida",
+    author_email="mnishida@hiroshima-u.ac.jp",
+    description="Python 3 + Pandas wrapper for the refractiveindex.info database",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=get_install_requires(),
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering",
+    ],
+)
