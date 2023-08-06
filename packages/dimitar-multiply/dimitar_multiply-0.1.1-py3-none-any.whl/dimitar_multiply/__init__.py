a1ab9d61b53c570075f94a2cdafb71f45b101381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:13 +0000

    Add package dimitar-multiply/0.1.1/dimitar_multiply-0.1.1-py3-none-any.whl

---
+++ packages/dimitar-multiply/dimitar_multiply-0.1.1-py3-none-any.whl/dimitar_multiply/__init__.py
@@ -0,0 +1,3 @@
+# This line of code will allow shorter imports
+# Order of imports is important, especially if some classes need parent classes in order to run
+from dimitar_multiply.multiplication import Multiplication
