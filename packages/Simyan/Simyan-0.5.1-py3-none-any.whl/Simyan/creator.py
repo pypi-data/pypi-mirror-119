b06cc84a3d2226ee4660f1816ac4390d68871f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:17 +0000

    Add package Simyan/0.5.1/Simyan-0.5.1-py3-none-any.whl

---
+++ packages/Simyan/Simyan-0.5.1-py3-none-any.whl/Simyan/creator.py
@@ -0,0 +1,51 @@
+from marshmallow import EXCLUDE, Schema, fields, post_load, pre_load
+
+from Simyan.generic_entries import GenericEntrySchema, ImageEntrySchema
+
+
+class Creator:
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+
+class CreatorSchema(Schema):
+    aliases = fields.Str(allow_none=True)
+    api_url = fields.Url(data_key="api_detail_url")
+    country = fields.Str()
+    created_characters = fields.Nested(GenericEntrySchema, many=True)
+    date_added = fields.DateTime()
+    date_last_updated = fields.DateTime()
+    date_of_birth = fields.Date(data_key="birth", allow_none=True)
+    date_of_death = fields.Date(format="%Y-%m-%d %H:%M:%S.%f", data_key="death", allow_none=True)
+    description = fields.Str()
+    email = fields.Str(allow_none=True)
+    gender = fields.Int()
+    hometown = fields.Str(allow_none=True)
+    id = fields.Int()
+    image = fields.Nested(ImageEntrySchema)
+    issue_count = fields.Int(data_key="count_of_isssue_appearances", allow_none=True)
+    issues = fields.Nested(GenericEntrySchema, many=True)
+    name = fields.Str()
+    site_url = fields.Url(data_key="site_detail_url")
+    story_arcs = fields.Nested(GenericEntrySchema, data_key="story_arc_credits", many=True)
+    summary = fields.Str(data_key="deck", allow_none=True)
+    volumes = fields.Nested(GenericEntrySchema, data_key="volume_credits", many=True)
+    website = fields.Str(allow_none=True)
+
+    class Meta:
+        unknown = EXCLUDE
+        dateformat = "%Y-%m-%d %H:%M:%S"
+
+    @pre_load
+    def process_input(self, data, **kwargs):
+        new_data = data
+
+        if "death" in new_data and new_data["death"] is not None:
+            new_data["death"] = new_data["death"]["date"]
+
+        return new_data
+
+    @post_load
+    def make_object(self, data, **kwargs) -> Creator:
+        return Creator(**data)
---
+++ packages/Simyan/Simyan-0.5.1-py3-none-any.whl/Simyan/sqlite_cache.py
@@ -0,0 +1,55 @@
+import json
+import sqlite3
+from datetime import datetime, timedelta
+from typing import Any, Dict, Optional
+
+
+class SqliteCache:
+    def __init__(self, name: str = "Simyan-Cache.sqlite", expiry: Optional[int] = 14):
+        self.expiry = expiry
+        self.con = sqlite3.connect(name)
+        self.cur = self.con.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS queries (query, response, expiry);")
+        self.delete()
+
+    def select(self, query: str) -> Dict[str, Any]:
+        if self.expiry:
+            self.cur.execute(
+                "SELECT response FROM queries WHERE query = ? AND expiry > ?;",
+                (query, datetime.now().strftime("%Y-%m-%d")),
+            )
+        else:
+            self.cur.execute(
+                "SELECT response FROM queries WHERE query = ?;",
+                (query,),
+            )
+        result = self.cur.fetchone()
+        if result:
+            return json.loads(result[0])
+        return {}
+
+    def get(self, key: str) -> Optional[Dict[str, Any]]:
+        return self.select(query=key) or None
+
+    def insert(self, query: str, response: str):
+        if self.expiry:
+            expiry = datetime.now() + timedelta(days=self.expiry)
+        else:
+            expiry = datetime.now()
+        self.cur.execute(
+            "INSERT INTO queries(query, response, expiry) VALUES(?, ?, ?);",
+            (query, json.dumps(response), expiry.strftime("%Y-%m-%d")),
+        )
+        self.con.commit()
+
+    def store(self, key: str, value: str):
+        return self.insert(query=key, response=value)
+
+    def delete(self):
+        if not self.expiry:
+            return
+        self.cur.execute(
+            "DELETE FROM queries WHERE expiry < ?;",
+            (datetime.now().strftime("%Y-%m-%d"),),
+        )
+        self.con.commit()
