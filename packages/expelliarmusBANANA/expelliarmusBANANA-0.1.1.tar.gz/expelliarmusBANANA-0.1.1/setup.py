ee816f21c96eeabcac5074051acde76a911c03a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:03 +0000

    Add package expelliarmusBANANA/0.1.1/expelliarmusBANANA-0.1.1.tar.gz

---
+++ packages/expelliarmusBANANA/expelliarmusBANANA-0.1.1.tar.gz/expelliarmusBANANA-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='expelliarmusBANANA',
+      version='0.1.1',  # Development release
+      description='A Python Spell to apply data handling and segmentation magic to a dataframe.',
+      url='https://github.com/nalishamen/expelliarmusBANANA',
+      author='Nalisha Men',
+      author_email='nalisha.m@student.ie.edu',
+      license='MIT',
+          packages=['expelliarmusBANANA'],
+      zip_safe=False)
