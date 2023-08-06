cbdb749da494f4dbc635fb6f25582ffa1e0520d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:17 +0000

    Add package usefulfunctionsbyjoci/0.0.3/usefulfunctionsbyjoci-0.0.3-py3.8.egg

---
+++ packages/usefulfunctionsbyjoci/usefulfunctionsbyjoci-0.0.3-py3.8.egg/usefulfunctionsbyjoci/list_items.py
@@ -0,0 +1,16 @@
+
+def list_printer(list_1,list_2=None):
+		"""
+		list items from a list
+		
+		"""
+		if list_2:
+			for list_items2 in list_2:
+					print(list_items2)
+				
+		else:
+			pass
+
+		for list_items in list_1:
+			print(list_items)
+	   
