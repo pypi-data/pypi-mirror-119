444f456a670bc88d40aa235e95415956f859d137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package endaq-calc/0.0.0a1/endaq-calc-0.0.0a1.tar.gz

---
+++ packages/endaq-calc/endaq-calc-0.0.0a1.tar.gz/endaq-calc-0.0.0a1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name='endaq-calc',
+        version='0.0.0a1',
+        author='Mide Technology',
+        author_email='help@mide.com',
+        packages=[],
+        package_dir={'endaq-calc': './endaq'},
+        install_requires=[],
+)
