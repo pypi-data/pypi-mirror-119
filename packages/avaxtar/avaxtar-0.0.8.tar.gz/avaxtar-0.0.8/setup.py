cda7a5a6dcd2bb37a2fd19f46bbb4b06cff883a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:00 +0000

    Add package avaxtar/0.0.8/avaxtar-0.0.8.tar.gz

---
+++ packages/avaxtar/avaxtar-0.0.8.tar.gz/avaxtar-0.0.8/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+#import subprocess
+
+classifiers = [
+	'Operating System :: OS Independent',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 3'
+	]
+
+setup(
+	name='avaxtar',
+	version='0.0.8',
+	description='Identify if a Twitter account displays anti-vaccine sentiment.',
+	py_modules=["Avaxtar",  "Avax_NN", "DF_from_DICT"],
+	#package_dir={'': 'avatar'},
+	author = 'Matheus Schmitz',
+	author_email = 'mschmitz@usc.edu',
+	packages = find_packages(),
+	#package_data = {'': ['*.pt', '*.joblib', '*.bin']},
+	#data_files = {'': ['*.pt', '*.joblib', '*.bin']},
+	include_package_data = True,
+	classifiers = classifiers,
+	install_requires=[
+          'numpy',
+          'pandas',
+          'torch',
+          'scikit-learn',
+          'tweepy',
+          'requests',
+          'nltk',
+          'tqdm',
+          'gdown',
+          #'sent2vec @ git+https://github.com/epfml/sent2vec.git@master#egg=sent2vec'
+          #'sent2vec==0.0.0'
+      ],
+     dependency_links = ['http://github.com/epfml/sent2vec/tarball/master#egg=sent2vec-0.0.0']
+	)
+
+#if name == "__main__":cd 
+#subprocess.run(["pip", "install", "git+https://github.com/epfml/sent2vec.git#egg=sent2vec"], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
