0c8f3840449545696b28c8cee1757ad62c993acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:28 +0000

    Add package ms2pip/3.7.1/ms2pip-3.7.1-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/ms2pip/ms2pip-3.7.1-cp38-cp38-macosx_10_9_x86_64.whl/ms2pip/exceptions.py
@@ -0,0 +1,42 @@
+class MS2PIPError(Exception):
+    pass
+
+
+class InvalidPeptideError(MS2PIPError):
+    pass
+
+
+class UnknownModificationError(ValueError):
+    pass
+
+
+class InvalidPEPRECError(Exception):
+    pass
+
+
+class NoValidPeptideSequencesError(Exception):
+    pass
+
+
+class UnknownOutputFormatError(ValueError):
+    pass
+
+
+class UnknownFragmentationMethodError(ValueError):
+    pass
+
+
+class MissingConfigurationError(Exception):
+    pass
+
+
+class FragmentationModelRequiredError(Exception):
+    pass
+
+
+class InvalidModificationFormattingError(Exception):
+    pass
+
+
+class InvalidAminoAcidError(Exception):
+    pass
