66a78b228efba85ad4968d9de66acbff9d60f0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:29 +0000

    Add package simpleSound/1.0.0rc0/simpleSound-1.0.0rc0.tar.gz

---
+++ packages/simpleSound/simpleSound-1.0.0rc0.tar.gz/simpleSound-1.0.0rc0/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name = "simpleSound", version = "1.0.0c", author = "nekumelon", author_email = "nekumelon@gmail.com", long_description_content_type = "text/markdown", long_description = "README.md", packages = find_packages(), description = "A very very very simple sound playing library for windows.", url = "https://github.com/nekumelon/simpleSound", license = "MIT", install_requires = [""]);
---
+++ packages/simpleSound/simpleSound-1.0.0rc0.tar.gz/simpleSound-1.0.0rc0/simpleSound/__init__.py
@@ -0,0 +1,11 @@
+import ctypes
+
+def windowsCommand(command):
+    buffer = ctypes.create_unicode_buffer(600);
+    ctypes.windll.winmm.mciSendStringW(command, buffer, 559, 0);
+
+class SimpleSound():
+    def play(self, fileName):
+        windowsCommand("open " + fileName);
+        windowsCommand("play " + fileName + " wait");
+        windowsCommand("close " + fileName);
