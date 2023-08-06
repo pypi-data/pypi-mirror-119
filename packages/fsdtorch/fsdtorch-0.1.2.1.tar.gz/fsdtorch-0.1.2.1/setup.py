3594bc8858e9a94e13f9f8199d4f313f4d3cb7e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:43 +0000

    Add package fsdtorch/0.1.2.1/fsdtorch-0.1.2.1.tar.gz

---
+++ packages/fsdtorch/fsdtorch-0.1.2.1.tar.gz/fsdtorch-0.1.2.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['fsdtorch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['gdown>=3.13.0,<4.0.0',
+ 'onnx>=1.10.1,<2.0.0',
+ 'onnxruntime-gpu>=1.8.1,<2.0.0',
+ 'opencv-contrib-python>=4.5.3,<5.0.0']
+
+setup_kwargs = {
+    'name': 'fsdtorch',
+    'version': '0.1.2.1',
+    'description': 'Face shape detector in pytorch that works with pretrained networks',
+    'long_description': None,
+    'author': 'Alper',
+    'author_email': 'itsc0508@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
