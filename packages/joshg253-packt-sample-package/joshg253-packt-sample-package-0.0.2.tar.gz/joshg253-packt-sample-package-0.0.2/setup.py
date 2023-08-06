a99df52b78ecbc960aa089ba4889e8bf947b3bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:07 +0000

    Add package joshg253-packt-sample-package/0.0.2/joshg253-packt-sample-package-0.0.2.tar.gz

---
+++ packages/joshg253-packt-sample-package/joshg253-packt-sample-package-0.0.2.tar.gz/joshg253-packt-sample-package-0.0.2/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="joshg253-packt-sample-package",
+    version="0.0.2",
+    author="joshg253",
+    author_email="joshg253@users.noreply.github.com",
+    description="joshg253 packt example package",
+    long_description="This is the longer description and will appear in the web.",
+    py_modules=["packt"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
