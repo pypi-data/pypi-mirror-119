757630966e98340f4f04bbfa688cabb363da6fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:03 +0000

    Add package GB-distributions-test1/0.0/GB_distributions_test1-0.0.tar.gz

---
+++ packages/GB-distributions-test1/GB_distributions_test1-0.0.tar.gz/GB_distributions_test1-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='GB_distributions_test1',
+      version='0.0',
+      description='Gaussian and Bionomial distributions',
+      packages=['GB_distributions_test1'],
+      author = 'Ayyoub Rezaeian',
+      author_email = 'ayyoub.rezaeian@gmail.com',
+      zip_safe=False)
