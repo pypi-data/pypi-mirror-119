addeca8d09090ea104490f6b71134463c65397ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:52 +0000

    Add package autofj/0.0.3/autofj-0.0.3-py3-none-any.whl

---
+++ packages/autofj/autofj-0.0.3-py3-none-any.whl/autofj/datasets.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+from os.path import dirname
+
+def load_data(name):
+    module_path = dirname(__file__)
+    if os.path.exists(os.path.join(module_path, "benchmark", name)):
+        left_table = pd.read_csv(os.path.join(module_path, "benchmark", name, "left.csv"))
+        right_table = pd.read_csv(os.path.join(module_path, "benchmark", name, "right.csv"))
+        gt_table = pd.read_csv(os.path.join(module_path, "benchmark", name, "gt.csv"))
+        return left_table, right_table, gt_table
+    else:
+        print(os.path.join(module_path, "benchmark", name))
+        raise Exception("Dataset {} does not exist.".format(name))
