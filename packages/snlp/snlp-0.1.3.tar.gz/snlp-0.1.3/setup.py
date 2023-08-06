75244da118f84464e97f88dd009b8221157e35a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:35 +0000

    Add package snlp/0.1.3/snlp-0.1.3.tar.gz

---
+++ packages/snlp/snlp-0.1.3.tar.gz/snlp-0.1.3/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+if os.path.exists("README.md"):
+    with open("README.md", "r") as f:
+        long_description = f.read()
+else:
+    long_description = "Description could not be found."
+
+if os.path.exists("requirements.txt"):
+    with open("requirements.txt") as f:
+        requirements = f.read().split("\n")
+else:
+    requirements = []
+
+setup(
+    name="snlp",
+    version='0.1.3',
+    author="meghdadFar",
+    author_email="meghdad.farahmand@gmail.com",
+    description="Statistical NLP",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/meghdadFar/snlp",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    scripts=['bin/downloads.py'],
+    python_requires='>=3.7',
+)
