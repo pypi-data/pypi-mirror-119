cc2f692e915f5d88b1f126a66997b988e25e5512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:59 +0000

    Add package saypy/0.0.2/saypy-0.0.2.tar.gz

---
+++ packages/saypy/saypy-0.0.2.tar.gz/saypy-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='saypy',
+    author='Matt Eads',
+    version='0.0.2',
+    description='A very pointless, yet very friendly Python package.',
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    tests_requires=['pytest'],
+    entry_points={
+        'console_scripts': ['saypy=saypy.__main__:main']
+    }
+)
