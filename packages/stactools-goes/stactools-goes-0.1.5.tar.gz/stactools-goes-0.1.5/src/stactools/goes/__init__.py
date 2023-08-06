c6fdb2681d3ad02ca4f82871ce5e1ce0d6fe63bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:49 +0000

    Add package stactools-goes/0.1.5/stactools-goes-0.1.5.tar.gz

---
+++ packages/stactools-goes/stactools-goes-0.1.5.tar.gz/stactools-goes-0.1.5/src/stactools/goes/__init__.py
@@ -0,0 +1,15 @@
+import stactools.core
+from .dataset import Dataset
+from .errors import CogifyError
+
+stactools.core.use_fsspec()
+
+
+def register_plugin(registry):
+    from . import commands
+    registry.register_subcommand(commands.create_goes_command)
+
+
+__all__ = [Dataset, CogifyError]
+__version__ = '0.1.5'
+"""Library version"""
