a2ab736314c4f1622323df9497b62b42098555cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:50 +0000

    Add package scumprogrammer/0.0.3/scumprogrammer-0.0.3.tar.gz

---
+++ packages/scumprogrammer/scumprogrammer-0.0.3.tar.gz/scumprogrammer-0.0.3/scumprogrammer/__main__.py
@@ -0,0 +1,19 @@
+# built-in
+# third party
+# local
+import VERSION
+import ScumConnector
+import InterfaceWeb
+import InterfaceCli
+
+#========================== classes =========================================
+
+#========================== main ==============================================
+
+def main():
+    scumConnector = ScumConnector.ScumConnector()
+    interfaceWeb  = InterfaceWeb.InterfaceWeb()
+    interfaceCli  = InterfaceCli.InterfaceCli()
+
+if __name__== "__main__":
+    main()
