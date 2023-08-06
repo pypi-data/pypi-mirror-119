26bb03aa17a41dfda714955264221fd702c7137f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:43 +0000

    Add package system-calls/5.15.3/system-calls-5.15.3.tar.gz

---
+++ packages/system-calls/system-calls-5.15.3.tar.gz/system-calls-5.15.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="system-calls",
+    version="5.15.3",
+    author="Marcin Juszkiewicz",
+    author_email="marcin-python@juszkiewicz.com.pl",
+    description="Python module to check for system call numbers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hrw/python-syscalls",
+    project_urls={
+        "Bug Tracker": "https://github.com/hrw/python-syscalls/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    packages=["system_calls", "system_calls/tables/"],
+    python_requires=">=3.6",
+    license_files=["LICENSE"],
+    scripts=["bin/syscall"],
+)
