01cdc11f586d09856d117e21aa77913b6fcf8ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:35 +0000

    Add package sixi-web/0.1.4/sixi-web-0.1.4.tar.gz

---
+++ packages/sixi-web/sixi-web-0.1.4.tar.gz/sixi-web-0.1.4/sixi_web/__init__.py
@@ -0,0 +1,9 @@
+"""Sixi webframework"""
+
+__version__ = "0.1.4"
+
+from .api import API
+from .middleware import Middleware
+from .orm import Column, Database, ForeignKey, Table  # noqa
+
+__all__ = ["API", "Middleware", "Database" "Table", "Column", "ForeignKey"]
