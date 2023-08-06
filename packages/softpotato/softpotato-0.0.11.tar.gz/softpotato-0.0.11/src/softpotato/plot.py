d53394b68ed19dd101dab1ad39bc424db37f9cfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:52 +0000

    Add package softpotato/0.0.11/softpotato-0.0.11.tar.gz

---
+++ packages/softpotato/softpotato-0.0.11.tar.gz/softpotato-0.0.11/src/softpotato/plot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+def format(xlab, ylab, show):
+    plt.xticks(fontsize=14)
+    plt.yticks(fontsize=14)
+    plt.xlabel(xlab, fontsize=18)
+    plt.ylabel(ylab, fontsize=18)
+    plt.grid()
+    plt.tight_layout()
+    if show:
+        plt.show()
+
+def plot(x, y, xlab='$E$ / V', ylab='$i$ / A', fig=1, show=False):
+    plt.figure(fig)
+    plt.plot(x, y)
+    format_plot(xlab, ylab, show)
