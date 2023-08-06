9bfd309f033f6a8eaffda672bd046411ff8f3aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:32 +0000

    Add package gutils/0.1.0/gutils-0.1.0-py3-none-any.whl

---
+++ packages/gutils/gutils-0.1.0-py3-none-any.whl/gutils/components.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+import logging
+
+
+class LogLevel(Enum):
+    '''
+    What the stdlib did not provide!
+    '''
+    INFO = logging.INFO
+    DEBUG = logging.DEBUG
+    ERROR = logging.ERROR
+    NOTSET = logging.NOTSET
+    WARNING = logging.WARNING
+    CRITICAL = logging.CRITICAL
+
+    def __str__(self):
+        return self.name
