dadabab9b7fbfa0247326b01758c8a418a8a5ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:18 +0000

    Add package demo-energy-consumption/0.2.2/demo_energy_consumption-0.2.2-py2.py3-none-any.whl

---
+++ packages/demo-energy-consumption/demo_energy_consumption-0.2.2-py2.py3-none-any.whl/demo_energy_consumption/__init__.py
@@ -0,0 +1,18 @@
+"""
+Demo Energy Consumption for Python
+============================
+The demo energy consumption package is a basic demonstration of a Python
+package leveraging Vantage.
+It is a package example that provides a Python interface to manipulate data
+stored in Vantage. It pushes downs to Vantage the workload so that the user
+can benefit from the MPP engine and the data preparation capabilities provi-
+ded by Teradata Vantage in their client running Python codes.
+"""
+__version__ = '0.2.2'
+__author__ = """Denis Molin"""
+__email__ = 'denis.molin@teradata.com'
+__version__ = '0.2.2'
+
+from demo_energy_consumption.demo_energy_consumption import getmydata
+
+
