017193a1178d8b5cfe05b7a9eafe46ce8d67b40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:30 +0000

    Add package airflow-customs-by-novigi/1.0.10/airflow_customs_by_novigi-1.0.10.tar.gz

---
+++ packages/airflow-customs-by-novigi/airflow_customs_by_novigi-1.0.10.tar.gz/airflow_customs_by_novigi-1.0.10/setup.py
@@ -0,0 +1,20 @@
+
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='airflow_customs_by_novigi',
+    version="1.0.10",
+    description='Novigi Custom Airflow operators, hooks and plugins',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT', 
+    packages=find_packages(exclude=['tests']),
+    install_requires=['requests','jsonpath_ng','pandas', 'pyodbc', 'psycopg2'],
+    setup_requires=['setuptools', 'wheel'],
+    author='Novigi',
+    author_email='integration@novigi.com.au',
+)
