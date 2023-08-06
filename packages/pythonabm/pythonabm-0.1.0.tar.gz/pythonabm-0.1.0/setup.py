90b68f85adcce3ced7c5da7ecfeca38edb14d0c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:45 +0000

    Add package pythonabm/0.1.0/pythonabm-0.1.0.tar.gz

---
+++ packages/pythonabm/pythonabm-0.1.0.tar.gz/pythonabm-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="pythonabm",
+    version="0.1.0",
+    author="Jack Toppen",
+    author_email="jtoppen3@gatech.edu",
+    description="ABM framework for Python",
+    packages=["pythonabm"],
+    python_requires=">=3.7",
+    install_requires=[
+        "numpy",
+        "numba",
+        "opencv-python",
+        "psutil",
+        "python-igraph",
+        "pyyaml",
+        "scipy",
+    ],
+)
