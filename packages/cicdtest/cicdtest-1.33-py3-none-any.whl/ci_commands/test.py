cfadbe80de8fa842445dc171dfe81510ee2faa36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:54 +0000

    Add package cicdtest/1.33/cicdtest-1.33-py3-none-any.whl

---
+++ packages/cicdtest/cicdtest-1.33-py3-none-any.whl/ci_commands/test.py
@@ -0,0 +1,18 @@
+import pathlib
+import click
+import json
+
+path = pathlib.Path().resolve()
+
+@click.command()
+def test():
+    # with open(f"{path}/sample.txt","a") as file:
+    #     file.write("hello")
+
+    with open("info.txt") as file:
+        # info1 = json.load(file)
+        info = file.readlines().split("\n")
+        data = json.load(info)
+        print(data)
+        # print(info1) 
+
