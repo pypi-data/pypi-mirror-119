ab0ba5443a27341e9a484a5b440719429cb2782a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:55 +0000

    Add package NgroktoDuckDNS/0.0.3/NgroktoDuckDNS-0.0.3.tar.gz

---
+++ packages/NgroktoDuckDNS/NgroktoDuckDNS-0.0.3.tar.gz/NgroktoDuckDNS-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+__name__ = 'NgroktoDuckDNS'
+__description__ = 'Expose local services to the web through duckdns with ngrok.'
+__version__ = '0.0.3'
+__author__ = 'GrandMoff100'
+__author_email__ = 'nlarsen23.student@gmail.com'
+
+
+setup(
+    name=__name__,
+    description=__description__,
+    version=__version__,
+    author=__author__,
+    author_email=__author_email__,
+    packages=['ngroktoduckdns'],
+    install_requires=['click', 'pyngrok', 'requests'],
+    entry_points={
+        'console_scripts': 'ngroktoduckdns = ngroktoduckdns:cli'
+    }
+)
