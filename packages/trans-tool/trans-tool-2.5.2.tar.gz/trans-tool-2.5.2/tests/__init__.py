5bb6f25b774ee1b50c2490167625488edc41c94b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:25:09 +0000

    Add package trans-tool/2.5.2/trans-tool-2.5.2.tar.gz

---
+++ packages/trans-tool/trans-tool-2.5.2.tar.gz/trans-tool-2.5.2/tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+# trans-tool
+# The translation files checker and syncing tool.
+#
+# Copyright ©2021 Marcin Orlowski <mail [@] MarcinOrlowski.com>
+# https://github.com/MarcinOrlowski/trans-tool/
+#
+"""
