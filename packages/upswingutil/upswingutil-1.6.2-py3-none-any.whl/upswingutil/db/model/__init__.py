b32beabd032efd17878058b3300d6402050aeca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:51 +0000

    Add package upswingutil/1.6.2/upswingutil-1.6.2-py3-none-any.whl

---
+++ packages/upswingutil/upswingutil-1.6.2-py3-none-any.whl/upswingutil/db/model/__init__.py
@@ -0,0 +1,3 @@
+from .property import PropertyModel, PropertyAddressModel
+from .guest import ProfileIDObject, GuestsModel
+from .reservation import ReservationModel, Status, ReservationGuestInfo, ReservationGuestShort
