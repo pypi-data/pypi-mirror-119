77f45de39e4ece32e3d7897331311adecf310e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:42 +0000

    Add package CyroWebB/0.0.2/CyroWebB-0.0.2.tar.gz

---
+++ packages/CyroWebB/CyroWebB-0.0.2.tar.gz/CyroWebB-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CyroWebB",
+    version="0.0.2",
+    author="CyroCoders",
+    author_email="pypi@cyrocoders.ml",
+    description="Backend Framework To Go Together With CyroWebF Frontend Framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/CyroCoders/CyroWebB",
+    project_urls={
+        "Bug Tracker": "https://github.com/CyroCoders/CyroWebB/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3",
+    install_requires=[
+        'Jinja2==2.11.2',
+        'Brotli==1.0.9',
+        'WebOb==1.8.7',
+        'parse==1.19.0',
+    ]
+)
