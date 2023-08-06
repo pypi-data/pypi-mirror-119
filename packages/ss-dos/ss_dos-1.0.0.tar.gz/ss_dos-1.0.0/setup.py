954a36a51e39c7df2ee36ce61914745f2c9d9a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:51 +0000

    Add package ss-dos/1.0.0/ss_dos-1.0.0.tar.gz

---
+++ packages/ss-dos/ss_dos-1.0.0.tar.gz/ss_dos-1.0.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="ss_dos",
+    version="1.0.0",
+    author="enygames",
+    description="A small example package",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
