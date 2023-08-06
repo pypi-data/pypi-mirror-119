08480ac8f57085d6ece77eb6e07a5af9e965a7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:36 +0000

    Add package bokeh/2.4.0rc2/bokeh-2.4.0rc2.tar.gz

---
+++ packages/bokeh/bokeh-2.4.0rc2.tar.gz/bokeh-2.4.0rc2/bokeh/server/static/js/types/models/text/utils.d.ts
@@ -0,0 +1,5 @@
+import { TeX } from "./math_text";
+import { BaseText } from "./base_text";
+export declare function is_tex_string(text: unknown): boolean;
+export declare function tex_from_text_like(text: string | BaseText): TeX | null;
+//# sourceMappingURL=utils.d.ts.map
