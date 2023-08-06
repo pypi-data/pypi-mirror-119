df6d8d05d78f5b1b66de8bbe8f04699ae5a15b49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:20 +0000

    Add package HALsn/0.0.3/HALsn-0.0.3.tar.gz

---
+++ packages/HALsn/HALsn-0.0.3.tar.gz/HALsn-0.0.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Host for SharkNinja BDP Python APIs'
+
+# Setting up
+setup(
+    name="HALsn",
+    version=VERSION,
+    author="Mikhail Hyde",
+    author_email="<hyde.mikhail@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'pyserial', 'awscli', 'boto3'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+    ]
+)
