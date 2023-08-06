3999084ee7b8a3d24f12a96b7051094430e42679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:44 +0000

    Add package annofabcli/1.47.4/annofabcli-1.47.4.tar.gz

---
+++ packages/annofabcli/annofabcli-1.47.4.tar.gz/annofabcli-1.47.4/annofabcli/labor/subcommand_labor.py
@@ -0,0 +1,26 @@
+import argparse
+
+import annofabcli
+import annofabcli.common.cli
+import annofabcli.labor.list_worktime
+import annofabcli.labor.list_worktime_by_user
+
+
+def parse_args(parser: argparse.ArgumentParser):
+
+    subparsers = parser.add_subparsers(dest="subcommand_name")
+
+    # サブコマンドの定義
+    annofabcli.labor.list_worktime.add_parser(subparsers)
+    annofabcli.labor.list_worktime_by_user.add_parser(subparsers)
+
+
+def add_parser(subparsers: argparse._SubParsersAction):
+    subcommand_name = "labor"
+    subcommand_help = "労務管理関係のサブコマンド"
+    description = "労務管理関係のサブコマンド"
+
+    parser = annofabcli.common.cli.add_parser(
+        subparsers, subcommand_name, subcommand_help, description, is_subcommand=False
+    )
+    parse_args(parser)
