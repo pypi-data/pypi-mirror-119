2a7b357c9eb30896eebb40b75e6228e9813751f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:28 +0000

    Add package django-rgd-imagery/0.2.6.dev4/django_rgd_imagery-0.2.6.dev4-py3-none-any.whl

---
+++ packages/django-rgd-imagery/django_rgd_imagery-0.2.6.dev4-py3-none-any.whl/rgd_imagery/serializers/__init__.py
@@ -0,0 +1,21 @@
+from rgd import utility
+
+from .. import models
+from .base import ImageMetaSerializer, ImageSerializer, ImageSetSerializer
+from .processed import ProcessedImageGroupSerializer, ProcessedImageSerializer
+from .raster import RasterMetaSerializer, RasterSerializer
+from .stac import STACRasterFeatureSerializer
+
+utility.make_serializers(globals(), models)
+
+__all__ = [
+    'ImageSerializer',
+    'ImageMetaSerializer',
+    'ImageSetSerializer',
+    'ProcessedImageGroupSerializer',
+    'ProcessedImageSerializer',
+    'RasterMetaSerializer',
+    'RasterSerializer',
+    'RegionImageSerializer',
+    'STACRasterFeatureSerializer',
+]
---
+++ packages/django-rgd-imagery/django_rgd_imagery-0.2.6.dev4-py3-none-any.whl/rgd_imagery/serializers/stac/__init__.py
@@ -0,0 +1,9 @@
+from .feature import STACRasterFeatureSerializer
+from .featurecollection import STACRasterFeatureCollectionSerializer
+from .root import STACRootSerializer
+
+__all__ = [
+    'STACRasterFeatureSerializer',
+    'STACRasterFeatureCollectionSerializer',
+    'STACRootSerializer',
+]
