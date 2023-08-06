6816c11a5e5c9d11a67fe881bdd0af1fdfef92c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:32 +0000

    Add package chalice-http-toolkit/0.1.1/chalice-http-toolkit-0.1.1.tar.gz

---
+++ packages/chalice-http-toolkit/chalice-http-toolkit-0.1.1.tar.gz/chalice-http-toolkit-0.1.1/chalice_http_toolkit/response_with_binary.py
@@ -0,0 +1,16 @@
+from chalice import Response
+import os
+
+class ResponseWithBinary(Response):
+    """
+    Wrapper class to override isBase64Encoded behaviour in the Reponse class.
+    Setting isBase64Encoded is usually done to encode Binary content for API Gateway
+    so that it knows to decode it.
+    """
+    isBase64Encoded = False
+    isLocal = os.environ.get('STAGE', 'dev') == 'dev'
+    def to_dict(self, binary_types=None) -> dict:
+        response = super().to_dict(binary_types=binary_types)
+        if self.isBase64Encoded and not self.isLocal:
+            response['isBase64Encoded'] = True
+        return response
