db62a475dedbb7efe72c7c1d15eb212f650d9204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:29 +0000

    Add package pystyle/0.1/pystyle-0.1.tar.gz

---
+++ packages/pystyle/pystyle-0.1.tar.gz/pystyle-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+
+setup(
+    name="pystyle",
+    version='0.1',
+    license='Eclipse Public License 2.0',
+    authors=["billythegoat356", "loTus01"],
+    author_email="<billythegoat356356@gmail.com>",
+    description="heya",
+    long_description='Documentation: https://github.com/billythegoat356/pystyle',
+    keywords=['cli', 'fade', 'colors', 'terminal', 'tui'],
+    packages=['pystyle']
+)
