8d7bbea580fad64de1bfb17b0564ec0f54714c09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:17 +0000

    Add package aiohttp-ratelimiter/3.0.1/aiohttp-ratelimiter-3.0.1.tar.gz

---
+++ packages/aiohttp-ratelimiter/aiohttp-ratelimiter-3.0.1.tar.gz/aiohttp-ratelimiter-3.0.1/aiohttplimiter/decorators.py
@@ -0,0 +1,129 @@
+from functools import wraps
+import time
+from aiohttp import web
+from collections import defaultdict
+import json
+from typing import Callable, Awaitable, Union
+import asyncio
+from .utils import MemorySafeDict
+
+
+now = lambda: time.time()
+
+
+class RateLimitDecorator:
+    """
+    Decorator to ratelimit requests in the aiohttp.web framework
+    """
+    def __init__(self, last_reset: MemorySafeDict, num_calls: MemorySafeDict, keyfunc: Awaitable, ratelimit: str, exempt_ips: set = None, middleware_count: int = 0):
+        self.exempt_ips = exempt_ips or set()
+        calls, period = ratelimit.split("/")
+        self._calls = calls
+        calls = int(calls) + middleware_count
+        period = int(period)
+        self.period = period
+        self.keyfunc = keyfunc
+        self.calls = calls
+        self.last_reset = last_reset
+        self.num_calls = num_calls
+
+    def __call__(self, func):
+        @wraps(func)
+        async def wrapper(request):
+            self.func = func
+            func_key = id(func)
+            key = await self.keyfunc(request)
+
+            if self.last_reset.get(func_key) is None:
+                self.last_reset[func_key] = MemorySafeDict(default=now, main=self.last_reset)
+                self.last_reset.append_nested_iterable(self.last_reset[func_key])
+
+            if self.num_calls.get(func_key) is None:
+                self.num_calls[func_key] = MemorySafeDict(default=lambda: 0, main=self.num_calls)
+                self.num_calls.append_nested_iterable(self.num_calls[func_key])
+
+            # Checks if the user's IP is in the set of exempt IPs
+            if key in self.exempt_ips:
+                if asyncio.iscoroutinefunction(func):
+                    return await func(request)
+                return func(request)
+
+            # Checks if it is time to reset the number of calls
+            if await self.__period_remaining(request) <= 0:
+                self.num_calls[func_key][key] = 0
+                self.last_reset[func_key][key] = now()
+
+            # Increments the number of calls by 1
+            self.num_calls[func_key][key] += 1
+
+            # Returns a JSON response if the number of calls exceeds the max amount of calls
+            if self.num_calls[func_key][key] > self.calls:
+                response = json.dumps({"Rate limit exceeded": f'{self._calls} request(s) per {self.period} second(s)'})
+                return web.Response(text=response, content_type="application/json", status=429)
+
+            # Returns normal response if the user did not go over the ratelimit
+            if asyncio.iscoroutinefunction(func):
+                return await func(request)
+            return func(request)
+        return wrapper
+
+    async def __period_remaining(self, request):
+        """
+        Gets the ammount of time remaining until the number of calls resets
+        """
+        func_key = id(self.func)
+        key = await self.keyfunc(request)
+        elapsed = now() - self.last_reset[func_key][key]
+        return self.period - elapsed
+
+
+async def default_keyfunc(request):
+    """
+    Returns the user's IP
+    """
+    ip = request.headers.get(
+        "X-Forwarded-For") or request.remote or "127.0.0.1"
+    ip = ip.split(".")[0]
+    return ip
+
+class Limiter:
+    """
+    This should be used if you plan on having the same settings for each endpoint
+
+    ```
+    limiter = Limiter(keyfunc=your_keyfunc, exempt_ips={"192.168.1.345"})
+
+    @routes.get("/")
+    @limiter.limit("5/1")
+    def foo():
+        return web.Response(text="Hello World")
+    ```
+    """
+    def __init__(self, keyfunc: Awaitable, exempt_ips: set = None, middleware_count: int = 0, max_memory: Union[int, float] = 1):
+        self.exempt_ips = exempt_ips or set()
+        self.keyfunc = keyfunc
+        self.middleware_count = middleware_count
+        self.last_reset = MemorySafeDict(max_memory=max_memory/2)
+        self.num_calls = MemorySafeDict(max_memory=max_memory/2)
+
+    def limit(self, ratelimit: str, keyfunc: Awaitable = None, exempt_ips: set = None, middleware_count: int = None):
+        def wrapper(func: Callable):
+            _middleware_count = middleware_count or self.middleware_count
+            _exempt_ips = exempt_ips or self.exempt_ips
+            _keyfunc = keyfunc or self.keyfunc
+            return RateLimitDecorator(keyfunc=_keyfunc, ratelimit=ratelimit, exempt_ips=_exempt_ips, middleware_count=_middleware_count, num_calls=self.num_calls, last_reset=self.last_reset)(func)
+        return wrapper
+
+"""
+app = web.Application()
+routes = web.RouteTableDef()
+
+# This endpoint can only be requested one time per second per IP address.
+@routes.get("/")
+@RateLimitDecorator(ratelimit="1/1", keyfunc=default_keyfunc)
+async def test(request):
+    return web.Response(text="test")
+
+app.add_routes(routes)
+web.run_app(app)
+"""
---
+++ packages/aiohttp-ratelimiter/aiohttp-ratelimiter-3.0.1.tar.gz/aiohttp-ratelimiter-3.0.1/aiohttplimiter/utils.py
@@ -0,0 +1,156 @@
+from asyncio.coroutines import iscoroutinefunction
+from collections import defaultdict
+from typing import Callable, Any, Union
+from asyncio import iscoroutinefunction
+from sys import getsizeof
+
+
+none = lambda: None
+IntOrFloat = Union[int, float]
+
+
+class MemorySafeDict:
+    def __init__(self, dictionary: dict = None, default: Callable = None, max_memory: IntOrFloat = None, main = None) -> None:
+        """
+        MemorySafeDict acts as a defaultdict, but it allows you to specify the max ammount of memory it can use and it will never throw a MemoryError
+        """
+        self.main: MemorySafeDict = main
+        self.max_memory = max_memory * 1024**3 if max_memory is not None else None
+        self.default = default or none
+        self.data = dictionary or {}
+        self.nested_dicts = list()
+
+    def set_max_memory(self, size: IntOrFloat):
+        self.max_memory = size
+
+    def append_nested_iterable(self, dictionary: dict) -> None:
+        self.nested_dicts.append(dictionary)
+
+
+    def __missing__(self, key) -> None:
+        try:
+            self.data[key] = self.default()
+            if self.max_memory is not None:
+                if self.getsize() >= self.max_memory or self.getsize() >= self.main.max_memory:
+                    raise MemoryError
+        except MemoryError:
+            self.data.clear()
+            self.data[key] = self.default()
+
+    def __getitem__(self, key) -> Any:
+        if key not in self.data:
+            try:
+                self.data[key] = self.default()
+                if self.max_memory is not None:
+                    if self.getsize() >= self.max_memory:
+                        raise MemoryError
+            except MemoryError:
+                self.data.clear()
+                self.data[key] = self.default()
+
+            if self.main is None:
+                    return
+            if self.main.max_memory is not None:
+                if self.main.getsize() >= self.main.max_memory or self.getsize() >= self.main.max_memory:
+                    self.data.clear()
+                    self.data[key] = self.default()
+        return self.data[key]
+
+    def __setitem__(self, key, value) -> None:
+        try:
+            if self.max_memory is not None and key not in self.data:
+                if self.getsize() >= self.max_memory:
+                    raise MemoryError
+            self.data[key] = value
+        except MemoryError:
+            self.data.clear()
+            self.data[key] = value
+        finally:
+            if self.main is None:
+                return
+            if self.main.max_memory is not None:
+                if self.main.getsize() >= self.main.max_memory or self.getsize() >= self.main.max_memory:
+                    self.data.clear()
+                    self.data[key] = value
+             
+    def __repr__(self) -> str:
+        return str(self.data)
+
+    def __iter__(self):
+        for key in self.data:
+            yield key
+
+    def __call__(self):
+        funcs = [self.data[key] for key in self.data if callable(self.data[key]) and not iscoroutinefunction(self.data[key])]
+        for func in funcs:
+            func()
+    
+    def __delitem__(self, key) -> None:
+        del self.data[key]
+
+    def __len__(self) -> int:
+        counter = 0
+        for _ in self.data:
+            counter += 1
+        return counter
+
+    def pop(self, key) -> None:
+        self.data.pop(key)
+
+    def keys(self):
+       return [key for key in self.data]
+
+    def values(self):
+        return [self.data[key] for key in self.data]
+
+    def update(self, dictionary: dict) -> None:
+        try:
+            for key in dictionary:
+                if self.max_memory is not None and key not in self.data:
+                    if self.getsize() >= self.max_memory:
+                        raise MemoryError
+                self.data[key] = dictionary[key]
+        except MemoryError:
+            self.data.clear()
+            for key in dictionary:
+                self.data[key] = dictionary[key]
+        finally:
+            if self.main is None:
+                return
+            if self.main.max_memory is not None:
+                if self.main.getsize() >= self.main.max_memory or self.getsize() >= self.main.max_memory:
+                    self.data.clear()
+                    for key in dictionary:
+                        self.data[key] = dictionary[key]
+
+    def any(self) -> bool:
+        for key in self.data:
+            if not self.data[key]:
+                return False
+        return True
+
+    def getsize(self) -> IntOrFloat:
+        size = getsizeof(self.data)
+        if len(self.nested_dicts) > 0:
+            nested_dict_size = sum(getsizeof(item) for item in self.nested_dicts)
+            return size + nested_dict_size
+        return size
+
+    def get(self, key) -> None:
+        if key not in self.data:
+            return None
+        return self.data[key]
+
+    def clear(self) -> None:
+        self.data.clear()
+
+# Tesing performance
+# import time
+# start = time.time()
+# dict = MemorySafeDict(max_memory=.5)
+# dict[1] = MemorySafeDict(main=dict)
+# for i in range(100000**100):
+#     dict[i] = i
+# end = time.time()
+# print(end-start)
+
