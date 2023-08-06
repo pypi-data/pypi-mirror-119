0cad060ff4476c1fd84a0f2f1b4470bbc3a70dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:18:23 +0000

    Add package devpi-plumber/0.6.0/devpi-plumber-0.6.0.tar.gz

---
+++ packages/devpi-plumber/devpi-plumber-0.6.0.tar.gz/devpi-plumber-0.6.0/setup.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+
+import multiprocessing  # avoid crash on teardown
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='devpi-plumber',
+    use_scm_version=True,
+    packages=find_packages(exclude=['tests']),
+    author='Stephan Erb',
+    author_email='stephan.erb@blue-yonder.com',
+    url='https://github.com/blue-yonder/devpi-plumber',
+    description='Mario, the devpi-plumber, helps to automate and test large devpi installations.',
+    long_description=readme,
+    license='new BSD',
+    setup_requires=[
+        'setuptools_scm',
+    ],
+    install_requires=[
+        'devpi-client',
+        'requests',
+        'twitter.common.contextutil',
+    ],
+    extras_require={
+        'test': ['devpi-server>=5.2.0'],
+    },
+    tests_require=[
+        'mock',
+        'pytest',
+        'pytest-cov',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: System :: Archiving :: Packaging',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
