616ec5814a9cd1ae31dc1edb1ce6fa6a7dde50fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:53 +0000

    Add package cryptoXam/0.0.0.1/cryptoXam-0.0.0.1.tar.gz

---
+++ packages/cryptoXam/cryptoXam-0.0.0.1.tar.gz/cryptoXam-0.0.0.1/README.md
@@ -0,0 +1,36 @@
+# Packages needed to run
+
+- ccxt
+- plotly
+- pycoingecko
+- pandas
+
+
+# Command to install the required packages
+## would recommend pairing this with a virtual environment
+
+```
+#option virtual env
+#install
+pip install virtual env
+
+#creating
+python3 -m virtualenv env
+
+# activating
+env\scripts\activate
+
+# deactivating
+env\scripts\deactivate
+
+
+pip install -r requirements.text
+```
+
+
+
+# On the current free plan we can expect to retrieve one request per every 30 seconds
+
+
+## further improvements can be made to increase the traffic if need bee
+# testing changes
---
+++ packages/cryptoXam/cryptoXam-0.0.0.1.tar.gz/cryptoXam-0.0.0.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cryptoXam",
+    version="0.0.0.1",
+    author="Max Paul",
+    author_email="maxkpaul21@gmail.com",
+    description="A Data Science Package written in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires=[
+            "ccxt",
+            "pandas",
+            "plotly",
+            "pycoingecko",
+            "twine",
+            "wheel",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
