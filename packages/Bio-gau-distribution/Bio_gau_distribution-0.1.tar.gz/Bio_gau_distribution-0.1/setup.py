36a230756b3c1d786cc318fb8a9fe55990c5b688
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:25 +0000

    Add package Bio-gau-distribution/0.1/Bio_gau_distribution-0.1.tar.gz

---
+++ packages/Bio-gau-distribution/Bio_gau_distribution-0.1.tar.gz/Bio_gau_distribution-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Bio_gau_distribution',
+      version='0.1',
+      description='Gaussian and Bionomial distributions',
+      packages= ['Bio_gau_distribution'],
+      author = 'Moiz Khan',
+      author_email= 'smartmoeez@gmail.com',
+      zip_safe=False)
