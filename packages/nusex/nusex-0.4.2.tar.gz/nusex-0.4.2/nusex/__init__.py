72a9b469401db4c076da20c07484b24bfd4aefca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:27 +0000

    Add package nusex/0.4.2/nusex-0.4.2.tar.gz

---
+++ packages/nusex/nusex-0.4.2.tar.gz/nusex-0.4.2/nusex/__init__.py
@@ -0,0 +1,21 @@
+__productname__ = "nusex"
+__version__ = "0.4.2"
+__description__ = "A project templating utility for Python."
+__url__ = "https://github.com/nusex/nusex"
+__docs__ = "https://nusex.readthedocs.io/en/latest/"
+__author__ = "Ethan Henderson"
+__author_email__ = "ethan.henderson.1998@gmail.com"
+__license__ = "BSD 3-Clause"
+__bugtracker__ = "https://github.com/nusex/nusex/issues"
+
+import os
+from pathlib import Path
+
+if os.name == "nt":
+    CONFIG_DIR = Path.home() / ".nusex"
+    TEMP_DIR = CONFIG_DIR / "tmp"
+else:
+    CONFIG_DIR = Path.home() / ".config/nusex"
+    TEMP_DIR = Path("/tmp/nusex")
+
+from . import commands
