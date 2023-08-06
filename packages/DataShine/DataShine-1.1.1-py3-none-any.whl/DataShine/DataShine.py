47c3d6ee7bcc8dc5784aee2740776d4cdae90bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:13 +0000

    Add package DataShine/1.1.1/DataShine-1.1.1-py3-none-any.whl

---
+++ packages/DataShine/DataShine-1.1.1-py3-none-any.whl/DataShine/DataShine.py
@@ -0,0 +1,42 @@
+import asyncio
+from copy import deepcopy
+
+
+class DataShine:
+    def __init__(self):
+        self._unlocked = asyncio.Event()
+        self._unlocked.set()
+        self._period_change_event = asyncio.Event()
+        self._data_container = None
+
+    async def push_data(self, data):
+        '''
+        Set the lamp to carry a data to be taken, and shine the data to notify monitors new data coming.
+
+        :param data:
+        :return:
+        '''
+        self._data_container = data
+
+        self._period_change_event.clear()
+        self._period_change_event.set()
+        self._period_change_event.clear()
+        await asyncio.sleep(0)
+
+    @property
+    def data(self):
+        '''
+        Query the data last pushed.
+
+        :return:
+        '''
+        return self._data_container
+
+    async def wait_data_shine(self):
+        '''
+        Wait the shined data.
+
+        :return:
+        '''
+        await self._period_change_event.wait()
+        return deepcopy(self._data_container)
