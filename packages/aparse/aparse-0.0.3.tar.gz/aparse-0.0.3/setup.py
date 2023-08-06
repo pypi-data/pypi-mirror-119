11472e958e11b02acae6977e6cf2d674fcf11e87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:51 +0000

    Add package aparse/0.0.3/aparse-0.0.3.tar.gz

---
+++ packages/aparse/aparse-0.0.3.tar.gz/aparse-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from aparse import __version__
+
+setup(
+    name='aparse',
+    version=__version__,
+    packages=find_packages(include=('aparse', 'aparse.*')),
+    author='Jonáš Kulhánek',
+    author_email='jonas.kulhanek@live.com',
+    license='MIT License',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[x.rstrip('\n') for x in open('requirements.txt')])
