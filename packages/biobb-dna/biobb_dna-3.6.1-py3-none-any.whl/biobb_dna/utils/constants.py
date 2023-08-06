0f2c85a3f83288bcbcfe2479ecaee7e633e882e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:26 +0000

    Add package biobb-dna/3.6.1/biobb_dna-3.6.1-py3-none-any.whl

---
+++ packages/biobb-dna/biobb_dna-3.6.1-py3-none-any.whl/biobb_dna/utils/constants.py
@@ -0,0 +1,54 @@
+
+hp_singlebases = [
+    "shear",
+    "stagger",
+    "stretch",
+    "buckle",
+    "opening",
+    "propel",
+    "inclin",
+    "tip",
+    "xdisp",
+    "ydisp"
+]
+
+hp_basepairs = [
+    "majw",
+    "majd",
+    "minw",
+    "mind",
+    "rise",
+    "shift",
+    "slide",
+    "roll",
+    "tilt",
+    "twist"
+]
+
+hp_angular = [
+    "roll",
+    "tilt",
+    "twist",
+    "buckle",
+    "opening",
+    "propel",
+    "inclin",
+    "tip"
+]
+
+hp_translational = [
+    "shear",
+    "stagger",
+    "stretch",
+    "xdisp",
+    "ydisp",
+    "majw",
+    "majd",
+    "minw",
+    "mind",
+    "rise",
+    "shift",
+    "slide"
+]
+
+helical_parameters = hp_basepairs + hp_singlebases
