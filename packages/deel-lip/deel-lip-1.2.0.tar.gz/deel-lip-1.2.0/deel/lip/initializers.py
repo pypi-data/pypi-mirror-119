fb18cfa3854994fe1d321f0a79c7bbb1b7b443ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:43 +0000

    Add package deel-lip/1.2.0/deel-lip-1.2.0.tar.gz

---
+++ packages/deel-lip/deel-lip-1.2.0.tar.gz/deel-lip-1.2.0/deel/lip/initializers.py
@@ -0,0 +1,53 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
+from tensorflow.keras.initializers import Initializer, Orthogonal
+from tensorflow.keras import initializers
+from .normalizers import reshaped_kernel_orthogonalization
+from tensorflow.keras.utils import register_keras_serializable
+
+
+@register_keras_serializable("deel-lip", "SpectralInitializer")
+class SpectralInitializer(Initializer):
+    def __init__(
+        self,
+        niter_spectral=3,
+        niter_bjorck=15,
+        k_coef_lip=1.0,
+        base_initializer=Orthogonal(gain=1.0, seed=None),
+    ) -> None:
+        """
+        Initialize a kernel to be 1-lipschitz orthogonal using bjorck
+        normalization.
+
+        Args:
+            niter_spectral: number of iteration to do with the iterative power method
+            niter_bjorck: number of iteration to do with the bjorck algorithm
+            base_initializer: method used to generate weights before applying the
+                orthonormalization
+        """
+        self.niter_spectral = niter_spectral
+        self.niter_bjorck = niter_bjorck
+        self.k_coef_lip = k_coef_lip
+        self.base_initializer = initializers.get(base_initializer)
+        super(SpectralInitializer, self).__init__()
+
+    def __call__(self, shape, dtype=None, partition_info=None):
+        w = self.base_initializer(shape=shape, dtype=dtype)
+        wbar, u, sigma = reshaped_kernel_orthogonalization(
+            w,
+            None,
+            self.k_coef_lip,
+            self.niter_spectral,
+            self.niter_bjorck,
+        )
+        return wbar
+
+    def get_config(self):
+        return {
+            "niter_spectral": self.niter_spectral,
+            "niter_bjorck": self.niter_bjorck,
+            "k_coef_lip": self.k_coef_lip,
+            "base_initializer": initializers.serialize(self.base_initializer),
+        }
---
+++ packages/deel-lip/deel-lip-1.2.0.tar.gz/deel-lip-1.2.0/deel/lip/normalizers.py
@@ -0,0 +1,116 @@
+# Copyright IRT Antoine de Saint Exupéry et Université Paul Sabatier Toulouse III - All
+# rights reserved. DEEL is a research program operated by IVADO, IRT Saint Exupéry,
+# CRIAQ and ANITI - https://www.deel.ai/
+# =====================================================================================
+"""
+This module contains computation function, for Bjorck and spectral
+normalization. This is done for internal use only.
+"""
+import tensorflow as tf
+from tensorflow.keras import backend as K
+
+DEFAULT_NITER_BJORCK = 15
+DEFAULT_NITER_SPECTRAL = 3
+DEFAULT_NITER_SPECTRAL_INIT = 10
+DEFAULT_BETA_BJORCK = 0.25
+
+
+def reshaped_kernel_orthogonalization(
+    kernel,
+    u,
+    adjustment_coef,
+    niter_spectral=DEFAULT_NITER_SPECTRAL,
+    niter_bjorck=DEFAULT_NITER_BJORCK,
+    beta=DEFAULT_BETA_BJORCK,
+):
+    """
+    Perform reshaped kernel orthogonalization (RKO) to the kernel given as input. It
+    apply the power method to find the largest singular value and apply the Bjorck
+    algorithm to the rescaled kernel. This greatly improve the stability and and
+    speed convergence of the bjorck algorithm.
+
+    Args:
+        kernel: the kernel to orthogonalize
+        u: the vector used to do the power iteration method
+        adjustment_coef: the adjustment coefficient as used in convolution
+        niter_spectral: number of iteration to do in spectral algorithm
+        niter_bjorck: iteration used for bjorck algorithm
+        beta: the beta used in the bjorck algorithm
+
+    Returns: the orthogonalized kernel, the new u, and sigma which is the largest
+        singular value
+
+    """
+    W_bar, u, sigma = spectral_normalization(kernel, u, niter=niter_spectral)
+    W_bar = bjorck_normalization(W_bar, niter=niter_bjorck, beta=beta)
+    W_bar = W_bar * adjustment_coef
+    W_bar = K.reshape(W_bar, kernel.shape)
+    return W_bar, u, sigma
+
+
+def bjorck_normalization(w, niter=DEFAULT_NITER_BJORCK, beta=DEFAULT_BETA_BJORCK):
+    """
+    apply Bjorck normalization on w.
+
+    Args:
+        w: weight to normalize, in order to work properly, we must have
+            max_eigenval(w) ~= 1
+        niter: number of iterations
+        beta: beta used in each iteration, must be in the interval ]0, 0.5]
+
+    Returns:
+        the orthonormal weights
+
+    """
+    for i in range(niter):
+        w = (1 + beta) * w - beta * w @ tf.transpose(w) @ w
+    return w
+
+
+def _power_iteration(w, u, niter=DEFAULT_NITER_SPECTRAL):
+    """
+    Internal function that performs the power iteration algorithm.
+
+    Args:
+        w: weights matrix that we want to find eigen vector
+        u: initialization of the eigen vector
+        niter: number of iteration, must be greater than 0
+
+    Returns:
+         u and v corresponding to the maximum eigenvalue
+
+    """
+    _u = u
+    for i in range(niter):
+        _v = tf.math.l2_normalize(_u @ tf.transpose(w))
+        _u = tf.math.l2_normalize(_v @ w)
+    return _u, _v
+
+
+def spectral_normalization(kernel, u, niter=DEFAULT_NITER_SPECTRAL):
+    """
+    Normalize the kernel to have it's max eigenvalue == 1.
+
+    Args:
+        kernel: the kernel to normalize
+        u: initialization for the max eigen vector
+        niter: number of iteration
+
+    Returns:
+        the normalized kernel w_bar, it's shape, the maximum eigen vector, and the
+        maximum eigen value
+
+    """
+    W_shape = kernel.shape
+    if u is None:
+        niter *= 2  # if u was not known increase number of iterations
+        u = tf.ones(shape=tuple([1, W_shape[-1]]))
+    # Flatten the Tensor
+    W_reshaped = tf.reshape(kernel, [-1, W_shape[-1]])
+    _u, _v = _power_iteration(W_reshaped, u, niter)
+    # Calculate Sigma
+    sigma = _v @ W_reshaped
+    sigma = sigma @ tf.transpose(_u)
+    # normalize it
+    W_bar = W_reshaped / sigma
+    return W_bar, _u, sigma
