46864d98aa1825439dac2e8b194f8a89221d0bd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:33 +0000

    Add package d-py/0.0.3/d_py-0.0.3.tar.gz

---
+++ packages/d-py/d_py-0.0.3.tar.gz/d_py-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['d_py']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['discord.py>=1.7.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'd-py',
+    'version': '0.0.3',
+    'description': 'helper package for discord.py processes',
+    'long_description': None,
+    'author': 'Andrew Bordis',
+    'author_email': 'andrewbordis111@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
