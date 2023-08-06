759148d7cb8acd227a1bb760591f6b337644d792
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:23 +0000

    Add package Encrypt-Lab/0.1.1/Encrypt_Lab-0.1.1-py3-none-any.whl

---
+++ packages/Encrypt-Lab/Encrypt_Lab-0.1.1-py3-none-any.whl/Encrypt_Lab/__init__.py
@@ -0,0 +1,26 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+import Sim
+
+import sys
+
+class Simulator:
+    @classmethod
+    def run(cls): # open the simulator gui
+        app = QApplication(sys.argv)
+
+        m = QMainWindow()
+        m.resize(1000, 1000)
+        w = Sim.Mod(m)
+
+        m.setCentralWidget(w)
+        m.show()
+
+        sys.exit(app.exec_())
+    @classmethod
+
+    def statistics(cls, num=10): # statistics analysis
+        if num < 100: # don't start when num < 100
+            num = 100
+            print('The number of bits was overwritten to 100 because it was too small')
+        return Sim.statistics(num)
+
