36073f84557884842141998ae6f3b0c990fface5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:34 +0000

    Add package oso/0.20.1b0/oso-0.20.1b0-cp36-cp36m-win_amd64.whl

---
+++ packages/oso/oso-0.20.1b0-cp36-cp36m-win_amd64.whl/oso/__init__.py
@@ -0,0 +1,4 @@
+from polar import polar_class, Variable, Predicate
+from .oso import Oso
+from .exceptions import AuthorizationError, ForbiddenError, NotFoundError
+from polar.exceptions import OsoError
