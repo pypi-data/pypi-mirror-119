669d2a583626b313ac5ca60156a39eb09893a796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:38 +0000

    Add package distributionsmhs/0.1/distributionsmhs-0.1.tar.gz

---
+++ packages/distributionsmhs/distributionsmhs-0.1.tar.gz/distributionsmhs-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributionsmhs',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributionsmhs'],
+      zip_safe=False)
