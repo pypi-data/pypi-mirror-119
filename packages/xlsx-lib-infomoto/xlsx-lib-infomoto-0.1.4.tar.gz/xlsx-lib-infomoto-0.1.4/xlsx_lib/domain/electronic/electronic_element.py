c9706062a776587714633ef2141cdbd9d0cf5e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:08 +0000

    Add package xlsx-lib-infomoto/0.1.4/xlsx-lib-infomoto-0.1.4.tar.gz

---
+++ packages/xlsx-lib-infomoto/xlsx-lib-infomoto-0.1.4.tar.gz/xlsx-lib-infomoto-0.1.4/xlsx_lib/domain/electronic/electronic_element.py
@@ -0,0 +1,12 @@
+from xlsx_lib.domain.json_serializable.custom_json_serializable import JSONSerializable
+
+from xlsx_lib.domain.electronic.element_component import ElementComponent
+
+
+class ElectronicElement(JSONSerializable):
+    def __init__(
+            self,
+            name: str,
+    ):
+        self.name: str = name
+        self.components: list[ElementComponent] = list()
