43e17ed26910c2928462ec8ae159e8ba13fc9821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:49 +0000

    Add package rarity-tools-scraper/0.0.3/rarity-tools-scraper-0.0.3.tar.gz

---
+++ packages/rarity-tools-scraper/rarity-tools-scraper-0.0.3.tar.gz/rarity-tools-scraper-0.0.3/rarity_tools_scraper/data.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Optional, Dict, Any
+
+import requests
+from dataclasses_json import dataclass_json, CatchAll
+from fake_useragent import UserAgent
+
+BASE_DATA_ENDPOINT_URL = "https://data.rarity.tools/{endpoint}/{collection}"
+ua = UserAgent()
+
+
+@dataclass_json
+@dataclass
+class Prices:
+	prices: Dict[str, Any]
+	v: int
+	startDate: Optional[int] = None
+	unknown_things: CatchAll = None
+
+
+def get_collection_prices(collection: str) -> Prices:
+	res = requests.get(BASE_DATA_ENDPOINT_URL.format(endpoint="prices", collection=collection), headers={
+		"User-Agent": ua.random
+	})
+	json = res.json()
+
+	prices = Prices.from_dict(json)
+
+	return prices
