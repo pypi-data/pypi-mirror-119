d42d7dbb1d7c5d1b22551712338d3b0564c1c71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:19 +0000

    Add package tkzs/0.0.2/tkzs-0.0.2-py3-none-any.whl

---
+++ packages/tkzs/tkzs-0.0.2-py3-none-any.whl/tkzs/tokenizers.py
@@ -0,0 +1,29 @@
+import spacy
+import re
+import string
+from torch.nn.utils.rnn import pad_sequence
+    
+class SpacyTokenizer(object):
+    
+    def __init__(self, name='en_core_web_sm'):
+        
+        self.spacy_model = spacy.load(name=name, disable=['parser', 'ner'])
+        
+    def tokenize(self, sent):
+        
+        return [s.text for s in self.spacy_model(sent)]
+
+def re_tokenizer(txt):
+    """simple tokenization function based on regex
+
+    Args:
+        txt (str): string sequence
+
+    Returns:
+        list[str]: list of tokens
+    """
+    t = txt.strip().lower()
+    t = re.sub(r'([%s])' % re.escape(string.punctuation), r' \1 ', t) 
+    t = re.sub(r'\\.', r' ', t) 
+    t = re.sub(r'\s+', r' ', t)
+    return t.split()
