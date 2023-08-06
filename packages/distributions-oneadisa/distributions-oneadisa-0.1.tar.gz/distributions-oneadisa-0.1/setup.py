c764d1aeab740dc511f8d44e5135b3cdef004109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:06 +0000

    Add package distributions-oneadisa/0.1/distributions-oneadisa-0.1.tar.gz

---
+++ packages/distributions-oneadisa/distributions-oneadisa-0.1.tar.gz/distributions-oneadisa-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+      name='distributions-oneadisa',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions-oneadisa'],
+      author = 'Abdul Ibrahim',
+      author_email = 'theoneadisa@gmail.com',
+      zip_safe=False
+      )
