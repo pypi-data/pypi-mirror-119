8f1be897991d1ba6f44854d2c11b0a06da81dcb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:58 +0000

    Add package SplitPro/0.0.1/SplitPro-0.0.1.tar.gz

---
+++ packages/SplitPro/SplitPro-0.0.1.tar.gz/SplitPro-0.0.1/README.md
@@ -0,0 +1,3 @@
+This is a simple example package. You can use
+[My own repository](https://github.com/YaroslavYaryk/Python-OOP)
+to write your content.
---
+++ packages/SplitPro/SplitPro-0.0.1.tar.gz/SplitPro-0.0.1/program/split.py
@@ -0,0 +1,20 @@
+from string import ascii_letters
+
+def split_by_symbol(expression):
+
+    num_list = []
+    num = ""
+
+    for char in expression:
+        if char.isdigit() or char in ascii_letters:
+            num += char
+
+        else:
+            num_list.append(num)
+            num_list.append(char)
+
+            num = ""
+    if num:
+        num_list.append(num)
+
+    return list(filter(lambda x: x, num_list))
