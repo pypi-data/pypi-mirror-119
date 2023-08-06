40b2b691d88e74eb6b01f27d4c23adb21c74d773
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:49 +0000

    Add package QuickMiner/0.2.5/QuickMiner-0.2.5.tar.gz

---
+++ packages/QuickMiner/QuickMiner-0.2.5.tar.gz/QuickMiner-0.2.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.2.5' 
+DESCRIPTION = 'QuickMiner'
+LONG_DESCRIPTION = 'Advanced test and simulated mining module'
+
+setup(
+        name="QuickMiner", 
+        version=VERSION,
+        author="Jason Odd",
+        author_email="<jasonodd@gmaiI.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], 
+        
+        keywords=['python', 'first package'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
