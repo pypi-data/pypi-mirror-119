17a2bcda50c70171881298b32c8e19591c9f5a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:50 +0000

    Add package python-exiv2/0.2.3/python_exiv2-0.2.3-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/python-exiv2/python_exiv2-0.2.3-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/exiv2/__init__.py
@@ -0,0 +1,34 @@
+
+import logging
+import sys
+
+if sys.platform == 'linux':
+    import os
+    _lib = os.path.join(os.path.dirname(__file__), 'libexiv2.so')
+    if os.path.exists(_lib):
+        # import libexiv2 shared library (avoids setting LD_LIBRARY_PATH)
+        from ctypes import cdll
+        cdll.LoadLibrary(_lib)
+
+_logger = logging.getLogger(__name__)
+
+class AnyError(Exception):
+    """Python exception raised by exiv2 library errors"""
+    pass
+
+__version__ = "0.2.3"
+
+from exiv2.datasets import *
+from exiv2.error import *
+from exiv2.exif import *
+from exiv2.image import *
+from exiv2.iptc import *
+from exiv2.metadatum import *
+from exiv2.properties import *
+from exiv2.tags import *
+from exiv2.types import *
+from exiv2.value import *
+from exiv2.version import *
+from exiv2.xmp import *
+
+__all__ = [x for x in dir() if x[0] != '_']
