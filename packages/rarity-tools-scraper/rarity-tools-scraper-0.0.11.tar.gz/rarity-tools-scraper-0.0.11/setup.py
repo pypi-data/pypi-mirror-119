2a180e3539101bc9b87b61e9fbf77580aa7ea972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:23 +0000

    Add package rarity-tools-scraper/0.0.11/rarity-tools-scraper-0.0.11.tar.gz

---
+++ packages/rarity-tools-scraper/rarity-tools-scraper-0.0.11.tar.gz/rarity-tools-scraper-0.0.11/setup.py
@@ -0,0 +1,21 @@
+#!/bin/env python3
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="rarity-tools-scraper",
+    version="0.0.11",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/quantumlyy/rarity-tools-scraper-py",
+    author="Nejc Drobnič",
+    author_email="yo@quantumly.dev",
+    license="BSD-3-Clause",
+    packages=["rarity_tools_scraper"],
+    include_package_data=True,
+    setup_requires="setuptools-pipfile",
+    use_pipfile=True,
+)
