2b6fd03f41e9e9ba70f0b2340c6775cd164229b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:20 +0000

    Add package moredatatypes/1.0.0/moredatatypes-1.0.0.tar.gz

---
+++ packages/moredatatypes/moredatatypes-1.0.0.tar.gz/moredatatypes-1.0.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="moredatatypes",
+    version="1.0.0",
+    author="RhubarbDev",
+    description="Add Char and Pair types to Python",
+    url="https://github.com/RhubarbJamm/moretypes",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
