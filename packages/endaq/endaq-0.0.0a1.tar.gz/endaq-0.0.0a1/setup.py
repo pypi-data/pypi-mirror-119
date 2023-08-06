74d9a7af0902c08dc13e2249aa9a58ad7dc4586e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package endaq/0.0.0a1/endaq-0.0.0a1.tar.gz

---
+++ packages/endaq/endaq-0.0.0a1.tar.gz/endaq-0.0.0a1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name='endaq',
+        version='0.0.0a1',
+        author='Mide Technology',
+        author_email='help@mide.com',
+        packages=[],
+        package_dir={'endaq': './endaq'},
+        install_requires=[],
+)
