74337d13b20b8ea2e5d24419462a43bbe442705a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:06 +0000

    Add package edc-blood-results/0.1.9/edc_blood_results-0.1.9-py3-none-any.whl

---
+++ packages/edc-blood-results/edc_blood_results-0.1.9-py3-none-any.whl/edc_blood_results/model_mixins/hba1c_model_mixin.py
@@ -0,0 +1,62 @@
+from django.db import models
+from edc_constants.choices import YES_NO
+from edc_constants.constants import FASTING
+from edc_lab.choices import RESULT_QUANTIFIER
+from edc_lab.constants import EQ
+from edc_reportable import PERCENT
+from edc_reportable.choices import REPORTABLE
+
+
+class Hba1cModelMixin(models.Model):
+
+    is_poc = models.CharField(
+        verbose_name="Was a point-of-care test used?",
+        max_length=15,
+        choices=YES_NO,
+        null=True,
+    )
+
+    hba1c_value = models.DecimalField(
+        verbose_name="HbA1c value",
+        max_digits=8,
+        decimal_places=4,
+        null=True,
+        blank=True,
+    )
+
+    hba1c_quantifier = models.CharField(
+        max_length=10,
+        choices=RESULT_QUANTIFIER,
+        default=EQ,
+        null=True,
+        blank=True,
+    )
+
+    hba1c_units = models.CharField(
+        verbose_name="units",
+        max_length=15,
+        default=PERCENT,
+        null=True,
+        blank=True,
+    )
+
+    hba1c_abnormal = models.CharField(
+        verbose_name="abnormal", choices=YES_NO, max_length=25, null=True, blank=True
+    )
+
+    hba1c_reportable = models.CharField(
+        verbose_name="reportable",
+        choices=REPORTABLE,
+        max_length=25,
+        null=True,
+        blank=True,
+    )
+
+    def get_summary_options(self):
+        opts = super().get_summary_options()
+        fasting = True if self.fasting == FASTING else False
+        opts.update(fasting=fasting)
+        return opts
+
+    class Meta:
+        abstract = True
