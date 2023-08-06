78ced61d56fd8d12a3eb09a84af8c3838dc92a9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:39 +0000

    Add package pyVkBotApiV2/1.0.0/pyVkBotApiV2-1.0.0.tar.gz

---
+++ packages/pyVkBotApiV2/pyVkBotApiV2-1.0.0.tar.gz/pyVkBotApiV2-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='pyVkBotApiV2',
+    version='1.0.0',
+    author='Airkek',
+    author_email='',
+    description='Python vk.com API wrapper',
+    url='',
+    packages=['pyVkBotApiV2'],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license='MIT',
+    install_requires=['requests']
+)
