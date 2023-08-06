1222c8c7b63add5279d499d9086ce46e73a987a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:51 +0000

    Add package erosion/0.0.2/erosion-0.0.2-py2.py3-none-any.whl

---
+++ packages/erosion/erosion-0.0.2-py2.py3-none-any.whl/erosion/head.py
@@ -0,0 +1,17 @@
+import sys
+
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'
+}
+
+path = sys.path[4]+'\\data.txt'
+
+def cookie():
+    with open(path) as f:
+        msg = f.read()
+    return {'SESSDATA':msg}
+
+cookies ={
+    'SESSDATA':'41be34e2%2C1645695527%2C6af62%2A81'
+}
