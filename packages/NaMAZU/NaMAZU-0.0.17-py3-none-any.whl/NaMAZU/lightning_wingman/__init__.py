4622f8a5677be6bb12a4b37b6db089ffc71b91eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:53 +0000

    Add package NaMAZU/0.0.17/NaMAZU-0.0.17-py3-none-any.whl

---
+++ packages/NaMAZU/NaMAZU-0.0.17-py3-none-any.whl/NaMAZU/lightning_wingman/__init__.py
@@ -0,0 +1,8 @@
+from .inference_helper import *
+from .torch_knn import *
+from .torch_nbc import *
+from .torch_gmm import *
+from .lit_u2net import *
+
+
+__all__ = ["PredictionAssistant", "KNN", "NBC", "GMM", "LitU2Net"]
