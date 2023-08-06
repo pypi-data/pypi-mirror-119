fcb09e5751237016bd1658ba8ad53b843e631e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package endaq-cloud/0.0.0a1/endaq-cloud-0.0.0a1.tar.gz

---
+++ packages/endaq-cloud/endaq-cloud-0.0.0a1.tar.gz/endaq-cloud-0.0.0a1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name='endaq-cloud',
+        version='0.0.0a1',
+        author='Mide Technology',
+        author_email='help@mide.com',
+        packages=[],
+        package_dir={'endaq-cloud': './endaq'},
+        install_requires=[],
+)
