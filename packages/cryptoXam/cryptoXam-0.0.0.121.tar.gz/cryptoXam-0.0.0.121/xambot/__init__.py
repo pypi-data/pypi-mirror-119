7dfa12fb7fb0c4dab5dda0ae1b03297e7abf8fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:32 +0000

    Add package cryptoXam/0.0.0.121/cryptoXam-0.0.0.121.tar.gz

---
+++ packages/cryptoXam/cryptoXam-0.0.0.121.tar.gz/cryptoXam-0.0.0.121/xambot/__init__.py
@@ -0,0 +1,12 @@
+from charting import chart
+from tokens import *
+from time import sleep
+
+# can call any function from the above here for testing but all you do is import the folder for pypi functionality
+counter = 0
+#sleep(60)
+while True:
+    counter +=1
+    print(get_token_price("Cardano",'usd'))
+    print(counter)
+    sleep(30)
