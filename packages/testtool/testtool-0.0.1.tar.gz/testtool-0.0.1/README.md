ba2eabb418559822f823d815049b0d323e496fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:43 +0000

    Add package testtool/0.0.1/testtool-0.0.1.tar.gz

---
+++ packages/testtool/testtool-0.0.1.tar.gz/testtool-0.0.1/README.md
@@ -0,0 +1,2 @@
+# testtool
+This is rolence test project
---
+++ packages/testtool/testtool-0.0.1.tar.gz/testtool-0.0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    longdesc = f.read()
+
+setup(
+    name='testtool',
+    version='0.0.1',
+    description='redoor test',
+    py_modules=['testtool'],
+    package_dir={'':'src'},
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    long_description = longdesc,
+    long_description_content_type = "text/markdown"
+)
