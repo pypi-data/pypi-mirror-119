00d3c1aead7b58b92e7dfb83205c058f475ea8d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:44 +0000

    Add package taurus-vdk/0.1.369062590/taurus-vdk-0.1.369062590.tar.gz

---
+++ packages/taurus-vdk/taurus-vdk-0.1.369062590.tar.gz/taurus-vdk-0.1.369062590/setup.py
@@ -0,0 +1,18 @@
+# Copyright 2021 VMware, Inc.
+# SPDX-License-Identifier: Apache-2.0
+import setuptools
+
+"""
+Builds a package with the help of setuptools in order for this package to be imported in other projects
+"""
+
+__version__ = "0.1.369062590"
+
+setuptools.setup(
+    name="taurus-vdk",
+    version=__version__,
+    install_requires=["vdk-core", "vdk-plugin-control-cli", "vdk-trino"],
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
