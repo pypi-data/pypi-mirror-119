f75026ee0bc6b4f28b962d833c08be44221f1b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:29 +0000

    Add package sanepg/0.1.1/sanepg-0.1.1.tar.gz

---
+++ packages/sanepg/sanepg-0.1.1.tar.gz/sanepg-0.1.1/sanepg/version.py
@@ -0,0 +1,8 @@
+
+__author__    = 'Matthew Shaw'
+__email__     = 'mshaw.cx@gmail.com'
+__copyright__ = '(c)2018-2021, Matthew Shaw'
+__credits__   = ['Matthew Shaw']
+
+__license__   = 'MIT'
+__version__   = '0.1.1'
