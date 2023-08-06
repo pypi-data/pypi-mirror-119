99f143bef3583d52ed4a185c99d25e313e23ae99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:33 +0000

    Add package PyConfigParser/1.0.3/PyConfigParser-1.0.3.tar.gz

---
+++ packages/PyConfigParser/PyConfigParser-1.0.3.tar.gz/PyConfigParser-1.0.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name="PyConfigParser",
+    url="https://github.com/Akasan/PyConfigParser",
+    version="1.0.3",
+    install_requires=[],
+    packages=[
+        "ConfigParser",
+    ]
+)
