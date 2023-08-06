c29670bd73bf953cb2e90724288d81ff38362010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:40 +0000

    Add package threaded-context/1.2.2/threaded-context-1.2.2.tar.gz

---
+++ packages/threaded-context/threaded-context-1.2.2.tar.gz/threaded-context-1.2.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['threaded_context']
+setup_kwargs = {
+    'name': 'threaded-context',
+    'version': '1.2.2',
+    'description': 'Basic context manager with inheritance management.',
+    'long_description': None,
+    'author': 'François Schmidts',
+    'author_email': 'francois@schmidts.fr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
