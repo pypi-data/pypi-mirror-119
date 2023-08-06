797c290177570aac2f3109cf4f2182dd9880ae48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:05 +0000

    Add package AniManga/0.1.0/AniManga-0.1.0.tar.gz

---
+++ packages/AniManga/AniManga-0.1.0.tar.gz/AniManga-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+with open("README.md") as f:
+    LONGDESCRIPTION = f.read()
+
+VERSION = "0.1.0"
+DESCRIPTION = "AniManga is a python module which scrapes the web to get information on Anime, Manga (and hentai)."
+
+# Setting up
+setup(
+    name="AniManga",
+    version=VERSION,
+    author="Siddhant Kumar",
+    author_email="centipedemonster@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONGDESCRIPTION,
+    packages=find_packages(),
+    install_requires=["bs4","requests","html5lib"],
+    keywords=["anime","manga","hentai","nhentai","anime planet"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
