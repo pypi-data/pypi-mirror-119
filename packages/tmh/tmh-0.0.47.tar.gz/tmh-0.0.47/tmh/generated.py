e239ec2a3725816bb0f92ac66e2a9d69fec51868
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:41 +0000

    Add package tmh/0.0.47/tmh-0.0.47.tar.gz

---
+++ packages/tmh/tmh-0.0.47.tar.gz/tmh-0.0.47/tmh/generated.py
@@ -0,0 +1,10 @@
+"""
+
+print "What is your name?"
+name = raw_input()
+if name == "Sam":
+	print "What the hell, %s? Make it yourself." % name
+elif name == "Boss":
+	print "Yes sir!"
+else: 
+	print "Thank you, %s. I'll be sure to tell your mother." % name
