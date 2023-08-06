0c8593a5e9e858b95f5248fd8c5acc0a2d6b80fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:32 +0000

    Add package PyGL-Astroids/0.0.2/PyGL_Astroids-0.0.2-py3-none-any.whl

---
+++ packages/PyGL-Astroids/PyGL_Astroids-0.0.2-py3-none-any.whl/pygl_astroids/globalvars.py
@@ -0,0 +1,12 @@
+"""Defines global variables."""
+import pyglet
+
+BOARD_WIDTH = 640
+BOARD_HEIGHT = 448
+AST_GEN_FREQ = 1/2  # Number of asteroids generated / time in seconds
+
+board = pyglet.window.Window(BOARD_WIDTH, BOARD_HEIGHT, caption="PyGL-Astroids")
+batch = pyglet.graphics.Batch()
+asteroids_group = pyglet.graphics.OrderedGroup(0)
+player_group = pyglet.graphics.OrderedGroup(1)
+label_group = pyglet.graphics.OrderedGroup(2)
