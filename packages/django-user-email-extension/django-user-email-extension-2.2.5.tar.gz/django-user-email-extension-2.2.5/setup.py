4fbfd6102491039512b9d202e390c199d71d2326
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:10 +0000

    Add package django-user-email-extension/2.2.5/django-user-email-extension-2.2.5.tar.gz

---
+++ packages/django-user-email-extension/django-user-email-extension-2.2.5.tar.gz/django-user-email-extension-2.2.5/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+from pkg_resources import parse_requirements
+
+with open("requirements.txt") as f:
+    requirements = [str(r) for r in parse_requirements(f)]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-user-email-extension",
+    version="2.2.5",
+    author="Arie Lev",
+    author_email="levinson.arie@gmail.com",
+    description="User model extender for django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ArieLevs/Django-User-Email-Extension",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 3.0",
+    ],
+)
