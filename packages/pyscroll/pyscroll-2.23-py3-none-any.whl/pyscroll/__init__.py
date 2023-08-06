50879bafb4ca11fedf8d2df43530b3c51fa15c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:58 +0000

    Add package pyscroll/2.23/pyscroll-2.23-py3-none-any.whl

---
+++ packages/pyscroll/pyscroll-2.23-py3-none-any.whl/pyscroll/__init__.py
@@ -0,0 +1,11 @@
+# convenience imports
+from .orthographic import BufferedRenderer
+from .isometric import IsometricBufferedRenderer
+from .data import *
+from .group import *
+
+
+__version__ = 2, 23
+__author__ = 'bitcraft'
+__author_email__ = 'leif.theden@gmail.com'
+__description__ = 'Pygame Scrolling - Python 3.5+'
