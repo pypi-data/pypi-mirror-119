3d214bb1c48689f753484885716b9ae0468163f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:25 +0000

    Add package blackboxopt/1.0.6/blackboxopt-1.0.6-py3-none-any.whl

---
+++ packages/blackboxopt/blackboxopt-1.0.6-py3-none-any.whl/blackboxopt/__init__.py
@@ -0,0 +1,13 @@
+__version__ = "1.0.0"
+
+from parameterspace.base import SearchSpace
+
+from .base import (
+    ConstraintsError,
+    Objective,
+    ObjectivesError,
+    OptimizationComplete,
+    Optimizer,
+    OptimizerNotReady,
+)
+from .evaluation import Evaluation, EvaluationSpecification
