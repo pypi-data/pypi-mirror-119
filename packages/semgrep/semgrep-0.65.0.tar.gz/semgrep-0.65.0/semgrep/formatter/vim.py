43bb70230ff83eef4cfbf703f130a1b9d01984f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:24 +0000

    Add package semgrep/0.65.0/semgrep-0.65.0.tar.gz

---
+++ packages/semgrep/semgrep-0.65.0.tar.gz/semgrep-0.65.0/semgrep/formatter/vim.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from semgrep.constants import RuleSeverity
+from semgrep.formatter.base import BaseFormatter
+from semgrep.rule_match import RuleMatch
+
+
+class VimFormatter(BaseFormatter):
+    @staticmethod
+    def _get_parts(rule_match: RuleMatch) -> List[str]:
+        severity = {
+            RuleSeverity.INFO: "I",
+            RuleSeverity.WARNING: "W",
+            RuleSeverity.ERROR: "E",
+        }
+        return [
+            str(rule_match.path),
+            str(rule_match.start["line"]),
+            str(rule_match.start["col"]),
+            severity[rule_match.severity],
+            rule_match.id,
+            rule_match.message,
+        ]
+
+    def output(self) -> str:
+        return "\n".join(":".join(self._get_parts(rm)) for rm in self.rule_matches)
