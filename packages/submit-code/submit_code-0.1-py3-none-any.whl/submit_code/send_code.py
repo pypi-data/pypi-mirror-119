adb3eb46d5dddf6086078746a4142da36264942c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:36 +0000

    Add package submit-code/0.1/submit_code-0.1-py3-none-any.whl

---
+++ packages/submit-code/submit_code-0.1-py3-none-any.whl/submit_code/send_code.py
@@ -0,0 +1,60 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from IPython import get_ipython
+from datetime import datetime
+import time
+import os
+
+license_path = os.path.dirname(__file__)
+cred = credentials.Certificate('{}/web-project-c3c1a-firebase-adminsdk-9j2mf-e202997418.json'.format(license_path))
+firebase_admin.initialize_app(cred, {'databaseURL': 'https://web-project-c3c1a-default-rtdb.asia-southeast1.firebasedatabase.app'})
+
+
+class SubmitManager:
+
+    def __init__(self):
+        self.ip = get_ipython() 
+   
+    def Update(self):
+        data_dic = {
+                self.title : {
+                    "user_id" : self.user_id,
+                    "id" : int(time.time()),
+                    "date" :  self.nowDatetime,
+                    "text" : self.code,
+                }
+            }
+        user_ref = db.reference('code')
+        user_ref.update(data_dic)
+
+    def GetData(self):
+        code_ref = db.reference('code')
+        snapshot = code_ref.order_by_child('id').get()
+        for key, val in snapshot.items():
+            print("{} : {}".format(key, val))
+
+    def ExtractCode(self) :
+        codes = []
+        run_history = self.ip.history_manager.get_range()
+        for code in run_history :
+            codes.append(code[2])
+        return codes[-1]    
+
+    def Submit(self, user_id, title):
+
+        self.user_id, self.title = user_id, title
+        self.code = self.ExtractCode()
+        now = datetime.now()
+        self.nowDatetime = now.strftime('%Y-%m-%d %H:%M:%S\n')
+        self.Update()
+        print("#-----------Code-----------")
+        print(self.code)
+        print("#--------------------------")
+        print(self.nowDatetime, "제출완료" )
+        raise SystemExit("제출 완료에 따른 정상적인 Cell 실행 중지")
+        
+
+        
+
+
