18b7155bdd917e782ecc793d80794a7db0994a1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:48 +0000

    Add package xlsx-lib-infomoto/0.1.0/xlsx-lib-infomoto-0.1.0.tar.gz

---
+++ packages/xlsx-lib-infomoto/xlsx-lib-infomoto-0.1.0.tar.gz/xlsx-lib-infomoto-0.1.0/domain/motorcycle_model/motorcycle_model.py
@@ -0,0 +1,43 @@
+import random
+
+from typing import Optional, IO
+
+from domain.engine.engine_section import EngineSection
+from domain.frame.frame_element import FrameElement
+from domain.json_serializable.custom_json_serializable import JSONSerializable
+
+from domain.generic_replacements.replacement import Replacement
+from domain.tightening_torques.component import Component
+from domain.electronic.electronic_element import ElectronicElement
+
+
+class MotorcycleModel(JSONSerializable):
+    def __init__(
+            self,
+            model_name: str,
+            generic_replacements: Optional[list[Replacement]] = None,
+            components_screws_tightening_torques: Optional[list[Component]] = None,
+            electronic_elements: Optional[list[ElectronicElement]] = None,
+            engine_sections: Optional[list[EngineSection]] = None,
+            frame_elements: Optional[list[FrameElement]] = None,
+            directory_name: Optional[str] = None,
+    ):
+        self.model_name: str = model_name
+        self.generic_replacements: Optional[list[Replacement]] = generic_replacements
+        self.components_screws_tightening_torques: Optional[list[Component]] = components_screws_tightening_torques
+        self.electronic_elements: Optional[list[ElectronicElement]] = electronic_elements
+        self.engine_sections: list[EngineSection] = engine_sections
+        self.frame_elements: Optional[list[FrameElement]] = frame_elements
+
+        json_text: str = self.to_json()
+
+        path: str
+
+        if directory_name is not None:
+            path = f"{directory_name}/{self.model_name}.json"
+        else:
+            path = f"json/{random.randint(0, 999)}.json"
+
+        file: IO = open(path, "w")
+        file.write(json_text)
+        file.close()
