98c551dbbff9db340c9e2c48d182e1c856275a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:07 +0000

    Add package randomquotebot/0.3/randomquotebot-0.3-py3-none-any.whl

---
+++ packages/randomquotebot/randomquotebot-0.3-py3-none-any.whl/quotebot/jamesbrown.py
@@ -0,0 +1,9 @@
+from colr import color
+
+
+def artist():
+    print(color("Say it loud!", fore='orange', style='bright'))
+
+def audience():
+    print(color("I'm black and I'm proud", fore='blue', style='bright'))
+
