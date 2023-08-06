0e71af8b42c04d1fb276eba94823fd4260437fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:25 +0000

    Add package discwrappy/0.4-alpha/discwrappy-0.4-alpha.tar.gz

---
+++ packages/discwrappy/discwrappy-0.4-alpha.tar.gz/discwrappy-0.4-alpha/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name = 'discwrappy',
+    packages = ['discwrappy'],
+    version = '0.4-alpha',
+    description = 'A Python wrapper for the Discord API',
+    author = 'Jeff Morris',
+    author_email = 'jeffreyjr.morris@gmail.com',
+    url = 'https://github.com/JeffTheModder/DiscWrappy',
+    download_url = 'https://github.com/JeffTheModder/DiscWrappy/archive/refs/tags/v_0.4-alpha.tar.gz',
+    keywords = ['Discord', 'API', 'wrapper', 'python', 'library'],
+    install_requires = [
+        'requests',
+        'websocket-client',
+    ],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python'
+    ],
+)
