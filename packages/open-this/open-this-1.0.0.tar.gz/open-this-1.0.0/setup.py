1438fa8e7fa128dbf632ed96ec73b06ba25e76f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:02 +0000

    Add package open-this/1.0.0/open-this-1.0.0.tar.gz

---
+++ packages/open-this/open-this-1.0.0.tar.gz/open-this-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
+
+setup(
+    name="open-this",
+    version="1.0.0",
+    description="A Python tiny package that opens Facebook, Youtube from terminal",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/iamdarshan7/OpenSocialMediaWithCLI",
+    author="Darshan Thapa",
+    author_email="darshanthapa872@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["open_this"],
+    include_package_data=True,
+    install_requires=["pyperclip==1.8.0", "webbrowser"],
+    entry_points={
+        "console_scripts": [
+            "open-this=open_this.socialMedia:main",
+        ]
+    },
+)
