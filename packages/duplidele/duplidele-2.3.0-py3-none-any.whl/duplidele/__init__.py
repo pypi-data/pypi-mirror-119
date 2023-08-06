c0465aedf50155280dc978f54f55b96299161178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:53 +0000

    Add package duplidele/2.3.0/duplidele-2.3.0-py3-none-any.whl

---
+++ packages/duplidele/duplidele-2.3.0-py3-none-any.whl/duplidele/__init__.py
@@ -0,0 +1,8 @@
+from .duplidele import *
+
+__copyright__ = 'Copyright(C)2021 Takao Morita'
+__version__ = '2.3.0'
+__license__ = 'MIT'
+__author__ = 'Takao Morita'
+__author_email__ = 'tmolita077@gmail.com'
+__url__ = 'https://github.com/taotaotao3/duplidele'
