9f81270bedfb0a6b53f025230758e2094be847d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:03 +0000

    Add package retrolab/0.3.5/retrolab-0.3.5.tar.gz

---
+++ packages/retrolab/retrolab-0.3.5.tar.gz/retrolab-0.3.5/retrolab/static/_61f1-_6d15-_08fb-_9055-_1b37-_2a77.bundle.js
@@ -0,0 +1,71 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_JUPYTERLAB_CORE_OUTPUT"] = self["webpackChunk_JUPYTERLAB_CORE_OUTPUT"] || []).push([["_61f1-_6d15-_08fb-_9055-_1b37-_2a77"],{
+
+/***/ "?61f1":
+/*!**************************************!*\
+  !*** ./terminal-highlight (ignored) ***!
+  \**************************************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/./terminal-highlight_(ignored)?");
+
+/***/ }),
+
+/***/ "?6d15":
+/*!***************************!*\
+  !*** colorette (ignored) ***!
+  \***************************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/colorette_(ignored)?");
+
+/***/ }),
+
+/***/ "?08fb":
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/fs_(ignored)?");
+
+/***/ }),
+
+/***/ "?9055":
+/*!**********************!*\
+  !*** path (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/path_(ignored)?");
+
+/***/ }),
+
+/***/ "?1b37":
+/*!****************************!*\
+  !*** source-map (ignored) ***!
+  \****************************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/source-map_(ignored)?");
+
+/***/ }),
+
+/***/ "?2a77":
+/*!*********************!*\
+  !*** url (ignored) ***!
+  \*********************/
+/***/ (() => {
+
+eval("/* (ignored) */\n\n//# sourceURL=webpack://_JUPYTERLAB.CORE_OUTPUT/url_(ignored)?");
+
+/***/ })
+
+}]);
