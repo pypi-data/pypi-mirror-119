d4468e9248ee615f077a854705080a54f426fa14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:42 +0000

    Add package pixie-python/0.1.1/pixie-python-0.1.1.tar.gz

---
+++ packages/pixie-python/pixie-python-0.1.1.tar.gz/pixie-python-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name = "pixie-python",
+    version = "0.1.1",
+    author = "Andre von Houck",
+    author_email = "starplant@gmail.com",
+    description = "Python bindings for Pixie, a full-featured 2D graphics library",
+    url = "https://github.com/treeform/pixie-python",
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    include_package_data = True,
+    package_data = {"pixie": ["pixie.dll", "libpixie.so", "libpixie.dylib"]},
+    python_requires = ">=3.6.0",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License"
+  ],
+)
