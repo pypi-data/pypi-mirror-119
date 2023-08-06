69c9f5eafc4c39433860376e3348b5de4415bc35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:54 +0000

    Add package django-light/0.1.0.post1/django-light-0.1.0.post1.tar.gz

---
+++ packages/django-light/django-light-0.1.0.post1.tar.gz/django-light-0.1.0.post1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup
+
+setup(
+    name='django-light',
+    description='Disable dark mode in Django admin UI.',
+    long_description='file: README.md',
+    long_description_content_type='text/markdown',
+    version='0.1.0-1',
+    url='https://github.com/frnhr/django-light',
+    author='Fran Hrzenjak',
+    author_email='fran@changeset.hr',
+    scripts=[],
+    packages=['django_light'],
+    include_package_data=True,
+    license='MIT',
+    keywords='',
+    install_requires=[],
+    extras_require={},
+)
