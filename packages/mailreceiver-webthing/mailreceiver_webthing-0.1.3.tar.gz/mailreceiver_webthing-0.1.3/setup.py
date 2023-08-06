dd1724467dbd5b03793acb11cad2b99cbb7fc6ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:41 +0000

    Add package mailreceiver-webthing/0.1.3/mailreceiver_webthing-0.1.3.tar.gz

---
+++ packages/mailreceiver-webthing/mailreceiver_webthing-0.1.3.tar.gz/mailreceiver_webthing-0.1.3/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+from os import path
+
+PACKAGENAME = 'mailreceiver_webthing'
+ENTRY_POINT = "mailreceiver"
+DESCRIPTION = "A web connected mail receiver"
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name=PACKAGENAME,
+    packages=[PACKAGENAME],
+    version_config={
+        "version_format": "{tag}.dev{sha}",
+        "starting_version": "0.0.1"
+    },
+    setup_requires=['better-setuptools-git-version'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='Gregor Roth',
+    author_email='gregor.roth@web.de',
+    url='https://github.com/grro/mailreceiver_webthing',
+    entry_points={
+        'console_scripts': [
+            ENTRY_POINT + '=' + PACKAGENAME + ':main'
+        ]
+    },
+    keywords=[
+        'webthings', 'home automation', 'mail', 'server'
+    ],
+    install_requires=[
+        'webthing==0.15.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha"
+    ],
+)
+
