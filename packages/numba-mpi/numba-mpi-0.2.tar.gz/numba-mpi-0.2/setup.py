3a2480fd2e063da9afdb4d0fb574ebb42b429cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:11 +0000

    Add package numba-mpi/0.2/numba-mpi-0.2.tar.gz

---
+++ packages/numba-mpi/numba-mpi-0.2.tar.gz/numba-mpi-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    with open("README.md", "r", encoding="utf8") as file:
+        long_description = file.read()
+    return long_description
+
+
+setup(
+    name='numba-mpi',
+    url="https://github.com/atmos-cloud-sim-uj/numba-mpi",
+    author='https://github.com/atmos-cloud-sim-uj/numba-mpi/graphs/contributors',
+    version='0.2',
+    setup_requires=['setuptools_scm'],
+    license='GPL v3',
+    description='Numba @njittable MPI wrappers tested on Linux, macOS and Windows',
+    install_requires=('numba', 'numpy', 'mpi4py'),
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=['numba_mpi', 'numba_mpi.*'])
+)
