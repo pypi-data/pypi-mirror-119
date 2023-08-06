7045c23a35d5e1dc483b2d196599cb6ec224d89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:28 +0000

    Add package tsts/0.4.0/tsts-0.4.0-py3-none-any.whl

---
+++ packages/tsts/tsts-0.4.0-py3-none-any.whl/tsts/core/__init__.py
@@ -0,0 +1,29 @@
+from .context_manager import ContextManager
+from .registry import (
+    COLLATORS,
+    DATALOADERS,
+    DATASETS,
+    LOGGERS,
+    LOSSES,
+    METRICS,
+    MODELS,
+    OPTIMIZERS,
+    SCALERS,
+    SCHEDULERS,
+    TRAINERS,
+)
+
+__all__ = [
+    "ContextManager",
+    "COLLATORS",
+    "DATALOADERS",
+    "DATASETS",
+    "LOGGERS",
+    "LOSSES",
+    "METRICS",
+    "MODELS",
+    "OPTIMIZERS",
+    "SCALERS",
+    "SCHEDULERS",
+    "TRAINERS",
+]
---
+++ packages/tsts/tsts-0.4.0-py3-none-any.whl/tsts/loggers/builder.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from tsts.cfg import CfgNode as CN
+from tsts.core import LOGGERS, ContextManager
+from tsts.losses import Loss
+from tsts.metrics import Metric
+from tsts.models import Module
+
+from .logger import Logger
+
+__all__ = ["build_logger"]
+
+
+def build_logger(
+    model: Module,
+    losses: List[Loss],
+    metrics: List[Metric],
+    context_manager: ContextManager,
+    cfg: CN,
+) -> Logger:
+    logger_name = cfg.LOGGER.NAME
+    cls = LOGGERS[logger_name]
+    logger = cls.from_cfg(
+        model,
+        losses,
+        metrics,
+        context_manager,
+        cfg,
+    )
+    return logger
