fc1159eea7cbf3877a0a9b84fd959eac297d043c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:58 +0000

    Add package fastspider/1.0.31/fastspider-1.0.31-py3-none-any.whl

---
+++ packages/fastspider/fastspider-1.0.31-py3-none-any.whl/fastspider/core/base/light_base.py
@@ -0,0 +1,45 @@
+# encoding=utf-8
+"""
+Auth: coco369
+Email: 779598160@qq.com
+
+CreateTime: 2021/07/29
+
+Desc: fastspider核心爬虫LightSpider的基类代码
+"""
+from fastspider.http.request.request import Request
+
+
+class LightBase(object):
+
+	def start_requests(self):
+		"""
+			解析url地址
+		:return: 返回可迭代的yield Request()
+		"""
+		if hasattr(self, "start_urls") and not self.start_urls:
+			raise Exception("Spider 必须定义 start_urls")
+		for url in self.start_urls:
+			yield Request(url=url)
+
+	def parser(self, request, response):
+		"""
+			默认的解析函数, 解析响应response的内容
+		:param request: 请求对象
+		:param response: 响应对象
+		:return: 可不返回内容, 或者返回Item对象
+		"""
+		pass
+
+	def start_callback(self):
+		pass
+
+	def end_callback(self):
+
+		pass
+
+	@property
+	def name(self):
+		# 示例方法, 获取类名
+		return self.__class__.__name__
+
