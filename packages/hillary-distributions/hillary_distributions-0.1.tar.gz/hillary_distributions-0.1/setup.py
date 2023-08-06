bffeb46c904e188c362675f53c777dfdee21fb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:30 +0000

    Add package hillary-distributions/0.1/hillary_distributions-0.1.tar.gz

---
+++ packages/hillary-distributions/hillary_distributions-0.1.tar.gz/hillary_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='hillary_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hillary_distributions'],
+      zip_safe=False)
