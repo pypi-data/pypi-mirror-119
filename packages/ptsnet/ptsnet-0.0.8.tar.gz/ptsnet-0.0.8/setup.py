98859e634ebd3ea54c36caa69a204973adf5ccc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:48 +0000

    Add package ptsnet/0.0.8/ptsnet-0.0.8.tar.gz

---
+++ packages/ptsnet/ptsnet-0.0.8.tar.gz/ptsnet-0.0.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import os
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ptsnet",
+    version="0.0.8",
+    author="Gerardo Riano",
+    author_email="griano@utexas.edu",
+    description="Parallel Transient Simulation in Water Networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gandresr/PTSNET",
+    project_urls={
+        "Bug Tracker": "https://github.com/gandresr/PTSNET/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6"
+)
