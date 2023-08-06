59ec2be3f62bbeb571aa82cb9e8bee2695449706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:51 +0000

    Add package powerplayer/0.1.7/powerplayer-0.1.7.tar.gz

---
+++ packages/powerplayer/powerplayer-0.1.7.tar.gz/powerplayer-0.1.7/powerplayer.py
@@ -0,0 +1,57 @@
+'''
+TODO
+add playlist and asking to play another song one song ends
+So pplay add and pplay playlist
+pplay add adds a song to ~/.pplay/playlist.txt
+then pplay queue, takes them one by one and plays them
+'''
+from playsound import playsound
+import click
+from click_help_colors import HelpColorsGroup, HelpColorsCommand
+import re, requests, urllib.parse, urllib.request
+from bs4 import BeautifulSoup
+import os
+import vlc
+import pafy
+from time import sleep, time
+import os
+@click.group(
+    cls=HelpColorsGroup, help_headers_color="yellow", help_options_color="cyan")
+@click.version_option('0.1.7')
+def main():
+    """PowerPlayer - Play music in your terminal"""
+@main.command('playfromyt', help = 'Play music from youtube. Give the song name')
+@click.argument('song', nargs = -1)
+def playfromyt(song):
+    song = " ".join(song)
+    music_name = song
+    query_string = urllib.parse.urlencode({"search_query": music_name})
+    formatUrl = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r"watch\?v=(\S{11})", formatUrl.read().decode())
+    clip = requests.get("https://www.youtube.com/watch?v=" + "{}".format(search_results[0]))
+    clip2 = "https://www.youtube.com/watch?v=" + "{}".format(search_results[0])
+    inspect = BeautifulSoup(clip.content, "html.parser")
+    yt_title = inspect.find_all("meta", property="og:title")
+    for concatMusic1 in yt_title:
+        pass
+
+    
+    click.secho(clip2)
+    video = pafy.new(clip2)
+    length = video.length
+    best = video.getbestaudio()
+    
+    media = vlc.MediaPlayer(best.url)
+    click.secho(f'Playing...')
+    click.secho(concatMusic1['content'])
+
+    media.play()
+    start = time()
+    while (time() - start < length):
+        sleep(length - (time() - start))
+        a = click.prompt('Enter your next song or press Ctrl+C to exit')
+        playfromyt(a)
+ 
+
+if __name__ == '__main__':
+    main()
