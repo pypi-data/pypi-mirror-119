3b211027472651a05b71d0468aa6ae29e0b1585c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:52 +0000

    Add package cicdtest/1.53/cicdtest-1.53.tar.gz

---
+++ packages/cicdtest/cicdtest-1.53.tar.gz/cicdtest-1.53/buildpan/setting.py
@@ -0,0 +1,8 @@
+info = {
+    "HOST" : "35.225.89.124",
+    "PUSH_COMMIT_URL" : "http://35.225.89.124/push_commit",
+    "FETCH_LOG_URL" : "http://35.225.89.124/fetch_log",
+    "key" : "HB2xRCZSyKYMcTS9rm",
+    "secret_key" : "pQA5FMShe6deqr2b8Eq3GfDBmhEnn77C",
+    "version": "1.53"
+}
