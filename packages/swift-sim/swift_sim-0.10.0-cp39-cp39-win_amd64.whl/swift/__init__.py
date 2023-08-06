be2a42b7e78ef3db33e8d17728584521339a6646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:45 +0000

    Add package swift-sim/0.10.0/swift_sim-0.10.0-cp39-cp39-win_amd64.whl

---
+++ packages/swift-sim/swift_sim-0.10.0-cp39-cp39-win_amd64.whl/swift/__init__.py
@@ -0,0 +1,17 @@
+from swift.SwiftRoute import SwiftServer, SwiftSocket, start_servers
+from swift.SwiftElement import SwiftElement, Slider, Select, Checkbox, Radio, Button, Label
+from swift.Swift import Swift
+
+__all__ = [
+    'Swift',
+    'SwiftServer',
+    'SwiftSocket',
+    'start_servers',
+    'SwiftElement',
+    'Slider',
+    'Select',
+    'Checkbox',
+    'Radio',
+    'Button',
+    'Label'
+]
