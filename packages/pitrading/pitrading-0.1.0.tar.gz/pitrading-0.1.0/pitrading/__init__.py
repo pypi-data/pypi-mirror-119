f0862106c6a8401595a8fb05dd00c01be295014c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:04 +0000

    Add package pitrading/0.1.0/pitrading-0.1.0.tar.gz

---
+++ packages/pitrading/pitrading-0.1.0.tar.gz/pitrading-0.1.0/pitrading/__init__.py
@@ -0,0 +1,5 @@
+"""Top-level package for PiTrading."""
+
+__author__ = """saaltfiish"""
+__email__ = 'hujiazhen@piasset.net'
+__version__ = '0.1.0'
---
+++ packages/pitrading/pitrading-0.1.0.tar.gz/pitrading-0.1.0/tests/test_pitrading.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `pitrading` package."""
+
+
+import unittest
+
+from pitrading import pitrading
+
+
+class TestPitrading(unittest.TestCase):
+    """Tests for `pitrading` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
