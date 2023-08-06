8f2102e784032180dbaaf865072f41c0256379d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:50 +0000

    Add package colores/0.1.1/colores-0.1.1.tar.gz

---
+++ packages/colores/colores-0.1.1.tar.gz/colores-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['colores']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['colorama>=0.4.4,<0.5.0']
+
+setup_kwargs = {
+    'name': 'colores',
+    'version': '0.1.1',
+    'description': 'A Wrapper for Colorama',
+    'long_description': None,
+    'author': 'Eliaz Bobadilla',
+    'author_email': 'eliaz.bobadilladev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
