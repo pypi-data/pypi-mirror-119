1b37f433a089c81240f498d12af756ca5cbeffb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:09 +0000

    Add package designer/0.0.76/designer-0.0.76.tar.gz

---
+++ packages/designer/designer-0.0.76.tar.gz/designer-0.0.76/designer/__init__.py
@@ -0,0 +1,8 @@
+import pygame
+from designer.Director import *
+from designer.Animation import *
+from designer.DesignerObject import *
+from designer.colors import *
+
+
+GLOBAL_DIRECTOR = Director()
