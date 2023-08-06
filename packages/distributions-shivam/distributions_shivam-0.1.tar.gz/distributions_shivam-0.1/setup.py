1cc1505fd0e5101e443c0c9f8acd696d590e0b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:49 +0000

    Add package distributions-shivam/0.1/distributions_shivam-0.1.tar.gz

---
+++ packages/distributions-shivam/distributions_shivam-0.1.tar.gz/distributions_shivam-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_shivam',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_shivam'],
+      zip_safe=False)
