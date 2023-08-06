2b107f9533bcf701ba25f870fd5639ac340f01b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:34 +0000

    Add package pymasmovil/0.0.15/pymasmovil-0.0.15.tar.gz

---
+++ packages/pymasmovil/pymasmovil-0.0.15.tar.gz/pymasmovil-0.0.15/pymasmovil/models/order_item_status.py
@@ -0,0 +1,4 @@
+class OrderItemStatus():
+    available_status = ["Pendiente", "Esperando para enviar", "Enviada", "Completada",
+                        "Error", "Esperando para cancelar", "Cancelada", "Cancelando",
+                        "Ready for Service"]
