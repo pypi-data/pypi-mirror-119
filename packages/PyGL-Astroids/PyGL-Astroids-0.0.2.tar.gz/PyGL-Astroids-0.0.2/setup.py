850377a5b8b97c7eee3242ad96d4f837ea0ca0c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:32 +0000

    Add package PyGL-Astroids/0.0.2/PyGL-Astroids-0.0.2.tar.gz

---
+++ packages/PyGL-Astroids/PyGL-Astroids-0.0.2.tar.gz/PyGL-Astroids-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as desc_file:
+    long_description = desc_file.read()
+
+setuptools.setup(
+    name="PyGL-Astroids",
+    version="0.0.2",
+    author="Zach Pierog",
+    author_email="woldstn@live.com",
+    description="Asteroids clone written using pyglet.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Woldstn/PyGL-Astroids",
+    project_urls={
+        "Bug Tracker": "https://github.com/Woldstn/PyGL-Astroids/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=['pyglet'],
+    python_requires=">=3.6",
+)
