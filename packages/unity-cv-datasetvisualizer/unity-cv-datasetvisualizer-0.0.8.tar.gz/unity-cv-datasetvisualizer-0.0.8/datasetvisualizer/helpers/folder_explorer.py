73a29fa1578440adc461cc97a2ef43559309d7bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:07 +0000

    Add package unity-cv-datasetvisualizer/0.0.8/unity-cv-datasetvisualizer-0.0.8.tar.gz

---
+++ packages/unity-cv-datasetvisualizer/unity-cv-datasetvisualizer-0.0.8.tar.gz/unity-cv-datasetvisualizer-0.0.8/datasetvisualizer/helpers/folder_explorer.py
@@ -0,0 +1,20 @@
+﻿import sys
+from PySide2 import QtCore
+from PySide2.QtWidgets import QApplication, QFileDialog, QWidget
+
+if not QApplication.instance():
+    app = QApplication(sys.argv)
+else:
+    app = QApplication.instance()
+
+dialog = QFileDialog()
+dialog.setFileMode(QFileDialog.Directory)
+dialog.setOption(QFileDialog.DontUseNativeDialog, True)
+dialog.setWindowState(dialog.windowState() & ~QtCore.Qt.WindowMinimized | QtCore.Qt.WindowActive)
+dialog.activateWindow()
+dialog.setWindowFlags(dialog.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
+dialog.show()
+
+if dialog.exec_():
+    fileName = dialog.selectedFiles()
+    print(fileName[0])
