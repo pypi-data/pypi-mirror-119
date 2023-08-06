7fea9d58433dd8312a525251ee81d070e2a4018b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:10 +0000

    Add package enot-lite/0.2.0rc1/enot_lite-0.2.0rc1-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/enot-lite/enot_lite-0.2.0rc1-cp36-cp36m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/enot_lite/__init__.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+try:
+    # Importing torch after ORT ld_preload leads to crash.
+    # To avoid crash we must try to load torch before.
+    import torch
+except ImportError:
+    pass
+
+
+def _get_version() -> str:
+    with Path(__file__).parent.joinpath('VERSION').open('r') as version_file:
+        version = version_file.read().strip()
+
+    return version
+
+
+__version__ = _get_version()
