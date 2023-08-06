12eba71db39ef869e960d2ae3cdab935438669a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:31 +0000

    Add package gaussian-binomial-probabilities/1.0/gaussian_binomial_probabilities-1.0.tar.gz

---
+++ packages/gaussian-binomial-probabilities/gaussian_binomial_probabilities-1.0.tar.gz/gaussian_binomial_probabilities-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gaussian_binomial_probabilities',
+      version='1.0',
+      description='Gaussian and Binomial Distributions',
+      packages=['gaussian_binomial_probabilities'],
+      author = 'Angel Ivanov',
+      author_email = 'angel.iwanov@gmail.com',
+      zip_safe=False)
