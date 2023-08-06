83b71c3808e88032da2edd01763294026483004c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:11 +0000

    Add package easyfed/0.1.6/easyfed-0.1.6.tar.gz

---
+++ packages/easyfed/easyfed-0.1.6.tar.gz/easyfed-0.1.6/easyfed/effront/FModel/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Clients(models.Model):
+    CID= models.AutoField(primary_key=True)
+    CName=models.CharField(max_length=300)
+    Key=models.CharField(max_length=300,default='')
+    Status=models.IntegerField(default=0)
