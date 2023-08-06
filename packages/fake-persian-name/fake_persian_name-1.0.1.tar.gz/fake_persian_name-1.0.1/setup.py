1cbcad5f6d2e0e7a7856135a622156e9f439117d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:03 +0000

    Add package fake-persian-name/1.0.1/fake_persian_name-1.0.1.tar.gz

---
+++ packages/fake-persian-name/fake_persian_name-1.0.1.tar.gz/fake_persian_name-1.0.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'fake_persian_name',
+  packages = ['fake_persian_name'],
+  version = '1.0.1',
+  license='MIT',
+  description = 'A package to generate fake persian name',
+  author = 'Erfan Rahmati',
+  author_email = 'ErfanRht2005@gmail.com',
+  url = 'https://github.com/ErfanRht/fake-persian-name',
+  download_url = 'https://github.com/ErfanRht/fake_persian_name/blob/master/dist/fake_persian_name-1.0.1.tar.gz',
+  keywords = ['fake', 'persian', 'name', 'persian name', 'fake name'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
