b91ff62e0da694af602d7a6b6fc0d7afa1dca1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:34 +0000

    Add package zeversolarlocal/1.1.0/zeversolarlocal-1.1.0-py3-none-any.whl

---
+++ packages/zeversolarlocal/zeversolarlocal-1.1.0-py3-none-any.whl/zeversolarlocal/__init__.py
@@ -0,0 +1,12 @@
+"""zeversolarlocal"""
+
+
+__version__ = "1.1.0"
+
+from zeversolarlocal.api import (  # noqa
+    ZeverError,
+    ZeverTimeout,
+    default_url,
+    inverter_id,
+    solardata,
+)
