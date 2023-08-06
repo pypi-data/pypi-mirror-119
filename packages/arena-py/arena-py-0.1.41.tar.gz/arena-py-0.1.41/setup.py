070bac6726bca728310d48bf1f79936e88ab5d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:36 +0000

    Add package arena-py/0.1.41/arena-py-0.1.41.tar.gz

---
+++ packages/arena-py/arena-py-0.1.41.tar.gz/arena-py-0.1.41/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="arena-py",
+    version="0.1.41",
+    author="Conix Research Center",
+    author_email="info@conix.io",
+    license="BSD 3-clause \"New\" or \"Revised License\"",
+    description="Draw objects and run programs in the ARENA using Python!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/conix-center/ARENA-py",
+    packages=setuptools.find_packages(),
+    scripts=[
+        'scripts/arena-py-signout',
+        'scripts/arena-py-sub',
+        'scripts/arena-py-permissions',
+        'scripts/arena-py-pub',
+    ],
+    install_requires=[
+        "aiohttp>=3.7.4",
+        "paho-mqtt~=1.5.0",
+        "requests~=2.23.0",
+        "webcolors~=1.3",
+        "google_auth_oauthlib~=0.4.4",
+        "google-auth~=1.32.1",
+        "PyJWT~=2.0.0",
+        "numpy>=1.19.5",
+        "scipy>=1.5.4"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
