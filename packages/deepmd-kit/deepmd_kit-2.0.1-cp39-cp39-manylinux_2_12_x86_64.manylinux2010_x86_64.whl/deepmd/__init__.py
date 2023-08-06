807aaae5a0b88da7d7db8aeafb34925ab716e934
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:50 +0000

    Add package deepmd-kit/2.0.1/deepmd_kit-2.0.1-cp39-cp39-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/deepmd-kit/deepmd_kit-2.0.1-cp39-cp39-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/deepmd/__init__.py
@@ -0,0 +1,27 @@
+"""Root of the deepmd package, exposes all public classes and submodules."""
+
+import deepmd.utils.network as network
+
+from . import cluster, descriptor, fit, loss, utils
+from .env import set_mkl
+from .infer import DeepEval, DeepPotential
+from .infer.data_modifier import DipoleChargeModifier
+
+set_mkl()
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    from .__about__ import __version__
+
+__all__ = [
+    "descriptor",
+    "fit",
+    "loss",
+    "utils",
+    "cluster",
+    "network",
+    "DeepEval",
+    "DeepPotential",
+    "DipoleChargeModifier",
+]
