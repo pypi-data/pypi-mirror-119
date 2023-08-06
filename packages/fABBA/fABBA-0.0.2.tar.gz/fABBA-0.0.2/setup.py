e5b4a4f78eaf65e6d1c36744868e1f32f0da12a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:53 +0000

    Add package fABBA/0.0.2/fABBA-0.0.2.tar.gz

---
+++ packages/fABBA/fABBA-0.0.2.tar.gz/fABBA-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "RELEASE.md").read_text()
+
+setup(
+    name="fABBA",
+    packages=['fABBA'],
+    version="0.0.2",
+    long_description=long_description,
+    author="Stefan Guettel, Xinye Chen",
+    author_email="stefan.guettel@manchester.ac.uk",
+    description="An efficient aggregation based symbolic representation",
+    long_description_content_type='text/markdown',
+    url="https://github.com/nla-group/fABBA",
+    # packages=setuptools.find_packages(),
+    install_requires=['numpy', 'cv2'],
+    license='BSD 3-Clause'
+)
