b9ebdb0d194c219ba7999d9c4630d8ca7dacc35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:09 +0000

    Add package RoboticProcessAutomation/0.0.24/RoboticProcessAutomation-0.0.24.tar.gz

---
+++ packages/RoboticProcessAutomation/RoboticProcessAutomation-0.0.24.tar.gz/RoboticProcessAutomation-0.0.24/README.md
@@ -0,0 +1,4 @@
+# RoboticProcessAutomation
+Some handy functions especially useful while building Robotic Process Automations with Python.
+
+[GitHub](https://github.com/bartmazur90/RoboticProcessAutomation/)
---
+++ packages/RoboticProcessAutomation/RoboticProcessAutomation-0.0.24.tar.gz/RoboticProcessAutomation-0.0.24/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RoboticProcessAutomation',
+    version='0.0.24',
+    description='Robotic Process Automation Python Package',
+    long_description='Some handy functions especially useful while building Robotic Process Automations with Python.',
+    url='https://github.com/bartmazur90/RoboticProcessAutomation',
+    author='Bartosz Mazur',
+    author_email='bartmazur90@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pillow',
+        'opencv-python',
+        'pyautogui',
+        'pyscreeze'
+    ],
+    keywords=['python', 'RPA', 'Robotic', 'Process', 'Automation', 'Image Recognition'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent'
+    ],
+)
