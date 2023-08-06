da8f506c7a7deeb6b4f4e737a77b9520c5359c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:22 +0000

    Add package posemaro/1.0.3/posemaro-1.0.3-py3-none-any.whl

---
+++ packages/posemaro/posemaro-1.0.3-py3-none-any.whl/datumaro/cli/commands/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2019-2021 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+# pylint: disable=redefined-builtin
+
+from . import (
+    add, checkout, commit, convert, create, diff, explain, export, filter, info,
+    log, merge, remove, stats, status, transform, validate,
+)
