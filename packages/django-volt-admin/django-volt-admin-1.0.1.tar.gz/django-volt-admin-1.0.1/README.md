b7245ab14dac42d842f8cf88282d08ea2521aa81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:08 +0000

    Add package django-volt-admin/1.0.1/django-volt-admin-1.0.1.tar.gz

---
+++ packages/django-volt-admin/django-volt-admin-1.0.1.tar.gz/django-volt-admin-1.0.1/README.md
@@ -0,0 +1,43 @@
+# Django Volt Admin
+Django Volt is a django admin customization for https://github.com/themesberg/volt-bootstrap-5-dashboard
+
+Samples
+----------
+![alt text](https://raw.githubusercontent.com/adams-okode/django-volt-admin/main/docs/login.png)
+
+![alt text](https://raw.githubusercontent.com/adams-okode/django-volt-admin/main/docs/dashboard.png)
+
+Quick start
+-----------
+
+1. Add "volt" to your INSTALLED_APPS setting like this::
+    ```python
+    INSTALLED_APPS = [
+        ...
+        'volt',
+    ]
+    ```
+
+2. Include Templates from the volt admin customization
+    ```python
+    TEMPLATES = [
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [
+                BASE_DIR /'volt/templates',
+            ],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
+    ```
+
+3. Start the development server and visit http://127.0.0.1:8000/admin/
+   to view (you'll need the Admin app enabled).
---
+++ packages/django-volt-admin/django-volt-admin-1.0.1.tar.gz/django-volt-admin-1.0.1/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, find_packages
+
+extras_require = {
+    'lint': [
+        'pep8',
+    ],
+    'dev': [
+        'bumpversion>=0.5.3,<1',
+        'wheel',
+        'twine',
+    ],
+}
+
+extras_require['dev'] = (
+    extras_require['dev'] +
+    extras_require['lint']
+)
+
+setup(
+    name='django-volt-admin',
+    version='1.0.1',
+    description='Django admin implementation of volt admin dashboard',
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/adams-okode/django-volt-admin',
+    author='Adams Okode',
+    author_email='adamsokode@gmail.com',
+    license='MIT',
+    packages=find_packages(
+        exclude=['tests', 'tests.*', 'licenses', 'requirements']),
+    install_requires=[
+        'Django>=2.2',
+    ],
+    extras_require=extras_require,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
