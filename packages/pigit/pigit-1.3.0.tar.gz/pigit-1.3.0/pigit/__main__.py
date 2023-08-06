1a3bbf98c28e56dd2ee310d9374a99434f9d784e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:18 +0000

    Add package pigit/1.3.0/pigit-1.3.0.tar.gz

---
+++ packages/pigit/pigit-1.3.0.tar.gz/pigit-1.3.0/pigit/__main__.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+# For `python -m pigit`.
+# When you run a python module directly, the interpreter
+# looks for the `__main__.py` file in the module.
+
+from pigit import main
+
+main()
