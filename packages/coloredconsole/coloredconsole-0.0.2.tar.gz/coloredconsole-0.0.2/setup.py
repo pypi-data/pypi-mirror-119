b07121b37c509c8f602d06ff2f10cdafed9cf1cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:49 +0000

    Add package coloredconsole/0.0.2/coloredconsole-0.0.2.tar.gz

---
+++ packages/coloredconsole/coloredconsole-0.0.2.tar.gz/coloredconsole-0.0.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['coloredconsole']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'coloredconsole',
+    'version': '0.0.2',
+    'description': 'Easy to use console coloring tool.',
+    'long_description': None,
+    'author': 'Andrew Bordis',
+    'author_email': 'andrewbordis@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
