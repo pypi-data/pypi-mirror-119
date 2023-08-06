94cf17024869f5f1d9dd27225db6455b41c74b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:44 +0000

    Add package overloads/1.5/overloads-1.5.tar.gz

---
+++ packages/overloads/overloads-1.5.tar.gz/overloads-1.5/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="overloads",
+    version="1.5",
+    author="jian.zhang",
+    author_email="zj19180525254@163.com",
+    description="Implement the function overload in Python by the decorator named @overloads",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhangjian-ai/pypi/blob/master/packaging_tutorial/overload/overload/overload.py",
+    packages=setuptools.find_packages(include=['overloads']),  #
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+# 在当前目录下打包并上传
+# python3 setup.py sdist
+# python3 setup.py sdist upload
