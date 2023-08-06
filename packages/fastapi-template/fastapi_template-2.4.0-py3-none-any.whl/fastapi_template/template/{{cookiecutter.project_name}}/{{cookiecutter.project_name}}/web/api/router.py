ca09b60f6fe5e7dbf196c67cad659e41984f4c5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:06 +0000

    Add package fastapi-template/2.4.0/fastapi_template-2.4.0-py3-none-any.whl

---
+++ packages/fastapi-template/fastapi_template-2.4.0-py3-none-any.whl/fastapi_template/template/{{cookiecutter.project_name}}/{{cookiecutter.project_name}}/web/api/router.py
@@ -0,0 +1,33 @@
+from fastapi.routing import APIRouter
+
+{%- if cookiecutter.enable_routers == "True" %}
+from {{cookiecutter.project_name}}.web.api import echo
+{%- if cookiecutter.db_info.name != "none" %}
+{%- if cookiecutter.add_dummy == 'True' %}
+from {{cookiecutter.project_name}}.web.api import dummy
+{%- endif %}
+{%- endif %}
+{%- if cookiecutter.enable_redis == "True" %}
+from {{cookiecutter.project_name}}.web.api import redis
+{%- endif %}
+{%- endif %}
+{%- if cookiecutter.self_hosted_swagger == "True" %}
+from {{cookiecutter.project_name}}.web.api import docs
+{%- endif %}
+
+
+api_router = APIRouter()
+{%- if cookiecutter.self_hosted_swagger == "True" %}
+api_router.include_router(docs.router)
+{%- endif %}
+{%- if cookiecutter.enable_routers == "True" %}
+api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
+{%- if cookiecutter.db_info.name != "none" %}
+{%- if cookiecutter.add_dummy == 'True' %}
+api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
+{%- endif %}
+{%- endif %}
+{%- if cookiecutter.enable_redis == "True" %}
+api_router.include_router(redis.router, prefix="/redis", tags=["redis"])
+{%- endif %}
+{%- endif %}
