9f08e89fa2c40d1fd69f3b0f60631073c25954d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:40 +0000

    Add package async-scrape/0.1.5/async-scrape-0.1.5.tar.gz

---
+++ packages/async-scrape/async-scrape-0.1.5.tar.gz/async-scrape-0.1.5/async_scrape/libs/base_scrape.py
@@ -0,0 +1,66 @@
+from datetime import datetime, timedelta
+from pypac import PACSession, get_pac
+import sys
+
+
+class BaseScrape:
+    def __init__(self,
+        use_proxy:bool=False,
+        proxy:str=None,
+        pac_url:str=None
+        ):
+        """Class for scrapping webpages
+        
+        args:
+        ----
+        use_proxy - bool:False - should a proxy be used
+        proxy - str:None - what is the address of the proxy ONLY VALID IF
+            PROXY IS TRUE
+        pac_Url - str:None - the location of the pac information ONLY VALID IF
+            PROXY IS TRUE
+        """
+        self.pages_scraped = 0
+        self.total_to_scrape = 0
+        self.job_start = None
+        self.job_end = None
+        self.time_marks = []
+        self.use_proxy = use_proxy
+        self.proxy = proxy
+        self.pac = get_pac(url=pac_url) if self.use_proxy else None
+        self.pac_session = None
+
+    def reset_pages_scraped(self):
+        self.pages_scraped = 0
+        self.time_marks = [datetime.now()]
+
+    def increment_pages_scraped(self):
+        self.pages_scraped += 1
+        #Add time mark
+        self.time_marks.append(datetime.now())
+        #Calc loop time
+        loop_time_elapsed = self.time_marks[-1] - self.time_marks[-2]
+        #Calc estimated finish time
+        total_time_elapsed = (datetime.now() - self.job_start).seconds
+        est_total_time_s = total_time_elapsed + (self.total_to_scrape * total_time_elapsed / self.pages_scraped)
+        #Output
+        sys.stdout.write(f"\rprocessed -> {self.pages_scraped}/{self.total_to_scrape} - loop time {loop_time_elapsed} - est finish {datetime.now() + timedelta(seconds=est_total_time_s)}\n")
+        sys.stdout.flush()
+
+    def throttle_tasks(self):
+        """Introduces sleep between tasks to slow them to a throttled rate"""
+        pass
+
+    def start_job(self):
+        self.job_start = datetime.now()
+        self.time_marks = [self.job_start]
+        self.job_end = None
+
+    def end_job(self):
+        self.job_end = datetime.now()
+        runtime = self.job_end - self.job_start
+        print(f"Job completed in {runtime}")
+
+    def _get_pac_session(self):
+        if not self.pac_session:
+            self.pac_session = PACSession(self.pac)
+        return self.pac_session
