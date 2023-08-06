9985c9c1c3abee8873d436042074dd44fb3b31ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:31 +0000

    Add package fABBA/0.3.6/fABBA-0.3.6.tar.gz

---
+++ packages/fABBA/fABBA-0.3.6.tar.gz/fABBA-0.3.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+from pathlib import Path
+from Cython.Build import cythonize
+import numpy
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="fABBA",
+    packages=setuptools.find_packages(),
+    version="0.3.6",
+    setup_requires=["numpy"],
+    ext_modules=cythonize(["fABBA/*.pyx"], include_path=['fABBA/']),
+    include_dirs=[numpy.get_include()],
+    long_description=long_description,
+    author="Stefan Guettel, Xinye Chen",
+    author_email="stefan.guettel@manchester.ac.uk",
+    description="An efficient aggregation based symbolic representation",
+    long_description_content_type='text/markdown',
+    url="https://github.com/nla-group/fABBA",
+    install_requires=['numpy'],
+    license='BSD 3-Clause'
+)
