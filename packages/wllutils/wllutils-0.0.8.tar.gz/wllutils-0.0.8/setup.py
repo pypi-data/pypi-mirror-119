a48a892ca196ca368069b1d880d2e27d9b186387
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:17 +0000

    Add package wllutils/0.0.8/wllutils-0.0.8.tar.gz

---
+++ packages/wllutils/wllutils-0.0.8.tar.gz/wllutils-0.0.8/setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+import os
+
+REQUIRES = [
+    'pandas',
+    'numpy',
+    'tqdm',
+    'scikit-learn'
+]
+
+setup(
+    name='wllutils',
+    version='0.0.8',
+    description=('utils of wll' ),
+    author='weiliulei',
+    author_email='18500964455@163.com' ,
+    maintainer='weiliulei',
+    maintainer_email='18500964455@163.com' ,
+    license = 'MIT',
+    packages=find_packages(),
+    platforms=['all',],
+    # url='https://github.com/Wei-Liulei/utils',
+    install_requires=REQUIRES,
+    pbr=True
+)
+
+# pip install twine
+# 打包 tar.gz ,whl,  windows 下 bdist_wininst
+# python .\setup.py sdist bdist_wheel # bdist_wininst  windows
+# python ./setup.py sdist bdist_wheel # bdist_wininst  linux
+# twine upload dist/*
+
+
+# 免密上传
+# 用户目录下 .pypirc 文件 
+# [distutils]
+# index-servers =
+#   pypi
+# 
+# [pypi]
+# repository = https://upload.pypi.org/legacy/
+# username: ********
+# password: ********
+
