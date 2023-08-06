5cfe12d0d0d1b3a9bed0958f23014d29c82180a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:33 +0000

    Add package fake-persian-name/1.0.6/fake_persian_name-1.0.6.tar.gz

---
+++ packages/fake-persian-name/fake_persian_name-1.0.6.tar.gz/fake_persian_name-1.0.6/fake_persian_name/fake_name.py
@@ -0,0 +1,38 @@
+from importlib import resources
+import random
+import io
+import os
+
+def generate_name(gender):
+    if gender == "male":
+        gender = 0
+    elif gender == "female":
+        gender = 1
+    elif gender == "random":
+        gender = random.randint(0, 1)
+    else:
+        return None
+
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    files = ["name_boy.txt","name_girl.txt"]
+    def first_name(gender):
+        with resources.open_text('fake_persian_name',files[gender]) as file:
+            names = file.read().split("\n")
+        first_name = names[random.randrange(len(names))]
+
+        return first_name
+    
+    def last_name():
+        with resources.open_text(files[gender], resources) as file:
+            names = file.read().split("\n")
+        last_name = names[random.randrange(len(names))]+"ی"
+
+        return last_name
+    
+    first = first_name(gender)
+    last = last_name()
+    name = first + " " + last
+
+    return name
+
+generate_name("male")
