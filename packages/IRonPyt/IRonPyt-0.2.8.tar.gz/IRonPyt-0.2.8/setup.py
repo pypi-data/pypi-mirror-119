b3add3155e64ee2022cdb228de751c2400e4d6fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:11 +0000

    Add package IRonPyt/0.2.8/IRonPyt-0.2.8.tar.gz

---
+++ packages/IRonPyt/IRonPyt-0.2.8.tar.gz/IRonPyt-0.2.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+from setuptools import setup, find_packages
+setuptools.setup(
+    name="IRonPyt",
+    version="0.2.8",
+    author="berre",
+    packages=find_packages(),
+    package_data={'': ['*.dll'], '': ['src/*.so'], '': ['data/*.cvs']},  # This is the most important line.
+    zip_safe=False,
+    include_package_data=True,
+    author_email="berreergunn@gmail.com",
+    description="A sample test package",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gituser/test-tackage",
+    license='MIT',
+    python_requires='>=2',
+    install_requires=["pywin32 >= 1.0;platform_system=='Windows'","matplotlib",'numpy','pandas','sklearn']
+)
