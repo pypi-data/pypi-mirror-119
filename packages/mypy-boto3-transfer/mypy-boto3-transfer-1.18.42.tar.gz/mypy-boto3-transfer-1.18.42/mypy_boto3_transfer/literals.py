9fcafcc5c4d8f4c48f3f56383073e25a6f4ea3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:47 +0000

    Add package mypy-boto3-transfer/1.18.42/mypy-boto3-transfer-1.18.42.tar.gz

---
+++ packages/mypy-boto3-transfer/mypy-boto3-transfer-1.18.42.tar.gz/mypy-boto3-transfer-1.18.42/mypy_boto3_transfer/literals.py
@@ -0,0 +1,49 @@
+"""
+Type annotations for transfer service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_transfer/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_transfer.literals import CustomStepStatusType
+
+    data: CustomStepStatusType = "FAILURE"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "CustomStepStatusType",
+    "DomainType",
+    "EndpointTypeType",
+    "ExecutionErrorTypeType",
+    "ExecutionStatusType",
+    "HomeDirectoryTypeType",
+    "IdentityProviderTypeType",
+    "ListServersPaginatorName",
+    "OverwriteExistingType",
+    "ProtocolType",
+    "StateType",
+    "WorkflowStepTypeType",
+)
+
+
+CustomStepStatusType = Literal["FAILURE", "SUCCESS"]
+DomainType = Literal["EFS", "S3"]
+EndpointTypeType = Literal["PUBLIC", "VPC", "VPC_ENDPOINT"]
+ExecutionErrorTypeType = Literal["PERMISSION_DENIED"]
+ExecutionStatusType = Literal["COMPLETED", "EXCEPTION", "HANDLING_EXCEPTION", "IN_PROGRESS"]
+HomeDirectoryTypeType = Literal["LOGICAL", "PATH"]
+IdentityProviderTypeType = Literal["API_GATEWAY", "AWS_DIRECTORY_SERVICE", "SERVICE_MANAGED"]
+ListServersPaginatorName = Literal["list_servers"]
+OverwriteExistingType = Literal["FALSE", "TRUE"]
+ProtocolType = Literal["FTP", "FTPS", "SFTP"]
+StateType = Literal["OFFLINE", "ONLINE", "STARTING", "START_FAILED", "STOPPING", "STOP_FAILED"]
+WorkflowStepTypeType = Literal["COPY", "CUSTOM", "DELETE", "TAG"]
