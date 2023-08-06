8a6233482c532fb41f74a886707ad7cd3157cf4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:50 +0000

    Add package py21/1.6.0/py21-1.6.0.tar.gz

---
+++ packages/py21/py21-1.6.0.tar.gz/py21-1.6.0/py21/__init__.py
@@ -0,0 +1,11 @@
+from py21.card import *
+from py21.deck import *
+from py21.game import *
+from py21.hand import *
+from py21.player import *
+from py21.cli import cli_main
+import py21.strategies
+import py21.utils
+
+
+__version__ = "1.6.0"
