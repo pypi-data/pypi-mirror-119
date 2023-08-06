349fd4d998fa2ff9126bd05c5ee5d346f7f607bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:39 +0000

    Add package blob-creator/3.2.0/blob_creator-3.2.0.tar.gz

---
+++ packages/blob-creator/blob_creator-3.2.0.tar.gz/blob_creator-3.2.0/setup.py
@@ -0,0 +1,35 @@
+"""
+Setup Module for Blob Creator
+
+Author: Michael Kohlegger
+Date: 2021-09
+"""
+
+import setuptools
+from src.blob_creator.requirements import hard_requirements
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='blob_creator',
+    version='3.2.0',
+    author="Michael Kohlegger",
+    author_email="michael@datenberge.org",
+    description="Package to create dummy datasets for analysis tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/mckoh/blob_creator",
+    project_urls={
+        "Bug Tracker": "https://github.com/mckoh/blob_creator/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=hard_requirements
+ )
