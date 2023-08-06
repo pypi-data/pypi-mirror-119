33fa72af7a708357cbe9af441f3cf3a406893a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:48 +0000

    Add package c-logger/0.0.1/c_logger-0.0.1.tar.gz

---
+++ packages/c-logger/c_logger-0.0.1.tar.gz/c_logger-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="c_logger",
+    version="0.0.1",
+    description='Colored log output',
+    author='Andrey Bolshakov',
+    author_email='frost.dat@gmail.com',
+    packages=['c_logger'],
+    keywords=['python', 'logger']
+)
