fe47e1c4648b257b5676d34878772c39921407ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:13 +0000

    Add package unacastclient/0.0.18/unacastclient-0.0.18-py2.py3-none-any.whl

---
+++ packages/unacastclient/unacastclient-0.0.18-py2.py3-none-any.whl/unacastclient/address_component_builder.py
@@ -0,0 +1,46 @@
+from .address_component import AddressComponent
+from syncer import sync
+
+from unacastclient.unacast.maps.v1 import AddressComponent as v1_AddressComponent, ComponentKind
+from syncer import sync
+
+from unacastclient.unacast.maps.v1 import AddressComponent as v1_AddressComponent, ComponentKind
+from .address_component import AddressComponent
+
+
+class AddressComponentBuilder(object):
+  
+    def __init__(self, catalog: 'Catalog', component: str):
+        self._catalog = catalog
+        self.address_component_operator_service = catalog._client.address_component_operator_service
+
+        self._component = component
+        self._kind = ComponentKind.FREE_FORM_UNSPECIFIED
+        self._display_name = None
+        self._short_name = None
+
+    def with_display_name(self, display_name: str):
+      self._display_name = display_name
+      return self
+
+    def with_short_name(self, short_name: str):
+      self._short_name = short_name
+      return self
+
+    def with_kind(self, kind: ComponentKind):
+      self._kind = kind
+      return self
+
+    def create(self) -> AddressComponent:
+      res: v1_AddressComponent = sync(
+            self.address_component_operator_service.create_address_component(
+              catalog_id=self._catalog.id,
+              component=self._component, 
+              short_name=self._short_name,
+              display_name=self._display_name,
+              kind=self._kind
+            )
+      )
+      return AddressComponent(self._catalog, res)
+    
+    
