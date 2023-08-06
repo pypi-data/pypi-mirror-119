96eaa2e75216e0d0ab39ad3f01cf77716c48bc42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:45:06 +0000

    Add package physbiblio/1.8.3/physbiblio-1.8.3.tar.gz

---
+++ packages/physbiblio/physbiblio-1.8.3.tar.gz/physbiblio-1.8.3/physbiblio/version.py
@@ -0,0 +1,6 @@
+__version__ = "1.8.3"
+__version_date__ = "14/09/2021"
+
+__recent_changes__ = """<br>* fix index in messages when running searchOAIUpdates<br>
+* fix bugs when importing for tex and the entry exists but with a different key<br>
+"""
