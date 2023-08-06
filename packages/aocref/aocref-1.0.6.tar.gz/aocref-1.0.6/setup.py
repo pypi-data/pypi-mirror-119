32f49629deadaaefbfd52eba846f8df3eb69123d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:07 +0000

    Add package aocref/1.0.6/aocref-1.0.6.tar.gz

---
+++ packages/aocref/aocref-1.0.6.tar.gz/aocref-1.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aocref',
+    version='1.0.6',
+    description='Age of Empires reference data',
+    url='https://github.com/siegeengineers/aoc-reference-data',
+    packages=find_packages(),
+    package_data={'aocref': [
+        'data/datasets/*',
+        'data/constants.json'
+    ]},
+    install_requires=[
+        'requests==2.22.0',
+        'ruamel.yaml==0.16.12'
+    ]
+)
