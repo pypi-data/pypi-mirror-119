961a9f19af96feabc30f43e941cdadd222c1f0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:54 +0000

    Add package edc-auth/0.3.20/edc_auth-0.3.20-py2.py3-none-any.whl

---
+++ packages/edc-auth/edc_auth-0.3.20-py2.py3-none-any.whl/edc_auth/__init__.py
@@ -0,0 +1,18 @@
+from .codenames import screening
+from .default_group_names import (
+    ACCOUNT_MANAGER,
+    ADMINISTRATION,
+    AUDITOR,
+    CELERY_MANAGER,
+    CLINIC,
+    CLINIC_SUPER,
+    DISPENSING,
+    EVERYONE,
+    EXPORT,
+    PHARMACY,
+    PII,
+    PII_VIEW,
+    REVIEW,
+    SCREENING,
+)
+from .default_groups import default_groups
