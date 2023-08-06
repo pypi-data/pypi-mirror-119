b77a440367be54f6b1cc76405e2944c3cc5ac476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:05 +0000

    Add package caph1993-posets/2.0.7/caph1993-posets-2.0.7.tar.gz

---
+++ packages/caph1993-posets/caph1993-posets-2.0.7.tar.gz/caph1993-posets-2.0.7/cp93posets/poset_wbools.py
@@ -0,0 +1,38 @@
+from typing import Any, Callable
+
+from numpy.core.fromnumeric import nonzero
+
+
+class WBool:
+    'Boolean that explains why_false and calls hook when failed to assert'
+    value: Any
+    why_false: str
+    hook: Callable[[], Any]
+
+    def __nonzero__(self):
+        if hasattr(self.value, '__len__'):
+            return nonzero(len(self.value))
+        return nonzero(self.value)
+
+    def assert_explain(self):
+        if not self:
+            self.hook()
+            raise AssertionError(self.why_false)
+
+    def __str__(self):
+        return 'True' if self else f'False: {self.why_false}'
+
+
+class WBools:
+    'Class with WBool factory and describe methods'
+
+    def _wbool(self, value: bool, why_false=None):
+        'Boolean that describes self when failed to require'
+        r = WBool()
+        r.value = value
+        r.why_false = why_false
+        r.hook = lambda: self.describe()
+        return r
+
+    def describe(self):
+        pass
