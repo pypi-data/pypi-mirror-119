f1da28b069d6968b5397c5eed098da5872ac6499
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:09 +0000

    Add package Frepap-Alternativa2/0.3/Frepap_Alternativa2-0.3.tar.gz

---
+++ packages/Frepap-Alternativa2/Frepap_Alternativa2-0.3.tar.gz/Frepap_Alternativa2-0.3/Frepap_Alternativa2/__init__.py
@@ -0,0 +1,4 @@
+
+import random
+# from Frepap_Altenativa2 import funca
+# from funca import *
---
+++ packages/Frepap-Alternativa2/Frepap_Alternativa2-0.3.tar.gz/Frepap_Alternativa2-0.3/Frepap_Alternativa2/funca.py
@@ -0,0 +1,12 @@
+import random
+
+
+def add_one(number):
+    return print(number + 1)
+
+def dimelo():
+    return print('habla')
+
+def numero():
+    return print(random.randint(1,100))
+
