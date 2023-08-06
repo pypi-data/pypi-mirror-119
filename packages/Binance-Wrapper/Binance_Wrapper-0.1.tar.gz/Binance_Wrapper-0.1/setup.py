c89e55e60bff427baba15fd3638b82a6135bb563
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:44 +0000

    Add package Binance-Wrapper/0.1/Binance_Wrapper-0.1.tar.gz

---
+++ packages/Binance-Wrapper/Binance_Wrapper-0.1.tar.gz/Binance_Wrapper-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Binance_Wrapper',
+      version='0.1',
+      description='The FUnctions to Make using the Binance API easier and cleaner',
+      url='https://github.com/mario-likes-camels/Binance_Wrapper',
+      author='Patrick',
+      author_email='hi_in@hotmail.com',
+      license='MIT',
+      packages=['Binance_Wrapper'],
+      zip_safe=False)
