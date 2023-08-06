cfd952c0ca30ab82a1ba7df38f14b9c4b978b769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:53 +0000

    Add package novelsave/0.7.3/novelsave-0.7.3.tar.gz

---
+++ packages/novelsave/novelsave-0.7.3.tar.gz/novelsave-0.7.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.readlines()
+
+setup(
+    install_requires=requirements,
+    packages=find_packages(),
+)
