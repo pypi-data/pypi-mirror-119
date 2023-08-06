6aed1ecd9cee3ce24f20e0b1d9fe33a46544d62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:10 +0000

    Add package coding-foo/1.0.0/coding_foo-1.0.0.tar.gz

---
+++ packages/coding-foo/coding_foo-1.0.0.tar.gz/coding_foo-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup
+
+PWD = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (PWD / "README.md").read_text()
+
+setup(
+    name='coding_foo',
+    packages=['bar'],
+    description='pypi test pkg',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    version='1.0.0',
+    license='MIT',
+    author='coding',
+    author_email='coding@coding.com',
+    url='https://e.coding.net/codingcorp/coding-artifacts-demo.git',
+    download_url='https://codingcorp.coding.net/p/mayday/artifacts/297/pypi/package/471/version/5772',
+    keywords=['PyPI', 'artifacts', 'Python']
+)
