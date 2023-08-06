94053088fcc42aa6c68fc18f2ca0f451d516a923
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:32 +0000

    Add package projit/0.1.7/projit-0.1.7.tar.gz

---
+++ packages/projit/projit-0.1.7.tar.gz/projit-0.1.7/projit/cli.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+
+"""projit.cli: provides entry point main()."""
+import pandas as pd
+import sys
+import os
+
+from .utils import locate_projit_config
+from .config import config_folder
+from .utils import initialise_project
+from .utils import get_properties
+from .utils import write_properties
+from .projit import load as projit_load
+from .projit import init as projit_init
+
+from projit import __version__
+ 
+def main():
+    if len(sys.argv) < 2:
+        print("ERROR: MISSING ARGUMENTS")
+        print_usage(sys.argv)
+        exit(1)
+    else:
+        cmd = sys.argv[1]
+        if len(sys.argv) == 2:
+            if sys.argv[1] == "-v":
+                print(" Version:", __version__)
+                exit(1)
+            elif sys.argv[1] == "-h":
+                print_usage(sys.argv)
+                exit(1)
+        
+        if cmd == "init":
+            init(sys.argv)
+            exit(1)
+
+        config_path = locate_projit_config() 
+        if config_path=="":
+            print(" ERROR: This is not a projit project.")
+            print("        Please initialise the project first.")
+            print(" > projit init <PROJECT NAME>")
+            exit(1)
+
+        project = projit_load(config_path)
+
+        if cmd == "update":
+            update(project)
+            exit(1)
+        if cmd == "status":
+            project_status(project)
+            exit(1)
+        if cmd == "render":
+            if len(sys.argv) < 3:
+                print(" ERROR: Rendering a project requires a path for output file.")
+                exit(1)
+            else:
+                path = sys.argv[2]
+                render_doc(project, path)
+                exit(1)
+        if cmd == "list":
+            if len(sys.argv) < 3:
+                print(" ERROR: MISSING ARGUMENTS")
+                print_usage(sys.argv)
+                exit(1)
+            else:
+                subcmd = sys.argv[2]
+                list(subcmd, project, sys.argv)
+                exit(1)
+        if cmd == "add":
+            if len(sys.argv) < 5:
+                print(" ERROR: MISSING ARGUMENTS")
+                print_usage(sys.argv)
+                exit(1)
+            else:
+                subcmd = sys.argv[2]
+                add(subcmd, project, sys.argv)
+                exit(1)
+
+        print(" ERROR: UNKNOWN ARGUMENTS")
+        print_usage(sys.argv)
+
+##########################################################################################        
+def init(argv):
+    config_file = locate_projit_config()
+    if config_file != "":
+        print("ERROR: Project exists. Run `projit update` to change details ")
+        exit(1)
+    if len(argv) < 3:
+        print("ERROR: Project initialisation requires parameter: <Project_Name>")
+        exit(1)
+    print("Please enter a description for your project (or Press Enter to Cancel)")
+    descrip = input(">")
+    if len(descrip) > 0:
+        if len(argv)>3:
+            template=argv[3]
+            if len(template)>9:
+                if template[0:9]=="template=":
+                    template=template[9:]
+        else:
+            template=''
+        project = projit_init(template, argv[2], descrip)
+    else:
+        print("Cancelling...")
+        exit(0)
+
+##########################################################################################       
+def update(project):
+    print("Current Project Name: ", project.name)
+    print("Enter an alternative project name (or press enter to keep)")
+    name = input(">")
+    print("Current Description: ", project.desc)
+    print("Enter an alternative description (or press enter to keep)")
+    descrip = input(">")
+    if name != "":
+        project.name = name
+    if descrip != "":
+        project.desc = descrip
+    project.save()
+
+
+##########################################################################################        
+def project_status(project):
+    print("")
+    print("  Project: %s" % project.name)
+    print("  Description: %s" % project.desc)
+    print("  Datasets: %i" % len(project.datasets))
+    print("  Experiments: %i" % len(project.experiments))
+    print("")
+
+##########################################################################################        
+def render_doc(project, path):
+    project.render(path)
+ 
+##########################################################################################        
+def list(subcmd, project, argv):
+    if subcmd == "datasets":
+        long_key = max([len(k) for k in project.datasets.keys()])
+        print(" ___Datasets________________________________________")
+        for ds in project.datasets:
+            print(" ", ds, filler(len(ds), long_key+1 ), project.datasets[ds] )
+        print("")
+    elif subcmd == "experiments":
+        print(" ___Experiments_____________________________________")
+        long_key = max([len(k[0]) for k in project.experiments])
+        for exp in project.experiments:
+            print(" ", exp[0], filler(len(exp[0]), long_key+1 ), exp[1] )
+        print("")
+    elif subcmd == "results":
+        if len(argv)>3:
+            dataset = argv[3]
+            rez = project.get_results(dataset)
+        else:
+            rez = project.get_results()
+        print(" ___Results_____________________________________")
+        pd.set_option('expand_frame_repr', False)
+        pd.set_option('display.max_columns', 999)
+        print(rez)
+    else:
+        print("ERROR: Unrecognised SUBCOMMAND: %s" % subcmd)
+        exit(1)
+
+def filler(current, max_len):
+    return " " * (max_len - current) 
+
+##########################################################################################        
+def add(subcmd, project, args):
+    if subcmd == "dataset":
+        name = args[3]
+        path = args[4]
+        project.add_dataset(name, path)
+    elif subcmd == "experiment":
+        name = args[3]
+        path = args[4]
+        project.add_experiment(name, path)
+    else:
+        print("ERROR: Unrecognised SUBCOMMAND: %s" % subcmd)
+        exit(1)
+
+
+##########################################################################################        
+def print_usage(args):
+    """ Command line application usage instrutions. """
+    print(" USAGE ")
+    print(" ", args[0], " [OPTIONS] <COMMAND> [<SUBCOMMAND>] [<PARAMS>*]")
+    print("   <COMMAND>     - CORE TASK TO PERFORM: [init | upate | status | add | list | render]")
+    print("   <SUBCOMMAND>  - (OPTIONAL) Dependant on COMMAND: [dataset | experiment | results]")
+    print("   <PARAMS>      - (OPTIONAL) Dependant on COMMAND: Usually names and paths")
+    print("   [OPTIONS]")
+    print("      -v             - Print version")
+    print("      -h             - Print this usage help")
+    print("")
+    print("   COMMON PATTERNS")
+    print("   ", args[0], "init 'Project name'")
+    print("   ", args[0], "status")
+    print("   ", args[0], "add dataset train data/train.csv")
+    print("   ", args[0], "add dataset test data/test.csv")
+    print("   ", args[0], "add experiment exploration exp/explore.ipynb")
+    print("   ", args[0], "list datasets")
+    print("   ", args[0], "list experiments")
+    print("   ", args[0], "list results")
+    print("")
+
+##########################################################################################        
---
+++ packages/projit/projit-0.1.7.tar.gz/projit-0.1.7/projit/projit.py
@@ -0,0 +1,362 @@
+import pandas as pd
+import json
+import re
+import os
+
+from .config import config_file
+from .config import config_folder
+from .template import load_template
+from .utils import locate_projit_config
+from .pdf import PDF
+
+##########################################################################################
+
+class Projit:
+    """
+    Projit Class.
+    This is a data structure to contain the core elements of a data science 
+    project. It will permit loose coupling between processes and experiments
+    but provide a simple overarching structure for communication and 
+    documentation.
+    """
+
+    def __init__(self, 
+                 path, 
+                 name, 
+                 desc="", 
+                 experiments=[], 
+                 datasets={},
+                 results={}, 
+                 params={},
+                 hyperparams={},
+                 dataresults={}):
+        """
+        Initialise a projit project object.
+        This class will be used for storing and retrieving all data about the project,
+        as well as ensuring that it is written to the projit meta-data files.
+
+        :param path: The path to the project file.
+        :type path: string, required
+
+        :param name: The project name
+        :type name: string, required
+
+        :param desc: The project description
+        :type desc: string, optional
+
+        :param experiments: The array of experiments
+        :type experiments: Array, optional
+
+        :param datasets: The dictionary of datasets 'name':'path'
+        :type datasets: Dictionary, optional
+
+        :param results: The dictionary of results by experiment.
+                        Structure: {'experiment':{'metric':'value'}}
+        :type results: Dictionary of Dictionary, optional
+
+        :param params: A dictionary of additional parameters share across experiments. 
+                       For example: target variable name, identifier column.
+        :type params: Dictionary, optional
+
+        :param hyperparams: A dictionary of hyper parameters for experiments. 
+                       Structure: {'experiment':{'param':'value', etc}} 
+        :type hyperparams: Dictionary, optional
+
+        :param dataresults: The dictionary of results on specific data sets.
+                            These are used when you want your experimental results broken
+                            down by the datasets. 
+                            Structure: {'dataset':{'experiment':{'metric':'value'}}}
+        :type dataresults: Dictionary of Dictionary of Dictionary, optional
+
+        :return: None 
+        :rtype: None 
+        """
+        self.path = path
+        self.name = name
+        self.desc = desc
+        self.experiments = experiments
+        self.datasets = datasets
+        self.results = results
+        self.params = params
+        self.hyperparams = hyperparams
+        self.dataresults = dataresults
+
+
+    def get_root_path(self):
+        """
+        Get the path to where the project folder is located
+        """
+        return self.path[0:len(self.path) - len(config_folder)]
+
+
+    def add_experiment(self, name, path):
+        """
+        Add information of a new experiment to the project. 
+        Then save the project configuration.
+        This function will overwrite an experiment of the same name
+        and delete any previous results.
+
+        :param name: The experiment name
+        :type name: string, required
+
+        :param path: The path to the experiment.
+        :type path: string, required
+        """
+        for elem in self.experiments: 
+            if elem[0] == name:
+                self.experiments.remove(elem)
+                self.clean_experimental_results(name)
+        self.experiments.append( (name, path) )
+        self.save()
+
+
+    def experiment_exists(self, name):
+        for elem in self.experiments:
+            if elem[0] == name:
+                return True
+        return False
+
+    def clean_experimental_results(self, name):
+        """
+        Remove all results for a given experiment
+        """
+        self.results[name] = {}
+        for dataset in self.dataresults:
+            self.dataresults[dataset][name] = {}
+
+    def add_dataset(self, name, path):
+        """
+        Add a named dataset to the project.
+
+        :param name: The dataset name
+        :type name: string, required
+
+        :param path: The path to the data set (either local path, URL or S3 Bucket)
+        :type path: string, required
+        """
+        self.datasets[name] = path
+        self.save()
+
+    def add_param(self, name, value):
+        """
+        Add a parameter to the project.
+
+        :param name: The parameter name
+        :type name: string, required
+
+        :param value: The value taken by that parameter
+        :type value: Any
+        """
+        self.params[name] = value 
+        self.save()
+
+    def add_hyperparam(self, name, value):
+        """
+        Add a set of hyper parameters to the project.
+
+        :param name: The experiment name
+        :type name: string, required
+
+        :param value: The Dictionary of hyperparameters
+        :type value: Dictionary
+        """
+        if self.experiment_exists(name):
+            self.hyperparams[name] = value
+            self.save()
+        else:
+            raise Exception("ERROR: No experiment called: '%s' -- Register your experiment first." % name)
+
+    def add_result(self, experiment, metric, value, dataset=None):
+        """
+        Add results from an experiment to the project.
+
+        They can be overall project results, or associated with a specific dataset
+
+        :param name: The experiment name
+        :type name: string, required
+
+        :param metric: The name of the metric we are adding.
+        :type metric: string, required
+
+        :param value: The value of the metric to add.
+        :type value: float, required
+
+        :param dataset: The dataset against which the results are generated
+        :type dataset: string, optional 
+
+        :return: None
+        :rtype: None
+        """
+
+        if dataset==None:
+            if experiment in self.results:
+                rez = self.results[experiment]
+            else:
+                rez = {}
+            rez[metric] = value
+            self.results[experiment] = rez
+        else: 
+            if dataset in self.dataresults:
+                rez = self.dataresults[dataset]
+            else:
+                rez = {}
+            if experiment in rez:
+                rez2 = rez[experiment]
+            else:
+                rez2 = {}
+            rez2[metric] = value
+            rez[experiment] = rez2
+            self.dataresults[dataset] = rez
+        self.save()
+
+    def get_results(self, dataset=None):
+        """
+        Retrieve the experimental results as a DataFrame.
+
+        They can be overall project results, or associated with a specific dataset
+
+        :param dataset: The dataset against which the results are generated
+        :type dataset: string, optional
+
+        :return: DataFrame of results
+        :rtype: pandas.DataFrame
+        """
+
+        df = pd.DataFrame()
+        if dataset==None:
+            myresults = self.results
+        else:
+            if dataset in self.dataresults:
+                myresults = self.dataresults[dataset]
+            else:
+                raise Exception("ERROR: No results for dataset: %s " % dataset)
+        for exp in self.experiments:
+            key = exp[0]
+            if key in myresults:
+                rez = myresults[key]
+            else:
+                rez = {}
+            rez['experiment'] = key
+            df = df.append(rez, ignore_index=True)
+        return df 
+
+    def get_dataset(self, name):
+        """
+        Retrieve the dataset by name.
+
+        :param name: The dataset to retrieve
+        :type name: string, required
+
+        :return: Path to dataset
+        :rtype: String
+        """
+        if name in self.datasets:
+            return self.datasets[name]
+        else:
+            raise Exception("ERROR: Named dataset '%s' not available:" % name)
+
+    def get_param(self, name):
+        if name in self.params:
+            return self.params[name]
+        else:
+            raise Exception("ERROR: Named parameter '%s' is not available:" % name)
+
+    def get_hyperparam(self, name):
+        if name in self.hyperparams:
+            return self.hyperparams[name]
+        else:
+            raise Exception("ERROR: Hyper parameters for experiemnt '%s' are not available:" % name)
+
+    def get_path_to_dataset(self, name):
+        ds = self.get_dataset(name)
+        if self.is_complete_path(ds):
+            return ds
+        else:
+            return self.create_local_path(ds)
+
+    def is_complete_path(self, path):
+        if path[0:1] == "/":
+            return True
+        if path[0:3] == "s3:":
+            return True
+        if path[0:4] == "http":
+            return True
+        return False
+
+    def create_local_path(self, ds):
+        return self.get_root_path() + ds
+
+    def save(self):
+        """
+        Save your projit project. 
+        """
+        path_to_json = self.path + "/" + config_file
+        with open(path_to_json, 'w') as outfile:
+            json.dump(self.__dict__, outfile, indent=0)
+
+    def render(self, path):
+        results = self.get_results()
+        pdf = PDF()
+        pdf.setup()
+        pdf.add_title(self.name)
+        pdf.add_description(self.desc)
+        pdf.output(path, 'F')
+
+
+##########################################################################################
+def load(config_path):
+    """
+    This function allows you to instantiate a Projit project from an existing config_path
+    The config path must contain the required config file that contains the required fields.
+
+    Note: This function will always overwrite the path variable in the object so the instance
+    is aware of where it is relative to the config directory.
+
+    :param config_path: The path to the projit configuration
+    :type config_path: string, required
+
+    :return: Projit Object
+    :rtype: Projit
+    """
+    path_to_json = config_path + "/" + config_file
+    with open(path_to_json) as f:
+        _dict = json.load(f)
+    _object = Projit(**_dict)
+    _object.path = config_path
+    return _object
+
+##########################################################################################
+def projit_load():
+    return load( locate_projit_config() )
+
+##########################################################################################
+def init(template, name, desc=""):
+    """
+    Initialise a new projit project.
+    Create the config directory and write the project config there.
+
+    :param name: The name of the project
+    :type name: string, required
+
+    :param desc: The project description
+    :type desc: string, required
+
+    :return: Projit Object
+    :rtype: Projit
+    """
+    os.mkdir(config_folder)
+    project = Projit(config_folder, name, desc)
+    project.save()
+    init_template(template)
+    return project
+
+##########################################################################################
+
+def init_template(template):
+    if template != "":
+        temp = load_template(template)
+        for d in temp['dirs']:
+            if not os.path.isdir(d):
+                os.mkdir(d)
+
+
