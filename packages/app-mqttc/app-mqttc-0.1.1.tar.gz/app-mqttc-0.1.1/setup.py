69ebb565f2c7ebb21f747b03432bcbc2950004aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:28 +0000

    Add package app-mqttc/0.1.1/app-mqttc-0.1.1.tar.gz

---
+++ packages/app-mqttc/app-mqttc-0.1.1.tar.gz/app-mqttc-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['app_mqttc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['paho-mqtt>=1.5.1,<2.0.0', 'typer[all]>=0.3.2,<0.4.0']
+
+extras_require = \
+{':python_version < "3.8"': ['importlib-metadata>=1.0,<2.0']}
+
+entry_points = \
+{'console_scripts': ['mqttc = app_mqttc:app']}
+
+setup_kwargs = {
+    'name': 'app-mqttc',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '# app-mqttc\n\n',
+    'author': 'Ying Shaodong',
+    'author_email': 'helloysd@foxmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
