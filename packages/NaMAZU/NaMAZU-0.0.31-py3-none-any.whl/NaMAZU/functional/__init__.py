8975d5036d4be61669dbb5c4e7fb0caec9f9777c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:02 +0000

    Add package NaMAZU/0.0.31/NaMAZU-0.0.31-py3-none-any.whl

---
+++ packages/NaMAZU/NaMAZU-0.0.31-py3-none-any.whl/NaMAZU/functional/__init__.py
@@ -0,0 +1,16 @@
+from .image_control import *
+from .file_control import *
+
+__all__ = [
+    "rename_file",
+    "img_to_npy",
+    "npy_to_png",
+    "split_image",
+    "apply_to_all",
+    "collect_file_pathes_by_ext",
+    "zip_files",
+    "change_frame_rates_in",
+    "save_all_frames",
+    "compose_two_png",
+]
+
