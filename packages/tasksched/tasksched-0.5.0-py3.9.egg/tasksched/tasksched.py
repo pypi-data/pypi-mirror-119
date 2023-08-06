2480707a8c6c96878bb943dd5c4eb582f9a836d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:51 +0000

    Add package tasksched/0.5.0/tasksched-0.5.0-py3.9.egg

---
+++ packages/tasksched/tasksched-0.5.0-py3.9.egg/tasksched/tasksched.py
@@ -0,0 +1,299 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020-2021 Sébastien Helleu <flashcode@flashtux.org>
+#
+# This file is part of Tasksched.
+#
+# Tasksched is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tasksched is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tasksched.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Task scheduler with automatic resource leveling."""
+
+from typing import Any, Dict, IO, List, Union
+
+import json
+import sys
+
+import yaml
+
+from tasksched.parser import get_parser
+from tasksched.project import Project
+from tasksched.workplan import build_workplan
+from tasksched.workplan_text import workplan_to_text
+from tasksched.workplan_html import workplan_to_html
+from tasksched.utils import yaml_dump
+
+__version__ = '0.5.0'
+
+__all__ = (
+    '__version__',
+    'load_config',
+    'main',
+    'init',
+)
+
+
+def error(message: str):
+    """
+    Display an error.
+
+    :param message: error message to display
+    """
+    print(message, file=sys.stderr)
+    print('Try with --help to get help on tasksched', file=sys.stderr)
+
+
+def get_input_files(args) -> List[Any]:
+    """
+    Get list of input files (optional stdin file + filenames).
+
+    :param argparse.Namespace args: command-line arguments
+    :return: list of files/filenames
+    """
+    files = []
+    if not sys.stdin.isatty():
+        files.append(sys.stdin)
+    files.extend(args.filename)
+    return files
+
+
+def read_file(input_file: Union[IO, str]) -> Dict:
+    """
+    Read input file (YAML or JSON).
+
+    :param input_file: input file
+    :return: input file as dict
+    """
+    try:
+        if isinstance(input_file, str):
+            with open(input_file) as _file:
+                return yaml.safe_load(_file)
+        else:
+            return yaml.safe_load(input_file)
+    except (FileNotFoundError, yaml.parser.ParserError) as exc:
+        if isinstance(input_file, str):
+            error(f'ERROR: unable to decode input file "{input_file}": {exc}')
+        else:
+            error(f'ERROR: unable to decode input data: {exc}')
+        raise
+
+
+def search_item(list_items: List[Any], item_id: Any) -> Any:
+    """
+    Search an item by its id in a list.
+
+    :param list_items: list of items
+    :param item_id: item id to search in the list of items
+    :return: the item found or None
+    """
+    if item_id:
+        for item in list_items:
+            if item['id'] == item_id:
+                return item
+    return None
+
+
+def merge_configs(config: Dict, new_config: Dict):
+    """
+    Merge config2 into config: each value in config is updated with value
+    from config2: for dicts (like "project"), keys are updated, for lists
+    (like "resources" or "tasks"), items are added to the list.
+
+    :param dict config: first config to update
+    :param dict new_config: dictionary used to update the config
+    """
+    for key, value in new_config.items():
+        if key not in config:
+            config[key] = value
+            continue
+        if isinstance(config[key], dict):
+            if not isinstance(value, dict):
+                raise ValueError(f'merge config error: '
+                                 f'cannot update dict "{key}"')
+            config[key].update(value)
+        elif isinstance(config[key], list):
+            if not isinstance(value, list):
+                raise ValueError(f'merge config error: '
+                                 f'cannot update list "{key}"')
+            for item in value:
+                if isinstance(item, dict):
+                    config_item = search_item(config[key], item.get('id'))
+                    if config_item is None:
+                        config[key].append(item)
+                    else:
+                        config_item.update(item)
+                else:
+                    config[key].append(item)
+        else:
+            config[key] = value
+
+
+def load_config(files: List[Any]) -> Dict:
+    """
+    Load YAML/JSON configuration by reading stdin (if available) and list of
+    input files received on command line.
+
+    :param list files: files/filenames to load
+    :return: configuration
+    """
+    config: Dict[Any, Any] = {}
+    for _file in files:
+        new_config = read_file(_file)
+        if new_config:
+            merge_configs(config, new_config)
+    return config
+
+
+def load_project(args) -> Project:
+    """
+    Load project.
+
+    :param argparse.Namespace args: command-line arguments
+    :return: project
+    """
+    files = get_input_files(args)
+    config = load_config(files)
+    try:
+        return Project(config)
+    except (KeyError, ValueError) as exc:
+        error(f'ERROR: invalid project: "{exc.args[0]}"')
+        raise
+
+
+def read_workplan(args) -> Dict:
+    """
+    Read work plan.
+
+    :param argparse.Namespace args: command-line arguments
+    :return: work plan as dict
+    """
+    workplan = None
+    files = get_input_files(args)
+    if files:
+        workplan = read_file(files[-1])
+    if not workplan:
+        error('ERROR: missing input work plan')
+        raise OSError('missing input work plan')
+    return workplan
+
+
+def convert_workplan_to_text(workplan: Dict, args) -> str:
+    """
+    Convert workplan to text.
+
+    :param workplan: work plan
+    :param argparse.Namespace args: command-line arguments
+    """
+    try:
+        return workplan_to_text(
+            workplan,
+            quiet=args.quiet,
+            use_colors=not args.no_colors,
+            use_unicode=not args.no_unicode,
+        )
+    except (KeyError, ValueError) as exc:
+        error(f'ERROR: invalid work plan: "{exc}"')
+        raise
+
+
+def convert_workplan_to_html(workplan: Dict, args) -> str:
+    """
+    Convert workplan to HTML.
+
+    :param workplan: work plan
+    :param argparse.Namespace args: command-line arguments
+    """
+    try:
+        return workplan_to_html(
+            workplan,
+            template_file=args.template,
+            css_file=args.css,
+        )
+    except (KeyError, ValueError) as exc:
+        error(f'ERROR: invalid work plan: "{exc}"')
+        raise
+
+
+def action_workplan(args):
+    """
+    Return the work plan using the project configuration.
+
+    :param argparse.Namespace args: command-line arguments
+    """
+    project = load_project(args)
+    workplan = build_workplan(project)
+    if args.json:
+        return json.dumps(workplan.as_dict(), default=str)
+    return yaml_dump(workplan.as_dict())
+
+
+def action_text(args):
+    """
+    Return the work plan as text to display in the terminal.
+
+    :param argparse.Namespace args: command-line arguments
+    """
+    workplan = read_workplan(args)
+    return convert_workplan_to_text(workplan, args)
+
+
+def action_html(args):
+    """
+    Return the work plan as HTML.
+
+    :param argparse.Namespace args: command-line arguments
+    """
+    workplan = read_workplan(args)
+    return convert_workplan_to_html(workplan, args)
+
+
+def action_workplan_text(args):
+    """
+    Build the work plan and return it as text to display in the terminal.
+
+    :param argparse.Namespace args: command-line arguments
+    """
+    project = load_project(args)
+    workplan = build_workplan(project)
+    return convert_workplan_to_text(workplan.as_dict(), args)
+
+
+def action_workplan_html(args):
+    """
+    Build the work plan and return it as HTML.
+
+    :param argparse.Namespace args: command-line arguments
+    """
+    project = load_project(args)
+    workplan = build_workplan(project)
+    return convert_workplan_to_html(workplan.as_dict(), args)
+
+
+def main():
+    """Main function, entry point."""
+    args = get_parser(__version__).parse_args()
+    func = getattr(sys.modules[__name__], f'action_{args.action}')
+    try:
+        result = func(args)
+    except Exception:  # pylint: disable=broad-except
+        sys.exit(1)
+    print(result)
+
+
+def init(force=False):
+    """Init function."""
+    if __name__ == '__main__' or force:
+        main()
+
+
+init()
---
+++ packages/tasksched/tasksched-0.5.0-py3.9.egg/tasksched/workplan.py
@@ -0,0 +1,254 @@
+#!/usr/bin/env python3
+#
+# Copyright (C) 2020-2021 Sébastien Helleu <flashcode@flashtux.org>
+#
+# This file is part of Tasksched.
+#
+# Tasksched is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tasksched is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tasksched.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Task scheduler work plan."""
+
+from operator import attrgetter
+from typing import Dict, List, Optional
+
+import copy
+import datetime
+
+from tasksched.project import Project, Resource, Task
+from tasksched.utils import add_business_days
+
+__all__ = (
+    'WorkPlan',
+    'build_workplan',
+)
+
+
+class WorkPlanResource(Resource):  # pylint: disable=too-few-public-methods
+    """A workplan resource."""
+
+    def __init__(self, res_id: str, name: str):
+        super().__init__(res_id, name)
+        self.assigned: List[Dict] = []
+        self.assigned_tasks: List[Dict] = []
+        self.duration: int = 0
+        self.end_date: Optional[datetime.date] = None
+        self.use: int = 0
+
+
+class WorkPlanTask(Task):  # pylint: disable=too-few-public-methods
+    """A workplan task."""
+
+    # pylint: disable=too-many-arguments
+    def __init__(self, task_id: str, title: str, duration: int,
+                 priority: int = 0, max_resources: int = 2):
+        super().__init__(task_id, title, duration, priority, max_resources)
+        self.remaining: int = self.duration
+
+
+class WorkPlan:
+    """A work plan built for a project."""
+
+    def __init__(self, project: Project,
+                 tasks_to_split: Dict[str, int] = None):
+        self.project = copy.deepcopy(project)
+        self.resources = [
+            WorkPlanResource(res.res_id, res.name)
+            for res in self.project.resources
+        ]
+        self.tasks = [
+            WorkPlanTask(task.task_id, task.title, task.duration,
+                         task.priority, task.max_resources)
+            for task in self.project.tasks
+        ]
+        if tasks_to_split:
+            self.split_tasks(tasks_to_split)
+        self.remaining = sum(task.duration for task in self.tasks)
+        self.duration = 0
+        self.end_date = self.project.start_date
+        self.resources_use = 0
+        self.schedule()
+
+    def split_tasks(self, tasks_to_split: Dict[str, int]):
+        """
+        Split tasks in the project, to carry them out in parallel by several
+        people.
+
+        :param tasks_to_split: tasks to split, keys are task ids (str),
+            values are number of splits (int)
+        """
+        new_tasks = []
+        for task in self.tasks:
+            number = tasks_to_split.get(task.task_id, None)
+            if number is not None and 1 < number <= task.max_resources:
+                # split duration into multiple durations which are all almost
+                # the same, and sum == duration (and remove all null values);
+                # for example if task.duration == 10 and number == 3,
+                # durations == [4, 3, 3]
+                durations = list(filter(None, [
+                    task.duration // number + (1 if x < task.duration % number
+                                               else 0)
+                    for x in range(number)
+                ]))
+                for i, duration in enumerate(durations):
+                    title = f'{task.title} ({i+1}/{len(durations)})'
+                    new_tasks.append(
+                        WorkPlanTask(task.task_id, title, duration,
+                                     task.priority, task.max_resources)
+                    )
+            else:
+                new_tasks.append(
+                    WorkPlanTask(task.task_id, task.title, task.duration,
+                                 task.priority, task.max_resources)
+                )
+        self.tasks = new_tasks
+
+    def find_best_resource(self) -> Resource:
+        """
+        Find the best resource to use (the less used resource, by order).
+
+        :return: resource found
+        """
+        lowest_duration = 999999999
+        best_res = None
+        for res in self.resources:
+            if res.duration == 0:
+                return res
+            if res.duration < lowest_duration:
+                lowest_duration = res.duration
+                best_res = res
+        return best_res or self.resources[0]
+
+    def assign_task(self, task, resource, days):
+        """
+        Assign a task to a resource.
+
+        :param Task task: task
+        :param Resource resource: resource
+        :param int days: number of days in task to assign
+        """
+        resource.assigned.append({
+            'task': task.task_id,
+            'duration': days,
+        })
+        resource.assigned_tasks.append({
+            'id': task.task_id,
+            'title': task.title,
+        })
+        resource.duration += days
+        if resource.duration > self.duration:
+            self.duration = resource.duration
+        task.remaining -= days
+        self.remaining -= days
+
+    def sorted_tasks(self, key: str, reverse: bool = False) -> List[Task]:
+        """
+        Get list of tasks sorted by priority (from higher to lower) and
+        duration (from longest to shortest).
+
+        :param tuple,list key: key(s) to sort tasks
+        :param bool reverse: reverse sort
+        :return: sorted list of tasks
+        """
+        return sorted(
+            self.tasks,
+            key=attrgetter(*key),
+            reverse=reverse,
+        )
+
+    def schedule(self):
+        """Automatic resource leveling in the project."""
+        sorted_tasks = self.sorted_tasks(['priority', 'duration'],
+                                         reverse=True)
+        for task in sorted_tasks:
+            self.assign_task(task, self.find_best_resource(), task.remaining)
+        sum_use = 0
+        for res in self.resources:
+            if res.duration > 0:
+                res.end_date = add_business_days(
+                    self.project.start_date,
+                    res.duration - 1,
+                    self.project.hdays,
+                )
+                if res.end_date > self.end_date:
+                    self.end_date = res.end_date
+            res.use = (res.duration * 100) / self.duration
+            sum_use += res.use
+        if self.resources:
+            self.resources_use = sum_use / len(self.resources)
+
+    def as_dict(self) -> Dict:
+        """Return the work plan as dict."""
+        after_end = self.end_date + datetime.timedelta(days=1)
+        holidays = self.project.hdays[
+            self.project.start_date:after_end]  # type: ignore
+        return {
+            'workplan': {
+                'project': {
+                    'name': self.project.name,
+                    'start': self.project.start_date,
+                    'end': self.end_date,
+                    'duration': self.duration,
+                    'holidays_iso': self.project.holidays_iso,
+                    'holidays': holidays,
+                    'resources_use': self.resources_use,
+                },
+                'resources': [
+                    {
+                        'id': res.res_id,
+                        'name': res.name,
+                        'assigned': res.assigned,
+                        'assigned_tasks': res.assigned_tasks,
+                        'duration': res.duration,
+                        'end': res.end_date or None,
+                        'use': res.use,
+                    }
+                    for res in self.resources
+                ],
+                'tasks': [
+                    {
+                        'id': task.task_id,
+                        'title': task.title,
+                        'duration': task.duration,
+                        'priority': task.priority,
+                        'max_resources': task.max_resources,
+                    }
+                    for task in self.tasks
+                ],
+            },
+        }
+
+
+def build_workplan(project: Project) -> WorkPlan:
+    """
+    Build a work plan and tries to split tasks for the smallest possible
+    project duration.
+
+    :param project: the project
+    :return: work plan
+    """
+    tasks_ids = [
+        task.task_id
+        for task in project.sorted_tasks(['duration'], reverse=True)
+        if task.duration > 1
+    ]
+    best_workplan = WorkPlan(project)
+    for i in range(0, len(tasks_ids)):
+        tasks_to_split = {
+            task_id: 2
+            for task_id in tasks_ids[:i+1]
+        }
+        workplan = WorkPlan(project, tasks_to_split=tasks_to_split)
+        if workplan.duration < best_workplan.duration:
+            best_workplan = workplan
+    return best_workplan
