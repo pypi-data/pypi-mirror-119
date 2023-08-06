5ba97d0d5115f502679333a7444e44f7a9e83a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:54 +0000

    Add package mewtwo/0.2/mewtwo-0.2.tar.gz

---
+++ packages/mewtwo/mewtwo-0.2.tar.gz/mewtwo-0.2/mewtwo/mewtwo.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  8 02:10:08 2021
+
+@author: palizmun
+"""
+
+
+def mewtwo_hi(name = None):
+    if name is None : 
+        return 'Hi, I am Mewtwo.'
+    else: 
+        return f'Hi, {name}..I am Mewtwo.'
