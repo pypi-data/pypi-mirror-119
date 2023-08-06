748f2107fe4215cb921ba54d266cd6f579281c9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:53 +0000

    Add package main-gas-pipeline/0.0.0.1/main-gas-pipeline-0.0.0.1.tar.gz

---
+++ packages/main-gas-pipeline/main-gas-pipeline-0.0.0.1.tar.gz/main-gas-pipeline-0.0.0.1/README.md
@@ -0,0 +1,8 @@
+[![Documentation Status](https://readthedocs.org/projects/main-gas-pipeline/badge/?version=latest)](https://main-gas-pipeline.readthedocs.io/en/latest/?badge=latest)
+
+# Main gas pipelines calculations
+
+A program for thermal and hydraulic calculation of a gas pipeline section between 
+two compressor stations of a main gas pipeline is presented.  
+The purpose of the calculation is to determine the pressure and temperature of the gas at the end of the section.
+
---
+++ packages/main-gas-pipeline/main-gas-pipeline-0.0.0.1.tar.gz/main-gas-pipeline-0.0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="main-gas-pipeline",
+    version="0.0.0.1",
+    author="Buzovskyi Vitaliiy",
+    author_email="buzovskiy.v@gmail.com",
+    description="Main gas pipeline calculator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Buzovskiy/main-gas-pipeline",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.8',
+)
