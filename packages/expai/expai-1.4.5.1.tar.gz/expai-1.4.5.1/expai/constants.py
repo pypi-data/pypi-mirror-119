2965a4150272c702e12140b4dcea91d65ab49be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:28 +0000

    Add package expai/1.4.5.1/expai-1.4.5.1.tar.gz

---
+++ packages/expai/expai-1.4.5.1.tar.gz/expai-1.4.5.1/expai/constants.py
@@ -0,0 +1,5 @@
+explanation_types = {'Explicación del modelo': 1,
+                     'Explicación de la variable': 2,
+                     'Explicación de la muestra': 3,
+                     'What If': 4,
+                     'What If Battle': 5}
