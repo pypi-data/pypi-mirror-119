bf5e00b6b11dcfab8a76c0ef2a1a18c58285b016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:13 +0000

    Add package svtplay-dl/4.3/svtplay-dl-4.3.tar.gz

---
+++ packages/svtplay-dl/svtplay-dl-4.3.tar.gz/svtplay-dl-4.3/lib/svtplay_dl/service/pokemon.py
@@ -0,0 +1,39 @@
+import re
+from urllib.parse import urlparse
+
+from svtplay_dl.error import ServiceError
+from svtplay_dl.fetcher.hls import hlsparse
+from svtplay_dl.service import OpenGraphThumbMixin
+from svtplay_dl.service import Service
+
+
+class Pokemon(Service, OpenGraphThumbMixin):
+    supported_domains = ["watch.pokemon.com"]
+
+    def get(self):
+        parse = urlparse(self.url)
+        if parse.fragment == "":
+            yield ServiceError("need the whole url")
+            return
+
+        match = re.search(r"id=([a-f0-9]+)\&", parse.fragment)
+        if not match:
+            yield ServiceError("Cant find the ID in the url")
+            return
+
+        res = self.http.get("https://www.pokemon.com/api/pokemontv/v2/channels/us/")
+
+        janson = res.json()
+        stream = None
+        for i in janson:
+            for n in i["media"]:
+                if n["id"] == match.group(1):
+                    stream = n
+                    break
+
+        self.output["title"] = "pokemon"
+        self.output["season"] = stream["season"]
+        self.output["episode"] = stream["episode"]
+        self.output["episodename"] = stream["title"]
+
+        yield from hlsparse(self.config, self.http.request("get", stream["stream_url"]), stream["stream_url"], output=self.output)
