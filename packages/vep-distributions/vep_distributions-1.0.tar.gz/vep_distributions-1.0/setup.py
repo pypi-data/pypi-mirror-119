c1bd7eedb600414a4de20bbaad07e37227052d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:59 +0000

    Add package vep-distributions/1.0/vep_distributions-1.0.tar.gz

---
+++ packages/vep-distributions/vep_distributions-1.0.tar.gz/vep_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='vep_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['vep_distributions'],
+      author='Joshua Olaku',
+      author_email= 'joshuaolaku@gmail.com',
+      zip_safe=False)
