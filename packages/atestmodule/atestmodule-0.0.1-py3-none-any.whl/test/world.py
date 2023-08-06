d5bc7239e96aa6d4b07dce0f0a03349c2bc94e01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:14 +0000

    Add package atestmodule/0.0.1/atestmodule-0.0.1-py3-none-any.whl

---
+++ packages/atestmodule/atestmodule-0.0.1-py3-none-any.whl/test/world.py
@@ -0,0 +1,16 @@
+"""
+This module implements provides naming data of world.
+
+Author: Kashif Iftikhar
+YouTube: https://www.megasoftmoney.com
+"""
+
+__author__ = "Kashif Iftikhar, Megasoft"
+__email__ = "mkiak22571@gmail.com"
+__status__ = "Planning and Working"
+
+class World:
+    
+    def World():
+        world = ["Paksitan", "Indonesia"]
+        return world 
