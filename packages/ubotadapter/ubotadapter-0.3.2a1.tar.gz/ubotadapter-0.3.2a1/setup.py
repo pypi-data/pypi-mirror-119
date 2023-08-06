a8c85a349ebe1643bfb980695a406106355803e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:26 +0000

    Add package ubotadapter/0.3.2a1/ubotadapter-0.3.2a1.tar.gz

---
+++ packages/ubotadapter/ubotadapter-0.3.2a1.tar.gz/ubotadapter-0.3.2a1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name='ubotadapter',
+                 version='0.3.2a1',
+                 description='Bot Adapter class',
+                 url='',
+                 author='Smyek',
+                 author_email='smyek.job@gmail.com',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 install_requires=['backoff', 'vkale>=0.4.2', 'python-telegram-bot', 'deteefapi>=0.4.1'],
+                 zip_safe=False)
---
+++ packages/ubotadapter/ubotadapter-0.3.2a1.tar.gz/ubotadapter-0.3.2a1/ubotadapter/bots/__init__.py
@@ -0,0 +1,3 @@
+from ubotadapter.bots.dtf.dtf import DTF
+from ubotadapter.bots.telegram.tg import Telegram
+from ubotadapter.bots.vk.vk import VK
