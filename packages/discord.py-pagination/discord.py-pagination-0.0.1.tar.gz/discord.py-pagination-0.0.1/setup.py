46c55e7d1a09bb344ea2ef0f14baca0907818c55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:22 +0000

    Add package discord.py-pagination/0.0.1/discord.py-pagination-0.0.1.tar.gz

---
+++ packages/discord.py-pagination/discord.py-pagination-0.0.1.tar.gz/discord.py-pagination-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="discord.py-pagination",
+    version="0.0.1",
+    py_modules=["Paginator"],
+    package_dir={'': "src"},
+    url="https://github.com/soosBot-com/Pagination",
+    author="soosBot",
+    author_email="support@soosbot.com",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ]
+)
