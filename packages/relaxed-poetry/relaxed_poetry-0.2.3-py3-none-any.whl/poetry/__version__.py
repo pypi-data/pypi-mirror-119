6ad59458ff6166afb70936cd1d8686ffd5b9c66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:55 +0000

    Add package relaxed-poetry/0.2.3/relaxed_poetry-0.2.3-py3-none-any.whl

---
+++ packages/relaxed-poetry/relaxed_poetry-0.2.3-py3-none-any.whl/poetry/__version__.py
@@ -0,0 +1,7 @@
+try:
+    import importlib.metadata as mtd
+except ModuleNotFoundError:
+    import importlib_metadata as mtd
+
+
+__version__ = mtd.version("relaxed-poetry")
---
+++ packages/relaxed-poetry/relaxed_poetry-0.2.3-py3-none-any.whl/poetry/console/commands/env/list.py
@@ -0,0 +1,33 @@
+from cleo.helpers import option
+
+from ..command import Command
+from ... import console
+
+
+class EnvListCommand(Command):
+
+    name = "env list"
+    description = "Lists all virtualenvs associated with the current project."
+
+    options = [option("full-path", None, "Output the full paths of the virtualenvs.")]
+
+    def handle(self) -> int:
+
+        from poetry.utils.env import EnvManager
+
+        manager = EnvManager(self.poetry)
+        current_env = manager.get()
+
+        for venv in manager.list():
+            name = venv.path.name
+            if self.option("full-path"):
+                name = str(venv.path)
+
+            if venv == current_env:
+                self.line("<info>{} (Activated)</info>".format(name))
+
+                continue
+
+            self.line(name)
+
+        return 0
