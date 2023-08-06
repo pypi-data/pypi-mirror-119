d789a25171efb1e43dbdd98ac6450ebebfc894a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:24 +0000

    Add package distributions-GBNS/0.1/distributions_GBNS-0.1.tar.gz

---
+++ packages/distributions-GBNS/distributions_GBNS-0.1.tar.gz/distributions_GBNS-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_GBNS',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_GBNS'],
+      author = 'Chaminie De Silva',
+      author_email = 'kaushalyasarathchandra@gmail.com',
+      zip_safe=False)
