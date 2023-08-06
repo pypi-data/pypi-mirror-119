155c6c1f14b2cf35660d00934853e0278b7e80ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:46 +0000

    Add package tune/0.0.8/tune-0.0.8-py3-none-any.whl

---
+++ packages/tune/tune-0.0.8-py3-none-any.whl/tests/tune/concepts/space/test_parameters.py
@@ -0,0 +1,183 @@
+import json
+
+import numpy as np
+from pytest import raises
+from scipy import stats
+from triad import to_uuid
+from tune.concepts.space import (
+    Choice,
+    Grid,
+    NormalRand,
+    NormalRandInt,
+    Rand,
+    RandInt,
+    Space,
+)
+from tune.concepts.space.parameters import _decode_params
+from tune._utils import assert_close
+
+
+def test_grid():
+    v = Grid("a", "b")
+    assert ["a", "b"] == list(v)
+
+    v2 = Grid("b", "a")
+    assert v == v and v != v2
+    assert to_uuid(v) != to_uuid(v2)
+
+    raises(ValueError, lambda: Grid())
+
+
+def test_choice():
+    raises(ValueError, lambda: Choice())
+    v = Choice("a", "b", "c")
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    assert v.generate_many(20, 0) == v.generate_many(20, 0)
+    assert v.generate_many(20, 0) != v.generate_many(20, 1)
+    actual = set(v.generate_many(20, 0))
+    assert set(["a", "b", "c"]) == actual
+
+    assert to_uuid(v) != to_uuid(Grid("a", "b", "c"))
+    assert v != Grid("a", "b", "c")
+
+    v = Choice(1, 2, 3)
+    assert json.loads(json.dumps({"x": v.generate(0)}))["x"] <= 3
+
+    v = Choice("a", "b", "c")
+    assert isinstance(json.loads(json.dumps({"x": v.generate(0)}))["x"], str)
+
+
+def test_rand():
+    with raises(ValueError):
+        Rand(1.0, 0.9)
+
+    with raises(ValueError):
+        Rand(1.0, 10, q=-0.1)
+
+    with raises(ValueError):
+        Rand(1.0, 1.0, include_high=False)
+
+    with raises(ValueError):
+        Rand(0.0, 1.0, log=True)  # for log, low>=1.0
+
+    v = Rand(0.1, 0.1, q=0.1, log=False)
+    assert 0.1 == v.generate()
+    assert 0.1 == v.generate(10)
+
+    v = Rand(1.0, 1.0, q=0.1, log=True)
+    assert 1.0 == v.generate()
+    assert 1.0 == v.generate(10)
+
+    v = Rand(1.0, 2.0, q=0.1, log=False)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    assert_close([x / 10 for x in range(10, 21)], v.generate_many(100, 0))
+
+    v = Rand(1.0, 2.09999, q=0.1, log=False)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    assert_close([x / 10 for x in range(10, 21)], v.generate_many(100, 0))
+
+    v = Rand(1.0, 2.0, q=0.1, log=False, include_high=False)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    assert_close([x / 10 for x in range(10, 20)], v.generate_many(100, 0))
+
+    v = Rand(1.0, 2.09999, q=0.1, log=False, include_high=False)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    assert_close([x / 10 for x in range(10, 21)], v.generate_many(100, 0))
+
+    v = Rand(0.1, 2.0, log=True, include_high=False)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    res = v.generate_many(10000, 0)
+    for x in res:
+        assert x >= 0.1 and x <= 2.0
+    t = stats.kstest(np.log(res), "uniform", args=(np.log(0.1), np.log(2) - np.log(0.1)))
+    assert t.pvalue > 0.4
+
+
+def test_randint():
+    with raises(ValueError):
+        RandInt(0, 10, log=True)  # for log, low>=1.0
+
+    v = RandInt(10, 20, log=False)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    actual = set(v.generate_many(100, 0))
+    assert set(range(10, 21)) == actual
+
+    v = RandInt(10, 20, include_high=False)
+    actual = set(v.generate_many(100, 0))
+    assert set(range(10, 20)) == actual
+
+    v = RandInt(10, 20, q=5, include_high=False)
+    actual = set(v.generate_many(100, 0))
+    assert set([10, 15]) == actual
+
+    v = RandInt(10, 20, q=5, include_high=True)
+    actual = set(v.generate_many(100, 0))
+    assert set([10, 15, 20]) == actual
+
+    v = RandInt(3, 20, log=True)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    actual = set(v.generate_many(1000, 0))
+    assert set(range(3, 21)) == actual
+
+
+def test_normal_rand():
+    with raises(ValueError):
+        NormalRand(1.0, 0.0)
+
+    with raises(ValueError):
+        NormalRand(1.0, -1.0)
+
+    v = NormalRand(0.05, 0.2)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    res = v.generate_many(100000, 0)
+    t = stats.kstest(res, "norm", args=(0.05, 0.2))
+    assert t.pvalue > 0.4
+
+    v = NormalRand(0.05, 0.2, q=0.1)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    actual = [x for x in v.generate_many(1000, 0) if x >= -0.155 and x <= 0.255]
+    assert_close([-0.15, -0.05, 0.05, 0.15, 0.25], actual)
+
+
+def test_normal_randint():
+    v = NormalRandInt(5, 2)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(1)
+    actual = set(v.generate_many(50, 0))
+    for x in [3, 4, 5, 6, 7]:
+        assert x in actual
+
+    v = NormalRandInt(5, 2, q=3)
+    assert v.generate(0) == v.generate(0)
+    assert v.generate(0) != v.generate(2)
+    actual = set(v.generate_many(50, 0))
+    for x in [-1, 2, 5, 8, 11]:
+        assert x in actual
+    assert 6 not in actual
+
+
+def test_encode__decode_params():
+    s1 = Space(
+        a=Grid(1, 2),
+        b=Rand(1.0, 7.1, 0.2, log=True),
+        c=Choice(1, 2, 3),
+        d=[Grid(1, 2), Rand(0, 2.0)],
+        e={"x": "xx", "y": Choice("a", "b")},
+        f=RandInt(0, 10, log=False),
+        g=NormalRand(0.1, 1.0, q=0.1),
+        h=NormalRandInt(0.1, 1.0),
+    )
+    actual = [_decode_params(x) for x in s1.encode()]
+    assert list(s1) == actual
+    for x in s1.encode():
+        print(json.dumps(x, indent=2))
---
+++ packages/tune/tune-0.0.8-py3-none-any.whl/tune/concepts/space/parameters.py
@@ -0,0 +1,381 @@
+from typing import Any, Dict, Iterable, List, Optional
+
+import numpy as np
+from triad import assert_or_throw, to_uuid
+from tune._utils.math import (
+    normal_to_continuous,
+    normal_to_discrete,
+    normal_to_integers,
+    uniform_to_continuous,
+    uniform_to_discrete,
+    uniform_to_integers,
+)
+
+
+class Grid(object):
+    """Grid search, every value will be used.
+    Please read |SpaceTutorial|.
+
+    :param args: values for the grid search
+    """
+
+    def __init__(self, *args: Any):
+        assert_or_throw(
+            len(args) > 0, ValueError("Grid must take at least one element")
+        )
+        self._values = list(args)
+
+    def __iter__(self) -> Iterable[Any]:
+        yield from self._values
+
+    def __eq__(self, other: Any):
+        """Compare two ``Grid``"""
+        return isinstance(other, type(self)) and self._values == other._values
+
+    def __uuid__(self) -> str:
+        return to_uuid("grid", self._values)
+
+
+class StochasticExpression(object):
+    """Stochastic search base class.
+    Please read |SpaceTutorial|.
+    """
+
+    @property
+    def jsondict(self) -> Dict[str, Any]:  # pragma: no cover
+        """Dict representation of the expression that is json serializable"""
+        raise NotImplementedError
+
+    def __eq__(self, other: Any):
+        """Compare two ``StochasticExpression``"""
+        return isinstance(other, type(self)) and self.jsondict == other.jsondict
+
+    def generate(self, seed: Any = None) -> Any:  # pragma: no cover
+        """Return a randomly chosen value.
+
+        :param seed: if set, it will be used to call :func:`~np:numpy.random.seed`
+          , defaults to None
+        """
+        raise NotImplementedError
+
+    def generate_many(self, n: int, seed: Any = None) -> List[Any]:
+        """Generate ``n`` randomly chosen values
+
+        :param n: number of random values to generate
+        :param seed: random seed, defaults to None
+        :return: a list of values
+        """
+        if seed is not None:
+            np.random.seed(seed)
+        return [self.generate() for _ in range(n)]
+
+    def __uuid__(self) -> str:
+        """Unique id for the expression"""
+        return to_uuid(self.jsondict)
+
+
+class Choice(StochasticExpression):
+    """A random choice of values.
+    Please read |SpaceTutorial|.
+
+    :param args: values to choose from
+    """
+
+    def __init__(self, *args: Any):
+        assert_or_throw(
+            len(args) > 0, ValueError("Choice must take at least one element")
+        )
+        self._values = list(args)
+
+    @property
+    def values(self) -> List[Any]:
+        """values to choose from"""
+        return self._values
+
+    @property
+    def jsondict(self) -> Dict[str, Any]:
+        return dict(_expr_="choice", values=self.values)
+
+    def generate(self, seed: Any = None) -> Any:
+        if seed is not None:
+            np.random.seed(seed)
+        value = np.random.choice(self._values)
+        if isinstance(value, np.generic):
+            return value.item()
+        return value  # pragma: no cover
+
+
+class RandBase(StochasticExpression):
+    """Base class for continuous random variables.
+    Please read |SpaceTutorial|.
+
+    :param q: step between adjacent values, if set, the value will be rounded
+      using ``q``, defaults to None
+    :param log: whether to do uniform sampling in log space, defaults to False.
+      If True, lower values get higher chance to be sampled
+    """
+
+    def __init__(
+        self,
+        q: Optional[float] = None,
+        log: bool = False,
+    ):
+        if q is not None:
+            assert_or_throw(q > 0, f"{q} <= 0")
+        self.q = q
+        self.log = log
+
+
+class Rand(RandBase):
+    """Continuous uniform random variables.
+    Please read |SpaceTutorial|.
+
+    :param low: range low bound (inclusive)
+    :param high: range high bound (exclusive)
+    :param q: step between adjacent values, if set, the value will be rounded
+      using ``q``, defaults to None
+    :param log: whether to do uniform sampling in log space, defaults to False.
+      If True, ``low`` must be positive and lower values get higher chance to be sampled
+    """
+
+    def __init__(
+        self,
+        low: float,
+        high: float,
+        q: Optional[float] = None,
+        log: bool = False,
+        include_high: bool = True,
+    ):
+        if include_high:
+            assert_or_throw(high >= low, ValueError(f"{high} < {low}"))
+        else:
+            assert_or_throw(high > low, ValueError(f"{high} <= {low}"))
+        assert_or_throw(q is None or q > 0, ValueError(q))
+        if log:
+            assert_or_throw(
+                low > 0.0,
+                ValueError(f"for log sampling, low ({low}) must be greater than 0.0"),
+            )
+        self.low = low
+        self.high = high
+        self.include_high = include_high
+        super().__init__(q, log)
+
+    @property
+    def jsondict(self) -> Dict[str, Any]:
+        res = dict(
+            _expr_="rand",
+            low=self.low,
+            high=self.high,
+            log=self.log,
+            include_high=self.include_high,
+        )
+        if self.q is not None:
+            res["q"] = self.q
+        return res
+
+    def generate(self, seed: Any = None) -> float:
+        if seed is not None:
+            np.random.seed(seed)
+        value = np.random.uniform()
+        if self.q is None:
+            return float(
+                uniform_to_continuous(value, self.low, self.high, log=self.log)
+            )
+        else:
+            return float(
+                uniform_to_discrete(
+                    value,
+                    self.low,
+                    self.high,
+                    q=self.q,
+                    log=self.log,
+                    include_high=self.include_high,
+                )
+            )
+
+
+class RandInt(RandBase):
+    """Uniform distributed random integer values.
+    Please read |SpaceTutorial|.
+
+    :param low: range low bound (inclusive)
+    :param high: range high bound (exclusive)
+    :param log: whether to do uniform sampling in log space, defaults to False.
+      If True, ``low`` must be ``>=1`` and lower values get higher chance to be sampled
+    """
+
+    def __init__(
+        self,
+        low: int,
+        high: int,
+        q: int = 1,
+        log: bool = False,
+        include_high: bool = True,
+    ):
+        if include_high:
+            assert_or_throw(high >= low, ValueError(f"{high} < {low}"))
+        else:
+            assert_or_throw(high > low, ValueError(f"{high} <= {low}"))
+        assert_or_throw(q > 0, ValueError(q))
+        if log:
+            assert_or_throw(
+                low >= 1.0,
+                ValueError(
+                    f"for log sampling, low ({low}) must be greater or equal to 1.0"
+                ),
+            )
+        self.low = low
+        self.high = high
+        self.include_high = include_high
+        super().__init__(q, log)
+
+    @property
+    def jsondict(self) -> Dict[str, Any]:
+        return dict(
+            _expr_="randint",
+            low=self.low,
+            high=self.high,
+            q=self.q,
+            log=self.log,
+            include_high=self.include_high,
+        )
+
+    def generate(self, seed: Any = None) -> float:
+        if seed is not None:
+            np.random.seed(seed)
+        value = np.random.uniform()
+        return int(
+            uniform_to_integers(  # type: ignore
+                value,
+                self.low,
+                self.high,
+                q=int(self.q),  # type: ignore
+                log=self.log,
+                include_high=self.include_high,
+            )
+        )
+
+
+class NormalRand(RandBase):
+    """Continuous normally distributed random variables.
+    Please read |SpaceTutorial|.
+
+    :param mu: mean of the normal distribution
+    :param sigma: standard deviation of the normal distribution
+    :param q: step between adjacent values, if set, the value will be rounded
+      using ``q``, defaults to None
+    """
+
+    def __init__(
+        self,
+        mu: float,
+        sigma: float,
+        q: Optional[float] = None,
+    ):
+        assert_or_throw(sigma > 0, ValueError(sigma))
+        assert_or_throw(q is None or q > 0, ValueError(q))
+        self.mu = mu
+        self.sigma = sigma
+        super().__init__(q)
+
+    @property
+    def jsondict(self) -> Dict[str, Any]:
+        res = dict(
+            _expr_="randn",
+            mu=self.mu,
+            sigma=self.sigma,
+        )
+        if self.q is not None:
+            res["q"] = self.q
+        return res
+
+    def generate(self, seed: Any = None) -> float:
+        if seed is not None:
+            np.random.seed(seed)
+        value = np.random.normal()
+        if self.q is not None:
+            return normal_to_discrete(value, mean=self.mu, sigma=self.sigma, q=self.q)
+        else:
+            return normal_to_continuous(value, mean=self.mu, sigma=self.sigma)
+
+
+class NormalRandInt(RandBase):
+    """Normally distributed random integer values.
+    Please read |SpaceTutorial|.
+
+    :param mu: mean of the normal distribution
+    :param sigma: standard deviation of the normal distribution
+    """
+
+    def __init__(
+        self,
+        mu: int,
+        sigma: float,
+        q: int = 1,
+    ):
+        assert_or_throw(sigma > 0, ValueError(sigma))
+        assert_or_throw(q > 0, ValueError(q))
+        self.mu = mu
+        self.sigma = sigma
+        self.q = q
+        super().__init__(q)
+
+    @property
+    def jsondict(self) -> Dict[str, Any]:
+        return dict(
+            _expr_="randnint",
+            mu=self.mu,
+            sigma=self.sigma,
+            q=self.q,
+        )
+
+    def generate(self, seed: Any = None) -> int:
+        if seed is not None:
+            np.random.seed(seed)
+        value = np.random.normal()
+        return int(
+            normal_to_integers(  # type: ignore
+                value,
+                mean=self.mu,
+                sigma=self.sigma,
+                q=self.q,  # type: ignore
+            )
+        )
+
+
+def _encode_params(value: Any) -> Any:
+    if isinstance(value, StochasticExpression):
+        return value.jsondict
+    elif isinstance(value, str):
+        return value
+    elif isinstance(value, list):
+        return [_encode_params(v) for v in value]
+    elif isinstance(value, dict):
+        return {k: _encode_params(v) for k, v in value.items()}
+    return value
+
+
+def _decode_params(value: Any) -> Any:
+    if isinstance(value, str):
+        return value
+    elif isinstance(value, list):
+        return [_decode_params(v) for v in value]
+    elif isinstance(value, dict):
+        if "_expr_" in value:
+            e = value.pop("_expr_")
+            if e == "choice":  # TODO: embeded rand is not supported
+                return Choice(*value["values"])
+            if e == "rand":
+                return Rand(**value)
+            if e == "randint":
+                return RandInt(**value)
+            if e == "randn":
+                return NormalRand(**value)
+            if e == "randnint":
+                return NormalRandInt(**value)
+            raise ValueError(e)  # pragma: no cover
+        else:
+            return {k: _decode_params(v) for k, v in value.items()}
+    else:
+        return value
