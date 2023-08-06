fb71d1fb72793f02080708534a08db415c5d4292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:41 +0000

    Add package berval-distributions/1.2/berval_distributions-1.2.tar.gz

---
+++ packages/berval-distributions/berval_distributions-1.2.tar.gz/berval_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='berval_distributions',
+      version='1.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author='camberval',
+      author_email='cam@gmail.com',
+      zip_safe=False)
