74fc71544991e3853582629015cd86918984a1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:36 +0000

    Add package jawad-distributions/0.1/jawad_distributions-0.1.tar.gz

---
+++ packages/jawad-distributions/jawad_distributions-0.1.tar.gz/jawad_distributions-0.1/README.md
@@ -0,0 +1,3 @@
+# jawad-distributions package
+
+This package contains Bionomial and Gaussian distributions followed by following instructions
---
+++ packages/jawad-distributions/jawad_distributions-0.1.tar.gz/jawad_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jawad_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jawad_distributions'],
+      author='Jawad Rashid',
+      author_email='jawad@ndataconsulting.com',
+      zip_safe=False)
