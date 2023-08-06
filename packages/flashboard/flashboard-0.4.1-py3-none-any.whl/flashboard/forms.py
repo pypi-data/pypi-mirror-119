be56feeda83efb395b296fe78c41bd1e62e109c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:51 +0000

    Add package flashboard/0.4.1/flashboard-0.4.1-py3-none-any.whl

---
+++ packages/flashboard/flashboard-0.4.1-py3-none-any.whl/flashboard/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from tournamentcontrol.competition.forms import ConfigurationForm
+
+
+class FlashConfigurationForm(ConfigurationForm):
+    def __init__(self, *args, **kwargs):
+        super(FlashConfigurationForm, self).__init__(*args, **kwargs)
+
+        self.fields["delay"] = forms.IntegerField(
+            required=False,
+            help_text="Number of seconds to wait before moving to the next division.",
+        )
