2d2d1c53e91b39585aea366f8d6c561fd40f04c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:23 +0000

    Add package docker-image-cleaner/1.0.0a0/docker-image-cleaner-1.0.0a0.tar.gz

---
+++ packages/docker-image-cleaner/docker-image-cleaner-1.0.0a0.tar.gz/docker-image-cleaner-1.0.0a0/setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from setuptools import find_packages
+from setuptools import setup
+
+with open(Path(__file__).parent / "requirements.in") as f:
+    requirements = [l.strip() for l in f.readlines() if not l.strip().startswith("#")]
+
+setup(
+    name="docker-image-cleaner",
+    version="1.0.0a0",
+    python_requires=">=3.6",
+    author="Project Jupyter Contributors",
+    author_email="jupyter@googlegroups.com",
+    license="BSD",
+    packages=find_packages(),
+    url="https://github.com/jupyterhub/docker-image-cleaner",
+    description="Cleanup old docker images to free up disk space and inodes",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "docker-image-cleaner = docker_image_cleaner.__main__:main",
+        ]
+    },
+)
