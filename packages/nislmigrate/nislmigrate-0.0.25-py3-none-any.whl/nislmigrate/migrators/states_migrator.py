6708bc5e21eaf98c58572e7c2d48a58d1c47a60d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:10 +0000

    Add package nislmigrate/0.0.25/nislmigrate-0.0.25-py3-none-any.whl

---
+++ packages/nislmigrate/nislmigrate-0.0.25-py3-none-any.whl/nislmigrate/migrators/states_migrator.py
@@ -0,0 +1,70 @@
+import os
+
+from nislmigrate.facades.facade_factory import FacadeFactory
+from nislmigrate.extensibility.migrator_plugin import MigratorPlugin
+from nislmigrate.facades.file_system_facade import FileSystemFacade
+from nislmigrate.facades.mongo_configuration import MongoConfiguration
+from nislmigrate.facades.mongo_facade import MongoFacade
+from typing import Any, Dict
+
+
+class StatesMigrator(MigratorPlugin):
+
+    @property
+    def name(self):
+        return 'SystemsStateManager'
+
+    @property
+    def argument(self):
+        return 'states'
+
+    @property
+    def help(self):
+        return 'Migrate system states'
+
+    __data_directory = os.path.join(
+        str(os.environ.get('ProgramData')),
+        'National Instruments',
+        'Skyline',
+        'Data',
+        'SystemsStateManager')
+
+    def capture(self, migration_directory: str, facade_factory: FacadeFactory, arguments: Dict[str, Any]):
+        mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
+        file_facade: FileSystemFacade = facade_factory.get_file_system_facade()
+        mongo_configuration: MongoConfiguration = MongoConfiguration(self.config(facade_factory))
+        file_migration_directory = os.path.join(migration_directory, 'files')
+
+        mongo_facade.capture_database_to_directory(
+            mongo_configuration,
+            migration_directory,
+            self.name)
+        file_facade.copy_directory(
+            self.__data_directory,
+            file_migration_directory,
+            False)
+
+    def restore(self, migration_directory: str, facade_factory: FacadeFactory, arguments: Dict[str, Any]):
+        mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
+        file_facade: FileSystemFacade = facade_factory.get_file_system_facade()
+        mongo_configuration: MongoConfiguration = MongoConfiguration(self.config(facade_factory))
+        file_migration_directory = os.path.join(migration_directory, 'files')
+
+        mongo_facade.restore_database_from_directory(
+            mongo_configuration,
+            migration_directory,
+            self.name)
+        file_facade.copy_directory(
+            file_migration_directory,
+            self.__data_directory,
+            True)
+
+    def pre_restore_check(
+            self,
+            migration_directory: str,
+            facade_factory: FacadeFactory,
+            arguments: Dict[str, Any]) -> None:
+        mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
+        mongo_facade.validate_can_restore_database_from_directory(
+            migration_directory,
+            self.name)
