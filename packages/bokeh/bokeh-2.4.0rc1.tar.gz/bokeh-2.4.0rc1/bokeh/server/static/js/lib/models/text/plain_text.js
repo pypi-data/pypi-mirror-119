25a5bf84ecc17f2df4eb7d51a8a90339f49c9873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:52 +0000

    Add package bokeh/2.4.0rc1/bokeh-2.4.0rc1.tar.gz

---
+++ packages/bokeh/bokeh-2.4.0rc1.tar.gz/bokeh-2.4.0rc1/bokeh/server/static/js/lib/models/text/plain_text.js
@@ -0,0 +1,8 @@
+import { BaseText } from "./base_text";
+export class PlainText extends BaseText {
+    constructor(attrs) {
+        super(attrs);
+    }
+}
+PlainText.__name__ = "PlainText";
+//# sourceMappingURL=plain_text.js.map
---
+++ packages/bokeh/bokeh-2.4.0rc1.tar.gz/bokeh-2.4.0rc1/bokeh/server/static/js/lib/models/text/utils.js
@@ -0,0 +1,18 @@
+import { TeX } from "./math_text";
+import { isString } from "../../core/util/types";
+export function is_tex_string(text) {
+    if (!isString(text))
+        return false;
+    const dollars = /^\$\$.*?\$\$$/s;
+    const braces = /^\\\[.*?\\\]$/s;
+    const parens = /^\\\(.*?\\\)$/s;
+    return dollars.test(text) || braces.test(text) || parens.test(text);
+}
+export function tex_from_text_like(text) {
+    if (text instanceof TeX)
+        return text;
+    if (isString(text) && is_tex_string(text))
+        return new TeX({ text: text.slice(2, -2) });
+    return null;
+}
+//# sourceMappingURL=utils.js.map
