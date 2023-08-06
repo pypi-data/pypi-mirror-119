8996f613da342b628232b2d0d4765cd0308b6b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:53 +0000

    Add package dofast/0.6.7/dofast-0.6.7-py3-none-any.whl

---
+++ packages/dofast/dofast-0.6.7-py3-none-any.whl/dofast/qflask.py
@@ -0,0 +1,59 @@
+import codefast as cf
+from flask import Flask, request
+
+from dofast._flask.config import AUTH_KEY
+from dofast._flask.utils import authenticate_flask
+from dofast.security._hmac import certify_token
+from dofast.toolkits.telegram import bot_messalert
+
+from .config import SALT
+from .network import Twitter
+
+app = Flask(__name__)
+authenticate_flask(app)
+
+@app.route('/tweet', methods=['GET', 'POST'])
+def tweet():
+    msg = request.get_json()
+    text = cf.utils.decipher(AUTH_KEY, msg.get('text'))
+    media = [f'/tmp/{e}' for e in msg.get('media')]
+    cf.info(f'Input tweet: {text} / ' + ''.join(media))
+    Twitter().post([text] + media)
+    return 'SUCCESS'
+
+
+@app.route('/messalert', methods=['GET', 'POST'])
+def msg():
+    js = request.get_json()
+    bot_messalert(js['text'])
+    return 'SUCCESS'
+
+
+@app.route('/nsq', methods=['GET', 'POST'])
+def nsq():
+    msg = request.get_json()
+    topic = msg.get('topic')
+    channel = msg.get('channel')
+    data = msg.get('data')
+    cf.net.post(f'http://127.0.0.1:4151/pub?topic={topic}&channel={channel}',
+                json={'data': data})
+    print(topic, channel, data)
+    return 'SUCCESS'
+
+
+
+
+@app.route('/')
+def hello_world():
+    _json = request.get_json()
+    decode_msg = cf.utils.decipher(AUTH_KEY, _json['msg'])
+    print(decode_msg)
+    return 'SUCCESS!'
+
+
+def run():
+    app.run(host='0.0.0.0', port=6363, debug=True)
+
+
+if __name__ == '__main__':
+    run()
