e25aae01eaf92d7e6d12a34987cb38957e0c2259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:02 +0000

    Add package securityscorecard-api/0.10.8/securityscorecard_api-0.10.8-py3-none-any.whl

---
+++ packages/securityscorecard-api/securityscorecard_api-0.10.8-py3-none-any.whl/securityscorecard_api/client.py
@@ -0,0 +1,24 @@
+
+from requests.sessions import session
+import urllib3
+from urllib3.exceptions import InsecureRequestWarning
+urllib3.disable_warnings(InsecureRequestWarning)
+from securityscorecard_api.compliance import Compliance
+from securityscorecard_api.events import Events
+from securityscorecard_api.scores import Scores
+
+
+class SecurityScorecardClient(Scores, Events, Compliance):
+    '''
+        Primary Client Class 
+    '''
+    def __init__(self, api_token) -> None:
+        self.session = session()
+        self.session.headers = {
+            "Accept": "application/json; charset=utf-8",
+            "Authorization": "Token "+ api_token
+        }
+
+    def _perform_get(self, api_url):
+        response = self.session.get(api_url, verify=False)
+        return (response.json())
