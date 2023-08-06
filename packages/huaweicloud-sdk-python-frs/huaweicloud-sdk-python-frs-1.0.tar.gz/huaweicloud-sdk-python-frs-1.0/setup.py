60f516b4823d33ef3c5fadf7461a553a8b8fb7b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:24 +0000

    Add package huaweicloud-sdk-python-frs/1.0/huaweicloud-sdk-python-frs-1.0.tar.gz

---
+++ packages/huaweicloud-sdk-python-frs/huaweicloud-sdk-python-frs-1.0.tar.gz/huaweicloud-sdk-python-frs-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'huaweicloud-sdk-python-frs',
+    version = "1.0",
+    description = "FRS",
+    license = "MIT Licence",
+
+    url = "https://github.com/huaweicloud/huaweicloud-sdk-python-frs",
+    author = "sevanco",
+    author_email = "zhangqi130@huawei.com",
+)
