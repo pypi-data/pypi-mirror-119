4b8721e753abc200e3b90f6638e6f0135527b5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:19 +0000

    Add package datamatch/0.1.11/datamatch-0.1.11-py3-none-any.whl

---
+++ packages/datamatch/datamatch-0.1.11-py3-none-any.whl/datamatch/__init__.py
@@ -0,0 +1,14 @@
+from .indices import NoopIndex, ColumnsIndex, MultiIndex
+from .similarities import (
+    StringSimilarity, DateSimilarity, JaroWinklerSimilarity, AbsoluteNumericalSimilarity, RelativeNumericalSimilarity
+)
+from .matchers import ThresholdMatcher
+from .variators import Variator, Swap
+from .filters import DissimilarFilter, NonOverlappingFilter
+
+__all__ = [
+    "NoopIndex", "ColumnsIndex", "JaroWinklerSimilarity", "StringSimilarity",
+    "DateSimilarity", "ThresholdMatcher", "MultiIndex", "Variator", "Swap",
+    "DissimilarFilter", "NonOverlappingFilter", "AbsoluteNumericalSimilarity",
+    "RelativeNumericalSimilarity",
+]
