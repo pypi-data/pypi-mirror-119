9929d7350db23735061ba9ffbf9f0552afb03d58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:58 +0000

    Add package cicdtest/1.64/cicdtest-1.64-py3-none-any.whl

---
+++ packages/cicdtest/cicdtest-1.64-py3-none-any.whl/buildpan/yaml_reader.py
@@ -0,0 +1,29 @@
+import yaml
+
+
+def yaml_reader(path):
+    
+    try:
+        a_yaml_file = open(f"{path}/buildpan.yaml")
+        parsed_yaml_file = yaml.load(a_yaml_file, Loader=yaml.FullLoader)
+
+        print("in")
+
+        # tool version 
+        yaml_reader.version = parsed_yaml_file["version"]
+
+        # reading Project id 
+        yaml_reader.project_id = parsed_yaml_file['projotectDetail']["projectid"]
+
+        # Redeaing Platform and and its version 
+        platform = parsed_yaml_file['projotectDetail']["platform"]
+        temp_index=platform.find(":")
+        platform_ver = platform[temp_index + 1:].strip()
+        platform_name = platform[:temp_index].strip()
+
+        # jobs
+        jobs = parsed_yaml_file["jobs"]
+    
+    except:
+
+        print("Invalid format")
