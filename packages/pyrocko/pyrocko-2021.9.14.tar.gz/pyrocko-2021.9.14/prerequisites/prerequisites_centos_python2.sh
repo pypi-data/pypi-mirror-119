06ab1eb0dc608c453106e51d6186aef7aa6c7bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:44 +0000

    Add package pyrocko/2021.9.14/pyrocko-2021.9.14.tar.gz

---
+++ packages/pyrocko/pyrocko-2021.9.14.tar.gz/pyrocko-2021.9.14/prerequisites/prerequisites_centos_python2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo yum -y install make gcc patch git python python-yaml python-matplotlib numpy \
+    scipy python-requests python-coverage python-nose python-jinja2 \
+    PyQt4 python-matplotlib-qt4
