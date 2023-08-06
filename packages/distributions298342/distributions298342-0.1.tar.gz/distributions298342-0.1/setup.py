bcff8c56b6f4320da7b92a3d6bce7cbe55cdcdd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:42 +0000

    Add package distributions298342/0.1/distributions298342-0.1.tar.gz

---
+++ packages/distributions298342/distributions298342-0.1.tar.gz/distributions298342-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions298342',
+      version='0.1',
+      description='Gaussian distributions and Binomial distribution class',
+      packages=['distributions298342'],
+      author = 'Sudeendra',
+      zip_safe=False)
