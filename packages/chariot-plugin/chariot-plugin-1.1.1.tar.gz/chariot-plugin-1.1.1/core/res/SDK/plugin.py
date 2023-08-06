db501e320dd941bb96ea43ef6e507cc34c9b0a4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:51 +0000

    Add package chariot-plugin/1.1.1/chariot-plugin-1.1.1.tar.gz

---
+++ packages/chariot-plugin/chariot-plugin-1.1.1.tar.gz/chariot-plugin-1.1.1/core/res/SDK/plugin.py
@@ -0,0 +1,33 @@
+
+
+
+"""
+搜集插件各个类
+"""
+
+
+class Plugin(object):
+
+
+    def __init__(self):
+
+        # 插件的３个组件
+        self.connection = {}
+        self.actions = {}
+        self.triggers = {}
+        self.indicator_receivers = {}
+
+
+    def add_connection(self, connect):
+        self.connection[connect.name] = connect
+
+    def add_actions(self, action):
+        self.actions[action.name] = action
+
+    def add_triggers(self, trigger):
+        self.triggers[trigger.name] = trigger
+
+    def add_indicator_receivers(self, indicator_receiver):
+        self.indicator_receivers[indicator_receiver.name] = indicator_receiver
+
+
