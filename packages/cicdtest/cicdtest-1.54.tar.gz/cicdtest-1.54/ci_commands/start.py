c42f94d86fc1bb009f6ab692755865cf27870b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:53 +0000

    Add package cicdtest/1.54/cicdtest-1.54.tar.gz

---
+++ packages/cicdtest/cicdtest-1.54.tar.gz/cicdtest-1.54/ci_commands/start.py
@@ -0,0 +1,21 @@
+import click
+from click.decorators import command
+from crontab import CronTab
+
+@click.command()
+def start():
+    '''
+    For initiating the pull operation 
+
+    Please store config.yaml in the directory 
+    Please create the clone of the repository  
+    \f
+    
+   
+    '''
+
+    cron_job = CronTab(user=True)
+    job = cron_job.new(command='/home/kush/.local/bin/buildpan pull')
+    job.minute.every(1)
+    cron_job.write()
+    print("called")
