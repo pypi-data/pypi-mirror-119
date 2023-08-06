8cb6ee9592aa881ab512864b63f3b090ecc94faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:25 +0000

    Add package simpleos/0.0.1/simpleos-0.0.1.tar.gz

---
+++ packages/simpleos/simpleos-0.0.1.tar.gz/simpleos-0.0.1/README.txt
@@ -0,0 +1,3 @@
+clear() - clears cmd
+location() - shows current location 
+pause() - 'Press any key to continue ...' phrase
---
+++ packages/simpleos/simpleos-0.0.1.tar.gz/simpleos-0.0.1/simpleos/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+# ------ CLEAR THE CONSOLE ------
+def clear():
+    os.system('cls')
+
+# ------ SHOW YOUR WORKING LOCATION ------
+def location():
+    os.system('cd')
+
+# ------ DONT LET CONSOLE CLOSE ------
+def pause():
+    os.system('pause')
