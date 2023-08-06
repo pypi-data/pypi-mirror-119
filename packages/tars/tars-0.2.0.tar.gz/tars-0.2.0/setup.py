eb3093f02449b13230649d3a073ff8d1ea72ea1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:51 +0000

    Add package tars/0.2.0/tars-0.2.0.tar.gz

---
+++ packages/tars/tars-0.2.0.tar.gz/tars-0.2.0/setup.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+
+def read_requirements(path):
+    return list(Path(path).read_text().splitlines())
+
+
+setup(
+    name='tars',
+    packages=find_packages(),
+    version='0.2.0',
+    description='A cryptocurrency trading bot for research',
+    author='Fred Montet',
+    license='MIT',
+    install_requires=[
+          read_requirements("requirements.txt")
+    ],
+)
