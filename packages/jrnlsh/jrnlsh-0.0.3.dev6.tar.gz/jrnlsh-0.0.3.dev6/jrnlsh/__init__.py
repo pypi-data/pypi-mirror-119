8440d73280bd29dba8d470daba8699543e6e52fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:50 +0000

    Add package jrnlsh/0.0.3.dev6/jrnlsh-0.0.3.dev6.tar.gz

---
+++ packages/jrnlsh/jrnlsh-0.0.3.dev6.tar.gz/jrnlsh-0.0.3.dev6/jrnlsh/__init__.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+from cmd import Cmd
+from subprocess import Popen
+from shlex import quote
+
+import shutil
+import sys
+
+class JrnlShell(Cmd):
+    def run_command(self, command):
+        process = Popen(command, shell=True)
+        process.wait()
+
+    def default(self, line):
+        self.run_command('jrnl %s' % (quote(line)))
+
+    def do_list(self, line):
+        self.run_command('jrnl -from today')
+
+    def do_edit(self, line):
+        self.run_command('jrnl --edit')
+
+    def do_quit(self, line):
+        sys.exit(0)
+
+    def do_time(self, line):
+        run_command('timew start %s' % (quote(line)))
+
+    def do_start(self, line):
+        run_command('timew start %s' % (quote(line)))
+
+    def do_stop(self, line):
+        run_command('timew stop')
+
+    def do_fill(self, line):
+        run_command('timew start :fill %s' % (quote(line)))
+
+    def do_exit(self, line):
+        sys.exit(0)
+
+    def do_EOF(self, line):
+        sys.exit(0)
+
+def run_cli():
+    if shutil.which('jrnl') is None:
+        raise(Exception('Unable to find \'jrnl\' command.  Is jrnl installed?'))
+
+    prompt = JrnlShell()
+    prompt.prompt = 'jrnl> '
+    prompt.cmdloop()
+
+if __name__ == '__main__':
+    run_cli()
