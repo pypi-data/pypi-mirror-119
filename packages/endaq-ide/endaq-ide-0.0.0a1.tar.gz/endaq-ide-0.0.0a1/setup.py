2f739dd28bf98c937d925af8f8c8f41b10dd1cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package endaq-ide/0.0.0a1/endaq-ide-0.0.0a1.tar.gz

---
+++ packages/endaq-ide/endaq-ide-0.0.0a1.tar.gz/endaq-ide-0.0.0a1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+        name='endaq-ide',
+        version='0.0.0a1',
+        author='Mide Technology',
+        author_email='help@mide.com',
+        packages=[],
+        package_dir={'endaq-ide': './endaq'},
+        install_requires=[],
+)
