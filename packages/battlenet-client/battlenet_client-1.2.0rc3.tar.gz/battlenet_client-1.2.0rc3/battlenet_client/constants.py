6f1304a1a359c9c5bf3b930f0a04aac0571fe9fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:29 +0000

    Add package battlenet-client/1.2.0rc3/battlenet_client-1.2.0rc3.tar.gz

---
+++ packages/battlenet-client/battlenet_client-1.2.0rc3.tar.gz/battlenet_client-1.2.0rc3/battlenet_client/constants.py
@@ -0,0 +1,11 @@
+UNITED_STATES = 'us'
+EUROPE = 'eu'
+TAIWAN = 'tw'
+KOREA = 'kr'
+CHINA = 'cn'
+
+WOW = {'abbrev': 'wow', 'name': 'World of Warcraft'}
+D3 = {'abbrev': 'd3', 'name': 'Diablo III'}
+SC2 = {'abbrev': 'sc2', 'name': 'Starcraft II'}
+HS = {'abbrev': 'hs', 'name': 'Heartsthone'}
+OW = {'abbrev': 'ow', 'name': 'Overwatch'}
