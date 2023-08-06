cd28ec9334139a12413489db70c40eb59dd7fbf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:53 +0000

    Add package westerLee111/1.1.0/westerLee111-1.1.0.tar.gz

---
+++ packages/westerLee111/westerLee111-1.1.0.tar.gz/westerLee111-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name           = 'westerLee111',
+      version        = '1.1.0',
+      py_modules     = ['westerLee111'],
+      author         = 'jianyuanli',
+      author_email    = '1302733407@qq.com',
+      url            = 'https://pypi.org',
+      description    = 'A simple printer of nested lists',
+      )
