06f26728eb78ae4a64d3c25f89e4daea9dd05650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:36 +0000

    Add package base-folder-atstaeff/0.0.1/base-folder-atstaeff-0.0.1.tar.gz

---
+++ packages/base-folder-atstaeff/base-folder-atstaeff-0.0.1.tar.gz/base-folder-atstaeff-0.0.1/setup.py
@@ -0,0 +1,34 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Streaming video data via networks'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="base-folder-atstaeff",
+    version=VERSION,
+    author="Stefan Poss)",
+    author_email="sp@stefanposs.de",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
