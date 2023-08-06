8d271c8c7d46d375ffd1478c3d97e7017e75e648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:05 +0000

    Add package joonmyung/0.0.2/JoonMyung-0.0.2.tar.gz

---
+++ packages/joonmyung/JoonMyung-0.0.2.tar.gz/JoonMyung-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="JoonMyung",
+    version="0.0.2",
+    description="JoonMyung's Library",
+    url="https://github.com/pizard/JoonMyung.git",
+    author="JoonMyung Choi",
+    author_email="pizard@korea.ac.kr",
+    license="JoonMyung",
+    package=["utils"],
+    zip_safe=False,
+    install_requires=[
+
+    ]
+)
