b5cd9dc539cb3e47880ea66620083fc43cbc61dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:27 +0000

    Add package cobli-libi/0.0.1/cobli-libi-0.0.1.tar.gz

---
+++ packages/cobli-libi/cobli-libi-0.0.1.tar.gz/cobli-libi-0.0.1/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='cobli-libi',
+    version='0.0.1',
+    description='Generate dataframes from Cobli public API',
+    author='Marcus Beckenkamp',
+    author_email='mvbeck@gmai.com',
+    url='https://docs.cobli.co',
+    packages=['libi', ],
+)
