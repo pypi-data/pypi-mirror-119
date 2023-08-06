76c3eba00ee678877765c0c9c82942573d23cf70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:12 +0000

    Add package JBS-dsnd-distributions/0.1/JBS_dsnd_distributions-0.1.tar.gz

---
+++ packages/JBS-dsnd-distributions/JBS_dsnd_distributions-0.1.tar.gz/JBS_dsnd_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='JBS_dsnd_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['JBS_dsnd_distributions'],
+      zip_safe=False)
