6af6f61222b6669933a0af00c66e55ffd7501671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:04 +0000

    Add package pop-create-idem/0.3.1/pop-create-idem-0.3.1.tar.gz

---
+++ packages/pop-create-idem/pop-create-idem-0.3.1.tar.gz/pop-create-idem-0.3.1/pop_create_idem/pop_create/botocore/hooks/post_gen_project.py
@@ -0,0 +1,25 @@
+import pathlib
+
+import pop.hub
+from dict_tools.data import NamespaceDict
+
+if __name__ == "__main__":
+    root_directory = pathlib.Path.cwd()
+    hub = pop.hub.Hub()
+    hub.pop.sub.add(dyne_name="tool")
+    ctx = NamespaceDict({{cookiecutter}})
+
+    hub.tool.path.rmtree(
+        root_directory
+        / ctx.clean_name
+        / "exec"
+        / ctx.service_name
+        / ctx.clean_api_version
+        / "recursive_contracts"
+    )
+    try:
+        (
+            root_directory / ctx.clean_name / "exec" / ctx.service_name / "sample.py"
+        ).unlink()
+    except FileNotFoundError:
+        ...
