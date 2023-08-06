5e52534ca46f5bf9753dd7ab0e82e5a24d900d60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:43 +0000

    Add package sqlglot/1.3.2/sqlglot-1.3.2-py3-none-any.whl

---
+++ packages/sqlglot/sqlglot-1.3.2-py3-none-any.whl/sqlglot/__init__.py
@@ -0,0 +1,21 @@
+from sqlglot.dialects import Dialect
+from sqlglot.expressions import Expression
+from sqlglot.generator import Generator
+from sqlglot.tokens import Tokenizer, TokenType
+from sqlglot.parser import Parser
+
+
+__version__ = '1.3.2'
+
+
+def parse(code, read=None, **opts):
+    dialect = Dialect.get(read, Dialect)()
+    return dialect.parse(code, **opts)
+
+
+def transpile(code, read=None, write=None, identity=True, error_level=None, **opts):
+    write = write or read if identity else write
+    return [
+        Dialect.get(write, Dialect)().generate(expression, **opts)
+        for expression in parse(code, read, error_level=error_level)
+    ]
