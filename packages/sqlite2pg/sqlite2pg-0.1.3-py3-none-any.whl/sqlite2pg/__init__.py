2735a88fcbd3027967c6c92c814875234bcecba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:46 +0000

    Add package sqlite2pg/0.1.3/sqlite2pg-0.1.3-py3-none-any.whl

---
+++ packages/sqlite2pg/sqlite2pg-0.1.3-py3-none-any.whl/sqlite2pg/__init__.py
@@ -0,0 +1,43 @@
+"""An SQLite3 to PostgreSQL database migration tool."""
+import os
+import typing
+from pathlib import Path
+
+if os.name == "nt":
+    HOME_CONFIG = Path.home() / ".sqlite2pg"
+    LOG_CONFIG = HOME_CONFIG / "logs"
+else:
+    HOME_CONFIG = Path.home() / ".config/sqlite2pg"
+    LOG_CONFIG = HOME_CONFIG / "logs"
+
+DEV_HOME_CONFIG = Path(".") / ".config/sqlite2pg"
+DEV_LOG_CONFIG = DEV_HOME_CONFIG / "logs"
+
+from .modules import *
+from .cli import *
+
+__version__: str = "0.1.3"
+__author__: str = "Jonxslays"
+__maintainer__: str = "Jonxslays"
+__license__: str = "BSD-3-Clause"
+__url__: str = "https://github.com/Jonxslays/sqlite2pg"
+
+__all__: typing.List[str] = [
+    "Worker",
+    "S2PLogger",
+    "Sqlite2pgError",
+    "SqliteError",
+    "LoggingConfigError",
+    "PostgresError",
+    "__version__",
+    "__author__",
+    "__maintainer__",
+    "__license__",
+    "__url__",
+    "CommandHandler",
+    "HOME_CONFIG",
+    "DEV_HOME_CONFIG",
+    "DEV_LOG_CONFIG",
+    "CONFIG_SCHEMA",
+    "CleanSchemaT",
+]
