ddcdbb327fc39224df5a29f93522958289fbcfc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:20 +0000

    Add package weboa/0.5.7/weboa-0.5.7.tar.gz

---
+++ packages/weboa/weboa-0.5.7.tar.gz/weboa-0.5.7/weboa/__init__.py
@@ -0,0 +1,10 @@
+__VERSION__ = "0.5.7"
+
+import os
+import json
+from . import prepare
+from .project import *
+from .utils import *
+from .libraries import *
+
+__VERSION__ = __VERSION__
---
+++ packages/weboa/weboa-0.5.7.tar.gz/weboa-0.5.7/weboa_res/phpfs/site.php
@@ -0,0 +1,18 @@
+<?php
+/*LIBS*/
+require_once("php/lib/autoload.php");
+require_once("php/db.php");
+require_once("php/controller/controller.php");
+?>
+<!DOCTYPE HTML>
+<html lang='<?=$lang;?>'>
+<head>
+    <?php include("php/modules/header.phtml");?>
+</head>
+<body>
+<?php
+    include_once "php/controller/index.php";
+    include_once "php/modules/footer.phtml";
+?>
+</body>
+</html>
