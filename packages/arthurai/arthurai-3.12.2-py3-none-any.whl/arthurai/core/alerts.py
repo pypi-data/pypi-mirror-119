9c1c0ff904ac35470f7ab15191fe3c81f0110c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:54 +0000

    Add package arthurai/3.12.2/arthurai-3.12.2-py3-none-any.whl

---
+++ packages/arthurai/arthurai-3.12.2-py3-none-any.whl/arthurai/core/alerts.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from typing import Union, Optional, Dict, Any
+
+from arthurai.common.constants import ListableStrEnum
+from arthurai.core.base import ArthurBaseJsonDataclass, NumberType
+
+
+class AlertRuleBound(ListableStrEnum):
+    Upper = "upper"
+    Lower = "lower"
+
+
+class AlertRuleSeverity(ListableStrEnum):
+    Warning = "warning"
+    Critical = "critical"
+
+
+@dataclass
+class AlertRule(ArthurBaseJsonDataclass):
+    bound: AlertRuleBound
+    threshold: NumberType
+    metric_id: str
+    severity: AlertRuleSeverity
+    name: Optional[str] = None
+    lookback_period: Optional[NumberType] = None
+    subsequent_alert_wait_time: Optional[NumberType] = None
+    enabled: bool = True
+    id: Optional[str] = None
+    metric_name: Optional[str] = None
+
+
+class MetricType(ListableStrEnum):
+    ModelOutputMetric = "model_output_metric"
+    ModelInputDataMetric = "model_input_data_metric"
+    ModelPerformanceMetric = "model_performance_metric"
+    ModelDataDriftMetric = "model_data_drift_metric"
+
+
+@dataclass
+class Metric(ArthurBaseJsonDataclass):
+    id: str
+    name: str
+    query: Dict[str, Any]
+    is_default: bool
+    type: Optional[MetricType] = None
+    attribute: Optional[str] = None
