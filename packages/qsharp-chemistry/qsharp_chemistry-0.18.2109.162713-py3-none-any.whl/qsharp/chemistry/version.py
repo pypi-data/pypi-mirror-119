997e55fc98dacce75eeb9a4021c645b62e727bcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:01 +0000

    Add package qsharp-chemistry/0.18.2109.162713/qsharp_chemistry-0.18.2109.162713-py3-none-any.whl

---
+++ packages/qsharp-chemistry/qsharp_chemistry-0.18.2109.162713-py3-none-any.whl/qsharp/chemistry/version.py
@@ -0,0 +1,9 @@
+# Auto-generated file, do not edit.
+##
+# version.py: Specifies the version of the qsharp-chemistry package.
+##
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+##
+__version__ = "0.18.2109.162713"
+_user_agent_extra = ""
