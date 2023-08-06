770bba3205b9ce35dfd8b61dd44d08ebf2c7e784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:18 +0000

    Add package edc-appointment/0.3.12/edc_appointment-0.3.12-py3-none-any.whl

---
+++ packages/edc-appointment/edc_appointment-0.3.12-py3-none-any.whl/edc_appointment/auths.py
@@ -0,0 +1,22 @@
+from edc_auth.default_group_names import AUDITOR, CLINIC, EXPORT
+from edc_auth.site_auths import site_auths
+
+site_auths.update_group(
+    "edc_appointment.view_appointment",
+    "edc_appointment.view_historicalappointment",
+    name=AUDITOR,
+)
+
+site_auths.update_group(
+    "edc_appointment.add_appointment",
+    "edc_appointment.change_appointment",
+    "edc_appointment.view_appointment",
+    "edc_appointment.view_historicalappointment",
+    name=CLINIC,
+)
+
+site_auths.update_group(
+    "edc_appointment.export_appointment",
+    "edc_appointment.export_historicalappointment",
+    name=EXPORT,
+)
