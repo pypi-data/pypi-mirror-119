49293174d7fb7b1df6136d0db3741f3ed49160d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:40:56 +0000

    Add package ogdf-python/0.1.2.dev0/ogdf_python-0.1.2.dev0-py3-none-any.whl

---
+++ packages/ogdf-python/ogdf_python-0.1.2.dev0-py3-none-any.whl/ogdf_python/__init__.py
@@ -0,0 +1,41 @@
+import os
+import warnings
+
+import cppyy.ll
+from cppyy import include as cppinclude, cppdef, cppexec, nullptr
+
+cppyy.ll.set_signals_as_exception(True)
+cppyy.add_include_path(os.path.dirname(os.path.realpath(__file__)))
+
+if "OGDF_INSTALL_DIR" in os.environ:
+    INSTALL_DIR = os.path.expanduser(os.getenv("OGDF_INSTALL_DIR"))
+    cppyy.add_include_path(os.path.join(INSTALL_DIR, "include"))
+    cppyy.add_library_path(os.path.join(INSTALL_DIR, "lib"))
+elif "OGDF_BUILD_DIR" in os.environ:
+    BUILD_DIR = os.path.expanduser(os.getenv("OGDF_BUILD_DIR"))
+    cppyy.add_include_path(os.path.join(BUILD_DIR, "include"))
+    cppyy.add_include_path(os.path.join(BUILD_DIR, "..", "include"))
+    cppyy.add_library_path(BUILD_DIR)
+else:
+    warnings.warn("ogdf-python couldn't find OGDF. "
+                  "Please set environment variables OGDF_INSTALL_DIR or OGDF_BUILD_DIR.")
+
+cppyy.cppdef("#undef NDEBUG")
+cppyy.include("ogdf/basic/internal/config_autogen.h")
+cppyy.include("ogdf/basic/internal/config.h")
+cppyy.include("ogdf/basic/Graph.h")
+cppyy.include("ogdf/fileformats/GraphIO.h")
+
+cppyy.load_library("libOGDF.so")
+
+import ogdf_python.doxygen
+import ogdf_python.pythonize
+import ogdf_python.jupyter
+from cppyy.gbl import ogdf
+
+__all__ = ["ogdf", "cppinclude", "cppdef", "cppexec", "nullptr"]
+__keep_imports = [cppyy,
+                  ogdf_python.doxygen,
+                  ogdf_python.pythonize,
+                  ogdf_python.jupyter,
+                  ogdf, cppinclude, cppdef, cppexec, nullptr]
---
+++ packages/ogdf-python/ogdf_python-0.1.2.dev0-py3-none-any.whl/ogdf_python/test.py
@@ -0,0 +1,1 @@
+from ogdf_python import ogdf,cppinclude
