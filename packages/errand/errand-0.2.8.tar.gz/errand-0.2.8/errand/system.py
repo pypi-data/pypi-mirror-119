b32375449f138964627f4c77f67f8c9095a96716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:27 +0000

    Add package errand/0.2.8/errand-0.2.8.tar.gz

---
+++ packages/errand/errand-0.2.8.tar.gz/errand-0.2.8/errand/system.py
@@ -0,0 +1,51 @@
+"""Errand System module
+
+
+"""
+
+import abc
+
+
+class System(abc.ABC):
+    """Errand system class
+
+"""
+
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def isavail(self):
+        pass
+
+
+class CPUSystem(System):
+
+    def isavail(self):
+        return True
+
+class NvidiaGPUSystem(System):
+
+    def isavail(self):
+        return True
+
+
+class AmdGPUSystem(System):
+
+    def isavail(self):
+        return True
+
+
+def select_system(name):
+
+    if name == "cpu":
+        return CPUSystem()
+
+    elif name == "nvidia-gpu":
+        return NvidiaGPUSystem()
+
+    elif name == "amd-gpu":
+        return AmdGPUSystem()
+
+    else:
+        raise Exception("Unknown system: %s" % name)
