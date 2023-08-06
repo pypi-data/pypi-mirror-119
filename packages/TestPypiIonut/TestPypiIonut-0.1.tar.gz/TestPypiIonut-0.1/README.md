3d8fed691504a068fe64d0e1276f426ce6c4849a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:25 +0000

    Add package TestPypiIonut/0.1/TestPypiIonut-0.1.tar.gz

---
+++ packages/TestPypiIonut/TestPypiIonut-0.1.tar.gz/TestPypiIonut-0.1/README.md
@@ -0,0 +1,5 @@
+Ce faceti?   
+
+
+
+###Bine,voi?
---
+++ packages/TestPypiIonut/TestPypiIonut-0.1.tar.gz/TestPypiIonut-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+setup(
+    name="TestPypiIonut",
+    packages=["TestPypiIonut"],
+    version="0.1",
+    description="Do not install",
+    author="Bucur Ionut",
+    author_email="random@random.com",
+    keywords=["do", "not", "use"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.9"
+
+    ]
+)
