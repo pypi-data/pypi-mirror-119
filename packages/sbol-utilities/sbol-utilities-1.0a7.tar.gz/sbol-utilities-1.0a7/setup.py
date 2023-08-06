d364240071319db7417c4e58d90acdeb7d0c2d9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:57 +0000

    Add package sbol-utilities/1.0a7/sbol-utilities-1.0a7.tar.gz

---
+++ packages/sbol-utilities/sbol-utilities-1.0a7.tar.gz/sbol-utilities-1.0a7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(name='sbol-utilities',
+      description='SBOL-utilities',
+      long_description='SBOL-utilities is a collection of scripts and functions for manipulating SBOL 3 data that '
+                       'can be imported as packages or run from the command line.',
+      long_description_content_type='text/markdown',
+      version='1.0a7',
+      install_requires=[
+            'sbol3',
+            'graphviz',
+            'tyto',
+            'openpyxl'
+            ],
+      scripts=['graph-sbol'],
+      entry_points = {
+            'console_scripts': ['excel-to-sbol=sbol_utilities.excel_to_sbol:main',
+                                'sbol-expand-derivations=sbol_utilities.expand_combinatorial_derivations:main',
+                                'sbol-calculate-sequences=sbol_utilities.calculate_sequences:main']
+      },
+      packages=['sbol_utilities'],
+      )
