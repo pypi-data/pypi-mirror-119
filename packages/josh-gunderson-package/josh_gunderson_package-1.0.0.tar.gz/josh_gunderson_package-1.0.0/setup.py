f669968c92da89b464ef1c8c2f9e6aed1e6d2656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:34 +0000

    Add package josh-gunderson-package/1.0.0/josh_gunderson_package-1.0.0.tar.gz

---
+++ packages/josh-gunderson-package/josh_gunderson_package-1.0.0.tar.gz/josh_gunderson_package-1.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+    name="josh_gunderson_package",
+    version="1.0.0",
+    author="Josh Gunderson",
+    author_email="joshg253@users.noreply.github.com",
+    description="packt example package",
+    long_description="This is the longer description and will appear in the web.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
