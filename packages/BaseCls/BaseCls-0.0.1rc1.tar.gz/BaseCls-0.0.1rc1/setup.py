b32b6601acf65ce0f6b73935b3487685c70c52e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:59 +0000

    Add package BaseCls/0.0.1rc1/BaseCls-0.0.1rc1.tar.gz

---
+++ packages/BaseCls/BaseCls-0.0.1rc1.tar.gz/BaseCls-0.0.1rc1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open('README.md', 'r', encoding='UTF-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="BaseCls",
+    version="0.0.1rc1",
+    author="BaseCls",
+    author_email="basecls@megvii.com",
+    description="BaseCls",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MegEngine",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    license='Apache 2.0',
+    keywords='MegEngine classification'
+)
