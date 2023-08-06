a91a8b09f5d21157e1095f7c8a50c3f89f08c164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:35 +0000

    Add package incendium/1.0.7/incendium-1.0.7-py2-none-any.whl

---
+++ packages/incendium/incendium-1.0.7-py2-none-any.whl/incendium/vision/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2020-2021
+# Author: Cesar Roman
+# Contact: cesar@thecesrom.dev
+
+"""Vision Module.
+
+The Vision Module brings robust SCADA visualization to Ignition, giving
+you access to real-time and historical data on your HMIs and dashboard
+screens.
+"""
