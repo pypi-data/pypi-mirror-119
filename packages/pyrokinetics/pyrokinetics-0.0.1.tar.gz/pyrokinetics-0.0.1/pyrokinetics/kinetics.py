2c6996ab14815154642d5de1cf14a2ad005db201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:53 +0000

    Add package pyrokinetics/0.0.1/pyrokinetics-0.0.1.tar.gz

---
+++ packages/pyrokinetics/pyrokinetics-0.0.1.tar.gz/pyrokinetics-0.0.1/pyrokinetics/kinetics.py
@@ -0,0 +1,402 @@
+from scipy.interpolate import InterpolatedUnivariateSpline
+from .constants import *
+import netCDF4 as nc
+from .species import Species
+from cleverdict import CleverDict
+
+
+class Kinetics:
+    """
+    Contains all the kinetic data
+    made up of Species object
+    
+    CleverDict of Species with key being species name
+
+    kinetics['electron'] = Species(electron)
+
+    Need to do this for all species
+
+    Needs:
+    psi_n
+    r/a
+    Temperature
+    Density
+    Rotation
+    """
+
+    def __init__(
+            self,
+            kinetics_file=None,
+            kinetics_type=None,
+            nspec=None
+    ):
+
+        self.kinetics_file = kinetics_file
+        self.kinetics_type = kinetics_type
+        self.nspec = nspec
+
+        self.species_data = CleverDict()
+        self.species_names = []
+
+        if self.kinetics_type == 'SCENE':
+            self.read_scene()
+        elif self.kinetics_type == 'JETTO':
+            self.read_jetto()
+        elif self.kinetics_type == 'TRANSP':
+            self.read_transp()
+        else:
+            raise ValueError(f'{self.kinetics_type} as a source of Kinetic profiles not currently supported')
+
+    def read_scene(self):
+        """
+
+        Read NetCDF file from SCENE code
+        Assumes 3 species: e, D, T
+
+        """
+
+        kinetics_data = nc.Dataset(self.kinetics_file)
+
+        self.nspec = 3
+
+        psi = kinetics_data['Psi'][::-1]
+        psi_n = psi / psi[-1]
+
+        rho = kinetics_data['TGLF_RMIN'][::-1]
+        rho_func = InterpolatedUnivariateSpline(psi_n, rho)
+
+        electron_temp_data = kinetics_data['Te'][::-1]
+        electron_temp_func = InterpolatedUnivariateSpline(psi_n, electron_temp_data)
+
+        electron_density_data = kinetics_data['Ne'][::-1]
+        electron_density_func = InterpolatedUnivariateSpline(psi_n, electron_density_data)
+
+        electron_rotation_data = electron_temp_data * 0.0
+        electron_rotation_func = InterpolatedUnivariateSpline(psi_n, electron_rotation_data)
+
+        electron = Species(species_type='electron',
+                           charge=-1,
+                           mass=electron_mass,
+                           dens=electron_density_func,
+                           temp=electron_temp_func,
+                           rot=electron_rotation_func,
+                           rho=rho_func)
+
+        self.species_data.electron = electron
+
+        ion_temperature_func = electron_temp_func
+        ion_rotation_func = electron_rotation_func
+
+        ion_density_func = InterpolatedUnivariateSpline(psi_n, electron_density_data / 2)
+
+        deuterium = Species(species_type='deuterium',
+                            charge=1,
+                            mass=deuterium_mass,
+                            dens=ion_density_func,
+                            temp=ion_temperature_func,
+                            rot=ion_rotation_func,
+                            rho=rho_func)
+
+        self.species_data.deuterium = deuterium
+
+        tritium = Species(species_type='tritium',
+                          charge=1,
+                          mass=1.5 * deuterium_mass,
+                          dens=ion_density_func,
+                          temp=ion_temperature_func,
+                          rot=ion_rotation_func,
+                          rho=rho_func)
+
+        self.species_data.tritium = tritium
+
+        self.species_names = [*self.species_data.keys()]
+
+    def read_transp(self):
+        """ 
+        Reads in TRANSP profiles NetCDF file
+
+
+        """
+
+        kinetics_data = nc.Dataset(self.kinetics_file)
+
+        psi = kinetics_data['PLFLX'][-1, :].data
+        psi = psi - psi[0]
+        psi_n = psi / psi[-1]
+
+        rho = kinetics_data['RMNMP'][-1, :].data
+        rho = rho / rho[-1]
+
+        rho_func = InterpolatedUnivariateSpline(psi_n, rho)
+
+        nspec = 1
+
+        electron_temp_data = kinetics_data['TE'][-1, :].data
+        electron_temp_func = InterpolatedUnivariateSpline(psi_n, electron_temp_data)
+
+        electron_dens_data = kinetics_data['NE'][-1, :].data * 1e6
+        electron_dens_func = InterpolatedUnivariateSpline(psi_n, electron_dens_data)
+
+        omega_data = kinetics_data['OMEG_VTR'][-1, :].data
+        omega_func = InterpolatedUnivariateSpline(psi_n, omega_data)
+
+        electron = Species(species_type='electron',
+                           charge=-1,
+                           mass=electron_mass,
+                           dens=electron_dens_func,
+                           temp=electron_temp_func,
+                           ang=omega_func,
+                           rho=rho_func)
+
+        self.species_data.electron = electron
+
+        # TRANSP only has one ion temp
+        ion_temp_data = kinetics_data['TI'][-1, :].data
+        ion_temp_func = InterpolatedUnivariateSpline(psi_n, ion_temp_data)
+
+        # Go through each species output in TRANSP
+
+        # Deuterium
+        try:
+            deuterium_dens_data = kinetics_data['ND'][-1, :].data * 1e6
+
+            nspec += 1
+            deuterium_dens_func = InterpolatedUnivariateSpline(psi_n, deuterium_dens_data)
+
+            deuterium = Species(species_type='deuterium',
+                                charge=1,
+                                mass=deuterium_mass,
+                                dens=deuterium_dens_func,
+                                temp=ion_temp_func,
+                                ang=omega_func,
+                                rho=rho_func)
+
+            self.species_data.deuterium = deuterium
+
+        except IndexError:
+            pass
+
+        # Tritium
+        try:
+            tritium_dens_data = kinetics_data['NT'][-1, :].data * 1e6
+
+            nspec += 1
+            tritium_dens_func = InterpolatedUnivariateSpline(psi_n, tritium_dens_data)
+
+            tritium = Species(species_type='tritium',
+                              charge=1,
+                              mass=1.5 * deuterium_mass,
+                              dens=tritium_dens_func,
+                              temp=ion_temp_func,
+                              ang=omega_func,
+                              rho=rho_func)
+
+            self.species_data.tritium = tritium
+
+        except IndexError:
+            pass
+
+        # Helium 4
+        try:
+            helium4_dens_data = kinetics_data['NI4'][-1, :].data * 1e6
+
+            nspec += 1
+            helium_dens_func = InterpolatedUnivariateSpline(psi_n, helium4_dens_data)
+
+            helium = Species(species_type='helium',
+                             charge=2,
+                             mass=4 * hydrogen_mass,
+                             dens=helium_dens_func,
+                             temp=ion_temp_func,
+                             ang=omega_func,
+                             rho=rho_func)
+
+            self.species_data.helium = helium
+        except IndexError:
+            pass
+
+        # Helium 3
+        try:
+            helium3_dens_data = kinetics_data['NI3'][-1, :].data * 1e6
+
+            nspec += 1
+            helium3_dens_func = InterpolatedUnivariateSpline(psi_n, helium3_dens_data)
+
+            helium3 = Species(species_type='helium3',
+                              charge=2,
+                              mass=4 * hydrogen_mass,
+                              dens=helium3_dens_func,
+                              temp=ion_temp_func,
+                              ang=omega_func,
+                              rho=rho_func)
+
+            self.species_data.helium3 = helium3
+        except IndexError:
+            pass
+
+        try:
+            impurity_dens_data = kinetics_data['NIMP'][-1, :].data * 1e6
+
+            nspec += 1
+            impurity_dens_func = InterpolatedUnivariateSpline(psi_n, impurity_dens_data)
+
+            Z = int(kinetics_data['XZIMP'][-1].data)
+            M = int(kinetics_data['AIMP'][-1].data)
+
+            impurity = Species(species_type='impurity',
+                               charge=Z,
+                               mass=M * hydrogen_mass,
+                               dens=impurity_dens_func,
+                               temp=ion_temp_func,
+                               ang=omega_func,
+                               rho=rho_func)
+
+            self.species_data.impurity = impurity
+
+        except IndexError:
+            pass
+
+        self.nspec = nspec
+        self.species_names = [*self.species_data.keys()]
+
+    def read_jetto(self):
+        """ 
+        Reads in JETTO profiles NetCDF file
+        Loads Kinetics object
+
+        """
+
+        kinetics_data = nc.Dataset(self.kinetics_file)
+
+        psi = kinetics_data['PSI'][-1, :].data
+        psi = psi - psi[0]
+        psi_n = psi / psi[-1]
+
+        rho = kinetics_data['RMNMP'][-1, :].data
+        rho_func = InterpolatedUnivariateSpline(psi_n, rho)
+
+        nspec = 1
+
+        # Electron data
+        electron_temp_data = kinetics_data['TE'][-1, :].data
+        electron_temp_func = InterpolatedUnivariateSpline(psi_n, electron_temp_data)
+
+        electron_dens_data = kinetics_data['NE'][-1, :].data
+        electron_dens_func = InterpolatedUnivariateSpline(psi_n, electron_dens_data)
+
+        rotation_data = kinetics_data['VTOR'][-1, :].data
+        rotation_func = InterpolatedUnivariateSpline(psi_n, rotation_data)
+
+        electron = Species(species_type='electron',
+                           charge=-1,
+                           mass=electron_mass,
+                           dens=electron_dens_func,
+                           temp=electron_temp_func,
+                           rot=rotation_func,
+                           rho=rho_func)
+
+        self.species_data.electron = electron
+
+        # JETTO only has one ion temp
+        ion_temp_data = kinetics_data['TI'][-1, :].data
+        ion_temp_func = InterpolatedUnivariateSpline(psi_n, ion_temp_data)
+
+        # Go through each species output in JETTO
+
+        # Deuterium data
+        deuterium_dens_data = kinetics_data['NID'][-1, :].data
+
+        if any(deuterium_dens_data):
+            nspec += 1
+            deuterium_dens_func = InterpolatedUnivariateSpline(psi_n, deuterium_dens_data)
+
+            deuterium = Species(species_type='deuterium',
+                                charge=1,
+                                mass=deuterium_mass,
+                                dens=deuterium_dens_func,
+                                temp=ion_temp_func,
+                                rot=rotation_func,
+                                rho=rho_func)
+
+            self.species_data.deuterium = deuterium
+
+        # Tritium data
+        tritium_dens_data = kinetics_data['NIT'][-1, :].data
+
+        if any(tritium_dens_data):
+            nspec += 1
+            tritium_dens_func = InterpolatedUnivariateSpline(psi_n, tritium_dens_data)
+
+            tritium = Species(species_type='tritium',
+                              charge=1,
+                              mass=3 * hydrogen_mass,
+                              dens=tritium_dens_func,
+                              temp=ion_temp_func,
+                              rot=rotation_func,
+                              rho=rho_func)
+
+            self.species_data.tritium = tritium
+
+        # Helium data
+        alpha_dens_data = kinetics_data['NALF'][-1, :].data
+
+        if any(alpha_dens_data):
+            nspec += 1
+            alpha_dens_func = InterpolatedUnivariateSpline(psi_n, alpha_dens_data)
+
+            helium = Species(species_type='helium',
+                             charge=2,
+                             mass=2 * deuterium_mass,
+                             dens=alpha_dens_func,
+                             temp=ion_temp_func,
+                             rot=rotation_func,
+                             rho=rho_func)
+
+            self.species_data.helium = helium
+
+        # Impurity data
+        impurity_dens_data = kinetics_data['NIMP'][-1, :].data
+
+        if any(impurity_dens_data):
+            nspec += 1
+            impurity_dens_func = InterpolatedUnivariateSpline(psi_n, impurity_dens_data)
+
+            Z = int(kinetics_data['ZIA1'][-1, 0].data)
+            M = get_impurity_mass(Z)
+
+            impurity = Species(species_type='impurity',
+                               charge=Z,
+                               mass=M * hydrogen_mass,
+                               dens=impurity_dens_func,
+                               temp=ion_temp_func,
+                               rot=rotation_func,
+                               rho=rho_func)
+
+            self.species_data.impurity = impurity
+
+        self.nspec = nspec
+        self.species_names = [*self.species_data.keys()]
+
+
+def get_impurity_mass(Z=None):
+    """ Get impurity mass from charge
+        
+    """
+
+    Zlist = [2, 6, 8, 10, 18, 54, 74]
+    Mlist = [4, 12, 16, 20, 40, 132, 184]
+
+    M = Mlist[Zlist.index(Z)]
+
+    return M
+
+
+def get_impurity_charge(M=None):
+    """ Get impurity charge from mass
+
+    """
+
+    Zlist = [2, 6, 8, 10, 18, 54, 74]
+    Mlist = [4, 12, 16, 20, 40, 132, 184]
+
+    Z = Zlist[Mlist.index(M)]
---
+++ packages/pyrokinetics/pyrokinetics-0.0.1.tar.gz/pyrokinetics-0.0.1/pyrokinetics/species.py
@@ -0,0 +1,127 @@
+
+class Species:
+    """
+    Contains all species data as a function of psiN
+
+    Charge
+    Mass
+    Density
+    Temperature
+    Rotation
+    Omega
+
+    Also need r/a (rho) as a function of psi for a/Lt etc.
+    May need to add psi_toroidal
+    """
+
+    def __init__(
+            self,
+            species_type=None,
+            charge=None,
+            mass=None,
+            dens=None,
+            temp=None,
+            rot=None,
+            rho=None,
+            ang=None
+            ):
+
+        self.species_type = species_type
+        self.charge = charge
+        self.mass = mass
+        self.dens = dens
+        self.temp = temp
+        self.rotation = rot
+        self.omega = ang
+        self.rho = rho
+        self.grad_rho = self.rho.derivative()
+
+    def get_mass(
+            self,
+            ):
+
+        return self.mass
+
+    def get_charge(
+            self,
+            ):
+
+        return self.charge
+
+    def get_dens(
+            self,
+            psi_n=None
+            ):
+
+        return self.dens(psi_n)
+
+    def get_norm_dens_gradient(
+            self,
+            psi_n=None
+            ):
+        """
+        - 1/n dn/rho
+        """
+        
+        dens = self.get_dens(psi_n)
+        grad_n = self.dens.derivative()(psi_n)
+        grad_rho = self.grad_rho(psi_n)
+    
+        a_ln = -1/dens * grad_n/grad_rho
+
+        return a_ln
+    
+    def get_temp(
+            self,
+            psi_n=None
+            ):
+
+        return self.temp(psi_n)
+
+    def get_norm_temp_gradient(self,
+                               psi_n=None
+                               ):
+        """
+        - 1/T dT/drho
+        """
+
+        temp = self.get_temp(psi_n)
+        grad_t = self.temp.derivative()(psi_n)
+        grad_rho = self.grad_rho(psi_n)
+        
+        a_lt = -1/temp * grad_t/grad_rho
+
+        return a_lt
+
+    def get_velocity(self,
+                     psi_n=None
+                     ):
+
+        if self.rotation is None:
+            vel = 0.0
+        else:
+            vel = self.rotation(psi_n)
+            
+        return vel
+    
+    def get_norm_vel_gradient(self,
+                              psi_n=None
+                              ):
+        """
+        - 1/v dv/drho
+        """
+        vel = self.get_velocity(psi_n)
+
+        if self.rotation is None:
+            grad_v = 0
+        else:
+            grad_v = self.rotation.derivative()(psi_n)
+            
+        grad_rho = self.grad_rho(psi_n)
+
+        if vel != 0.0:
+            a_lv = -1/vel * grad_v / grad_rho
+        else:
+            a_lv = 0.0
+
+        return a_lv
