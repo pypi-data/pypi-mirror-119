9215f5dd0562a68bd0cfe5fd3976ef1d8734eaa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:18 +0000

    Add package mypy-boto3-servicecatalog-appregistry/1.18.41/mypy_boto3_servicecatalog_appregistry-1.18.41-py3-none-any.whl

---
+++ packages/mypy-boto3-servicecatalog-appregistry/mypy_boto3_servicecatalog_appregistry-1.18.41-py3-none-any.whl/mypy_boto3_servicecatalog_appregistry/literals.py
@@ -0,0 +1,41 @@
+"""
+Type annotations for servicecatalog-appregistry service literal definitions.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_servicecatalog_appregistry/literals.html)
+
+Usage::
+
+    ```python
+    from mypy_boto3_servicecatalog_appregistry.literals import ListApplicationsPaginatorName
+
+    data: ListApplicationsPaginatorName = "list_applications"
+    ```
+"""
+import sys
+
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+
+__all__ = (
+    "ListApplicationsPaginatorName",
+    "ListAssociatedAttributeGroupsPaginatorName",
+    "ListAssociatedResourcesPaginatorName",
+    "ListAttributeGroupsPaginatorName",
+    "ResourceGroupStateType",
+    "ResourceTypeType",
+    "SyncActionType",
+)
+
+
+ListApplicationsPaginatorName = Literal["list_applications"]
+ListAssociatedAttributeGroupsPaginatorName = Literal["list_associated_attribute_groups"]
+ListAssociatedResourcesPaginatorName = Literal["list_associated_resources"]
+ListAttributeGroupsPaginatorName = Literal["list_attribute_groups"]
+ResourceGroupStateType = Literal[
+    "CREATE_COMPLETE", "CREATE_FAILED", "CREATING", "UPDATE_COMPLETE", "UPDATE_FAILED", "UPDATING"
+]
+ResourceTypeType = Literal["CFN_STACK"]
+SyncActionType = Literal["NO_ACTION", "START_SYNC"]
