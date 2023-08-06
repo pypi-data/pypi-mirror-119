70067f3af1537ab57f0e81cd43db25e22d156442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:43 +0000

    Add package shadho/0.4.1/shadho-0.4.1-py3-none-any.whl

---
+++ packages/shadho/shadho-0.4.1-py3-none-any.whl/shadho/utils.py
@@ -0,0 +1,28 @@
+import json
+import re
+
+import numpy as np
+
+
+class ShadhoEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (np.ndarray, np.generic)):
+            return {
+                '__data': obj.tolist(),
+                '__dtype': str(obj.dtype),
+            }
+        else:
+            return super(ShadhoEncoder, self).default(obj)
+
+
+class ShadhoDecoder(json.JSONDecoder):
+    def __init__(self, *args, **kwargs):
+        super(ShadhoDecoder, self).__init__(
+            *args, object_hook=self.object_hook, **kwargs)
+
+    def object_hook(self, obj):
+        if isinstance(obj, dict) and sorted(obj.keys()) == ['__data', '__dtype']:
+            arr = np.array(obj['__data']).astype(obj['__dtype'])
+            return arr
+        else:
+            return obj
