ec91e1477c2739a64943cc6bfdd0f9ebf6af5b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:15 +0000

    Add package aio-databases/0.7.0/aio-databases-0.7.0.tar.gz

---
+++ packages/aio-databases/aio-databases-0.7.0.tar.gz/aio-databases-0.7.0/aio_databases/backends/_aiopg.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import typing as t
+
+import aiopg
+
+from . import ABCDatabaseBackend
+from .common import Connection as Connection_
+
+
+class Connection(Connection_):
+
+    async def _executemany(self, query: str, *params, **options) -> t.Any:
+        async with self.conn.cursor() as cursor:
+            for args_ in params:
+                await cursor.execute(query, args_, **options)
+
+
+class Backend(ABCDatabaseBackend):
+
+    name = 'aiopg'
+    db_type = 'postgresql'
+    connection_cls = Connection
+
+    pool: t.Optional[aiopg.Pool] = None
+
+    async def connect(self) -> None:
+        dsn = self.url._replace(scheme='postgresql').geturl()
+        self.pool = await aiopg.create_pool(dsn, **self.options)
+
+    async def disconnect(self) -> None:
+        pool = self.pool
+        assert pool is not None, "Database is not connected"
+        self.pool = None
+        pool.close()
+        await pool.wait_closed()
+
+    async def acquire(self) -> aiopg.Connection:
+        pool = self.pool
+        assert pool is not None, "Database is not connected"
+        return await pool.acquire()
+
+    async def release(self, conn: aiopg.Connection):
+        pool = self.pool
+        assert pool is not None, "Database is not connected"
+        await pool.release(conn)
