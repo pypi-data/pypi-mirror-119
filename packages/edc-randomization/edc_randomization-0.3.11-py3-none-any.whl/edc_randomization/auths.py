557b7c546e01339cc2b953296a3ee39e5c414c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:53 +0000

    Add package edc-randomization/0.3.11/edc_randomization-0.3.11-py3-none-any.whl

---
+++ packages/edc-randomization/edc_randomization-0.3.11-py3-none-any.whl/edc_randomization/auths.py
@@ -0,0 +1,17 @@
+from edc_auth.default_role_names import PHARMACIST_ROLE, SITE_PHARMACIST_ROLE
+from edc_auth.site_auths import site_auths
+
+from .auth_objects import (
+    export_rando,
+    get_rando_permissions_codenames,
+    make_randomizationlist_view_only,
+    update_rando_group_permissions,
+)
+from .constants import EXPORT_RANDO, RANDO
+
+site_auths.add_group(*export_rando, name=EXPORT_RANDO)
+site_auths.add_group(get_rando_permissions_codenames, name=RANDO)
+site_auths.update_role(RANDO, name=PHARMACIST_ROLE)
+site_auths.update_role(RANDO, name=SITE_PHARMACIST_ROLE)
+site_auths.add_post_update_func(update_rando_group_permissions)
+site_auths.add_post_update_func(make_randomizationlist_view_only)
