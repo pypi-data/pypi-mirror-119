c349536eae3e12d02021f0d5a39264efefd20521
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:31 +0000

    Add package smartpasslib/0.1.0/smartpasslib-0.1.0.tar.gz

---
+++ packages/smartpasslib/smartpasslib-0.1.0.tar.gz/smartpasslib-0.1.0/smartpasslib/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# --------------------------------------------------------
+# Licensed under the terms of the BSD 3-Clause License
+# (see LICENSE for details).
+# Copyright (c) 2021, A.A Suvorov
+# All rights reserved.
+# --------------------------------------------------------
+# Url: https://github.com/smartlegionlab/
+# --------------------------------------------------------
+"""A cross-platform package of modules for generating,
+secure storage and recovery of complex, cryptographic,
+smart passwords on the fly."""
+__version__ = '0.1.0'
