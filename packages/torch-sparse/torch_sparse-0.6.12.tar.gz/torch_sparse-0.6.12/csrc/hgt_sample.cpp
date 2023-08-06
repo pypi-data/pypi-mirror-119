3ac3b0b78b66a97c979a1440fa26f9f44e8348cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:11 +0000

    Add package torch-sparse/0.6.12/torch_sparse-0.6.12.tar.gz

---
+++ packages/torch-sparse/torch_sparse-0.6.12.tar.gz/torch_sparse-0.6.12/csrc/hgt_sample.cpp
@@ -0,0 +1,28 @@
+#include <Python.h>
+#include <torch/script.h>
+
+#include "cpu/hgt_sample_cpu.h"
+
+#ifdef _WIN32
+#ifdef WITH_CUDA
+PyMODINIT_FUNC PyInit__hgt_sample_cuda(void) { return NULL; }
+#else
+PyMODINIT_FUNC PyInit__hgt_sample_cpu(void) { return NULL; }
+#endif
+#endif
+
+// Returns 'output_node_dict', 'row_dict', 'col_dict', 'output_edge_dict'
+std::tuple<c10::Dict<node_t, torch::Tensor>, c10::Dict<rel_t, torch::Tensor>,
+           c10::Dict<rel_t, torch::Tensor>, c10::Dict<rel_t, torch::Tensor>>
+hgt_sample(const c10::Dict<std::string, torch::Tensor> &colptr_dict,
+           const c10::Dict<std::string, torch::Tensor> &row_dict,
+           const c10::Dict<std::string, torch::Tensor> &input_node_dict,
+           const c10::Dict<std::string, std::vector<int64_t>> &num_samples_dict,
+           const int64_t num_hops) {
+
+  return hgt_sample_cpu(colptr_dict, row_dict, input_node_dict,
+                        num_samples_dict, num_hops);
+}
+
+static auto registry =
+    torch::RegisterOperators().op("torch_sparse::hgt_sample", &hgt_sample);
