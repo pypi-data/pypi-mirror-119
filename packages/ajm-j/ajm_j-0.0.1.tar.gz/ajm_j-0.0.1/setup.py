0b8921bc2536c3112c7614b9cc9802ccf8f34e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:52 +0000

    Add package ajm-j/0.0.1/ajm_j-0.0.1.tar.gz

---
+++ packages/ajm-j/ajm_j-0.0.1.tar.gz/ajm_j-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name = 'ajm_j',
+    version = '0.0.1',
+    description = 'Say hello!',
+    py_modules = ['fun'],
+    package_dir = {'': 'src'},
+
+   classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+   long_description_content_type = "text/markdown",
+   url = "https://github.com/jayaunyauni/jaya.git",
+   author = "Jaya Unyauni",
+   author_email = "jayaunyauni9999@gmail.com"
+
+)
