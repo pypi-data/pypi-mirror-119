a948009d94913cb2032d79f33b7806de2410ff15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:55 +0000

    Add package glasswall/0.2.6/glasswall-0.2.6-py3-none-any.whl

---
+++ packages/glasswall/glasswall-0.2.6-py3-none-any.whl/glasswall/content_management/config_elements/pptConfig.py
@@ -0,0 +1,38 @@
+
+
+from glasswall.content_management import switches
+from glasswall.content_management.config_elements.config_element import ConfigElement
+
+
+class pptConfig(ConfigElement):
+    """ A pptConfig ConfigElement.
+
+    Args:
+        default (str): The default action: allow, disallow, or sanitise.
+
+    Key word arguments can be specified to change individual switch values:
+    pptConfig(default="allow", embedded_images="sanitise")
+    """
+
+    def __init__(self, default: str = "sanitise", **kwargs):
+        self.name = self.__class__.__name__
+        self.default = default
+        self.switches_module = switches.ppt
+        self.default_switches = [
+            self.switches_module.embedded_files,
+            self.switches_module.embedded_images,
+            self.switches_module.external_hyperlinks,
+            self.switches_module.internal_hyperlinks,
+            self.switches_module.javascript,
+            self.switches_module.macros,
+            self.switches_module.metadata,
+            self.switches_module.review_comments,
+        ]
+
+        super().__init__(
+            name=self.name,
+            default=self.default,
+            switches_module=self.switches_module,
+            default_switches=self.default_switches,
+            config=kwargs
+        )
---
+++ packages/glasswall/glasswall-0.2.6-py3-none-any.whl/glasswall/content_management/policies/editor.py
@@ -0,0 +1,26 @@
+
+
+import glasswall
+from glasswall.content_management.policies.policy import Policy
+
+
+class Editor(Policy):
+    """ A content management policy for Editor."""
+
+    def __init__(self, default: str = "sanitise", config: dict = {}):
+        self.default = default
+        self.default_config_elements = [
+            glasswall.content_management.config_elements.pdfConfig,
+            glasswall.content_management.config_elements.pptConfig,
+            glasswall.content_management.config_elements.sysConfig,
+            glasswall.content_management.config_elements.tiffConfig,
+            glasswall.content_management.config_elements.wordConfig,
+            glasswall.content_management.config_elements.xlsConfig,
+        ]
+        self.config = config or {}
+
+        super().__init__(
+            default=self.default,
+            default_config_elements=self.default_config_elements,
+            config=self.config,
+        )
