bf443e6363580f9d2e8f406de1bc89681baaf45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:44:08 +0000

    Add package pyrocko/2021.9.14/pyrocko-2021.9.14-cp39-cp39-win_amd64.whl

---
+++ packages/pyrocko/pyrocko-2021.9.14-cp39-cp39-win_amd64.whl/pyrocko/info.py
@@ -0,0 +1,6 @@
+# This module is automatically created from setup.py
+git_sha1 = '4323d4562983bfdaea0f9bfaf25d8c1917497bb0'
+local_modifications = False
+version = '2021.09.14'
+long_version = 'pyrocko-2021.09.14-4323d4562983bfdaea0f9bfaf25d8c1917497bb0-2021-09-14_12:34:08'  # noqa
+installed_date = '2021-09-14_12:34:08'
