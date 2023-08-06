c332820d30634158a1bb7f80036297533923598a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:17 +0000

    Add package lpa-input/1.0.2/lpa_input-1.0.2-py3-none-any.whl

---
+++ packages/lpa-input/lpa_input-1.0.2-py3-none-any.whl/lpa/input/__init__.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Tools for the generation and analysis of dislocation distributions.
+
+A dislocation is characterized by a position and a Burgers vector. A
+distribution is a set of dislocations in a material. The dislocations
+of a distribution are generated randomly according to a model. A sample
+is a set of distributions generated with the same model.
+
+A distribution can be exported as a map showing the location of its
+dislocations. It can also be exported in a standardized form for input
+to an X-ray diffraction simulation program. Finally it is possible to
+make a spatial analysis of a model by means of statistical functions
+on a sample of distributions.
+
+In this package, Burgers vectors are represented only by their sense
+(positive: +1, or negative: -1). Their direction is only determined in
+the header of the exported input data files. It is parallel to the
+dislocation line for the 'screw' type or perpendicular for the 'edge'
+type. The superposition principle simplifies the analysis by allowing
+to study only one direction at a time.
+
+To obtain relevant statistical results, the spatial analysis must be
+averaged over a large number of distributions. To reduce the
+computation time it is possible to parallelize the calculations. The
+MPI library can be used for this purpose. A sample of distributions is
+generated and analyzed on each core, then the results are averaged
+over the cores.
+"""
+
+__author__ = "Dunstan Becht"
+__version__ = "1.0.2"
+
+import os
+import sys
+import numpy as np
+from typing import Union, Optional, NewType, Any
+from beartype import beartype
+
+if sys.version_info[0]>=3 and sys.version_info[1]>=9:
+    from collections.abc import Callable
+    List = list
+    Tuple = tuple
+else:
+    from typing import Callable
+    from typing import List
+    from typing import Tuple
+
+# scalar and vectors
+Scalar = Union[int, float, np.intc]
+Vector = np.ndarray # shape: (n,)
+# sets
+ScalarList = np.ndarray # shape: (...,)
+ScalarListList = np.ndarray # shape: (..., ...)
+VectorList = np.ndarray # shape: (..., n)
+# model generation functions
+CorrectionFunction = Callable[
+    [Vector, ScalarList, ScalarList],
+    ScalarList
+]
+# edge correction functions
+GenerationFunction = Callable[
+    [str, Scalar, Scalar, dict],
+    Tuple[VectorList, ScalarList]
+]
+# generic analysis function output
+AnalysisOutput = Union[Tuple, Scalar, np.ndarray]
---
+++ packages/lpa-input/lpa_input-1.0.2-py3-none-any.whl/lpa/input/parallel.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Tools for parallelizing the spatial analysis of distributions.
+"""
+
+from mpi4py import MPI
+name = MPI.Get_processor_name()
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank() # number of the processor executing this script
+size = comm.Get_size() # number of processors
+root = 0 # master processor
+import warnings
+from . import *
+from . import sets
+from . import analyze
+
+@beartype
+def average_on_cores(
+    w: AnalysisOutput,
+    b: bool = False
+):
+    """
+    Return the average value of w ​​over the cores.
+
+    When b is True, all cores get the average value. When b is False,
+    the workers get None and the root gets the average value.
+
+    Input:
+        w: worker value
+        b: broadcast the result to all cores
+
+    Output:
+        m: averaged value of w over the cores
+
+    Input example:
+        On master core:
+            w = 1
+            b = False
+        On worker core:
+            w = 0
+            b = False
+
+    Output example:
+        On master core:
+            m = 0.5
+        On worker core:
+            m = None
+    """
+    if not isinstance(w, np.ndarray):
+        w = np.array(w)
+    if rank == root: # if the script is executed by the master
+        m = np.zeros_like(w) # prepare the buffer for data reception
+    else: # if the script is executed by a worker
+        m = None
+    comm.Reduce([w, MPI.DOUBLE], [m, MPI.DOUBLE], op=MPI.SUM, root=root)
+    if rank == root:
+        m = m/size # average the value
+    if b:
+        m = comm.bcast(m, root=root) # broadcast to the workers
+    return m
+
+@beartype
+def export(
+    o: Union[sets.Distribution, sets.Sample],
+    expdir: str = "",
+    expfmt: str = "pdf",
+    expstm: Optional[str] = None,
+    title: Optional[str] = None,
+) -> None:
+    """
+    Export a complete pooled analysis of the object o of each core.
+
+    Function similar to the export function of the analyze module.
+
+    Input:
+        o: distribution or sample to analyze on the core
+        expdir: export directory
+        expfmt: export format
+        expstm: export stem
+        title: figure title
+    """
+    if rank==root and not o.S is None:
+        msg = ("chosen random seed detected on "+o.name('stm')+" "
+            + "(The use of a seed is not recommended. In order for "
+            + "the distributions or samples not to be identical from "
+            + "one core to another, the seed must not be identical "
+            + "from one core to another.")
+        warnings.warn(msg, Warning)
+    i = average_on_cores(o.i, True) # averaged inter dislocation distance
+    r, iK = analyze.intervals(i, o.s) # intervals to display
+    f = ['KKKK', 'gggg', 'GaGs'] # functions to calculate
+    worker = analyze.calculate(f, o, r) # function results
+    master = [average_on_cores(worker[i]) for i in range(len(worker))]
+    if rank == root:
+        if isinstance(o, sets.Distribution):
+            c = str(size) # number of distributions analyzed
+        else:
+            c = str(len(o)*size) # number of distributions analyzed
+        # optional parameters
+        expdir = kwargs.pop('expdir', '') # export directory
+        expfmt = kwargs.pop('expfmt', 'pdf') # export format
+        expstm = kwargs.pop('expstm', c+"_"+o.name('dmgsS', c='stm')) # stem
+        title = kwargs.pop('title', c+" "+o.name('mgsd', c='ttl')) # title
+        if len(kwargs)>0:
+            raise ValueError("wrong keywords: "+str(kwargs))
+        # export
+        args = (expdir, expfmt, expstm, title)
+        analyze.plot_KKKK(r[:iK], master[f.index('KKKK')].T[:iK].T, *args)
+        analyze.plot_gggg(r, master[f.index('gggg')], *args)
+        analyze.plot_GaGs(r, master[f.index('GaGs')], *args)
