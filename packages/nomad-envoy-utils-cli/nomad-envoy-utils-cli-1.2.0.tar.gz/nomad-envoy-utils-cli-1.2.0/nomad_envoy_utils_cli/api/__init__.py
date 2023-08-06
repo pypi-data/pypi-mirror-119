8a524d7bcb301ebab3a1ce3d13e70a628c318bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package nomad-envoy-utils-cli/1.2.0/nomad-envoy-utils-cli-1.2.0.tar.gz

---
+++ packages/nomad-envoy-utils-cli/nomad-envoy-utils-cli-1.2.0.tar.gz/nomad-envoy-utils-cli-1.2.0/nomad_envoy_utils_cli/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from nomad_envoy_utils_cli.api.health_api import HealthApi
+from nomad_envoy_utils_cli.api.platform_api import PlatformApi
