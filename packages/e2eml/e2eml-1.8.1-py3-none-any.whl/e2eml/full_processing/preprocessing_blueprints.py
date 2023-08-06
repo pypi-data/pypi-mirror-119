460df09f2025e7741b17997fe6e30a289f8e7d86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:42 +0000

    Add package e2eml/1.8.1/e2eml-1.8.1-py3-none-any.whl

---
+++ packages/e2eml/e2eml-1.8.1-py3-none-any.whl/e2eml/full_processing/preprocessing_blueprints.py
@@ -0,0 +1,305 @@
+from e2eml.full_processing.postprocessing import FullPipeline
+from e2eml.full_processing.cpu_processing_nlp import NlpPreprocessing
+import logging
+
+
+class PreprocessingBluePrint(FullPipeline, NlpPreprocessing):
+    def pp_bp01_std_preprocessing(self, df=None, preprocessing_type='full'):
+        """
+        Our recommended blueprint for Tabnet testing.
+        Runs a preprocessing blueprint only. This is useful for building custom pipelines.
+        :param df: Accepts a dataframe to run ml preprocessing on it.
+        :param preprocessing_type: Select the type of preprocessing pipeline. "Minimum" executes the least possible steps,
+        "full" the whole standard preprocessing and "nlp" adds functionality especially for NLP tasks.
+        :return: Updates class attributes.
+        """
+        logging.info('Start blueprint.')
+        self.runtime_warnings(warn_about="future_architecture_change")
+        try:
+            if df.empty:
+                self.prediction_mode = False
+            else:
+                self.dataframe = df
+                self.prediction_mode = True
+        except AttributeError:
+            self.prediction_mode = False
+        self.train_test_split(how=self.train_split_type)
+        self.automatic_type_detection_casting()
+        self.fill_infinite_values()
+        self.fill_nulls(how='static') # can only be here when "static"
+        self.datetime_converter(datetime_handling='all')
+        self.pos_tagging_pca(pca_pos_tags=True)
+        if preprocessing_type == 'nlp':
+            self.append_text_sentiment_score()
+            self.tfidf_vectorizer_to_pca(pca_pos_tags=True)
+            self.tfidf_naive_bayes_proba(analyzer="char_wb", ngram_range=(1, 2))
+            self.tfidf_naive_bayes_proba(analyzer="word", ngram_range=(1, 1))
+        self.cardinality_remover(threshold=100)
+        self.delete_high_null_cols(threshold=0.5)
+        self.numeric_binarizer_pca()
+        self.onehot_pca()
+        self.category_encoding(algorithm='target')
+        self.fill_nulls(how='static') # can only be here when "static"
+        self.data_binning(nb_bins=10)
+        self.outlier_care(method='isolation', how='append')
+        self.remove_collinearity(threshold=0.8)
+        self.skewness_removal()
+        try:
+            self.clustering_as_a_feature(algorithm='dbscan', eps=0.3, n_jobs=-1, min_samples=10)
+        except ValueError:
+            print("Clustering as a feature skipped due to ValueError.")
+        for nb_cluster in [3, 5, 7, 9]:
+            try:
+                self.clustering_as_a_feature(algorithm='kmeans', nb_clusters=nb_cluster)
+            except ValueError:
+                print("Clustering as a feature skipped due to ValueError.")
+        for nb_cluster in [2, 4, 6, 8, 10]:
+            try:
+                self.clustering_as_a_feature(algorithm='gaussian', nb_clusters=nb_cluster)
+            except ValueError:
+                print("Clustering as a feature skipped due to ValueError.")
+        if self.low_memory_mode:
+            self.reduce_memory_footprint()
+        self.automated_feature_selection()
+        self.vowpal_bruteforce_feature_selection()
+        self.sort_columns_alphabetically()
+
+    def pp_bp02_std_preprocessing(self, df=None, preprocessing_type='full'):
+        """
+        This preprocessing blueprint contains alternative decision compare to pp_bp01.
+        Runs a preprocessing blueprint only. This is useful for building custom pipelines.
+        :param df: Accepts a dataframe to run ml preprocessing on it.
+        :param preprocessing_type: Select the type of preprocessing pipeline. "Minimum" executes the least possible steps,
+        "full" the whole standard preprocessing and "nlp" adds functionality especially for NLP tasks.
+        :return: Updates class attributes.
+        """
+        logging.info('Start blueprint.')
+        self.runtime_warnings(warn_about="future_architecture_change")
+        try:
+            if df.empty:
+                self.prediction_mode = False
+            else:
+                self.dataframe = df
+                self.prediction_mode = True
+        except AttributeError:
+            self.prediction_mode = False
+        self.train_test_split(how=self.train_split_type)
+        self.automatic_type_detection_casting()
+        self.fill_infinite_values()
+        self.fill_nulls(how='static') # can only be here when "static"
+        self.datetime_converter(datetime_handling='all')
+        self.pos_tagging_pca(pca_pos_tags=True)
+        if preprocessing_type == 'nlp':
+            self.pos_tagging_pca(pca_pos_tags=False)
+        self.rare_feature_processor(threshold=0.005, mask_as='miscellaneous', rarity_cols=self.rarity_cols)
+        self.cardinality_remover(threshold=100)
+        self.onehot_pca()
+        self.category_encoding(algorithm='GLMM')
+        self.delete_high_null_cols(threshold=0.4)
+        self.fill_nulls(how='iterative_imputation', fill_with=0)
+        self.data_binning(nb_bins=20)
+        #self.skewness_removal()
+        self.outlier_care(method='isolation', how='append')
+        self.remove_collinearity(threshold=0.85)
+        try:
+            self.clustering_as_a_feature(algorithm='dbscan', eps=0.3, n_jobs=-1, min_samples=10)
+        except ValueError:
+            print("Clustering as a feature skipped due to ValueError.")
+        for nb_cluster in range(2, 10):
+            try:
+                self.clustering_as_a_feature(algorithm='GLMM', nb_clusters=nb_cluster)
+            except ValueError:
+                print("Clustering as a feature skipped due to ValueError.")
+        if self.low_memory_mode:
+            self.reduce_memory_footprint()
+        self.automated_feature_selection()
+        self.vowpal_bruteforce_feature_selection()
+        self.sort_columns_alphabetically()
+
+    def pp_bp03_std_preprocessing(self, df=None, preprocessing_type='full'):
+        """
+        This blueprint adds skewness removal by log transformation, data scaling and SMOTE.
+        Runs a preprocessing blueprint only. This is useful for building custom pipelines.
+        :param df: Accepts a dataframe to run ml preprocessing on it.
+        :param preprocessing_type: Select the type of preprocessing pipeline. "Minimum" executes the least possible steps,
+        "full" the whole standard preprocessing and "nlp" adds functionality especially for NLP tasks.
+        :return: Updates class attributes.
+        """
+        logging.info('Start blueprint.')
+        self.runtime_warnings(warn_about="future_architecture_change")
+        try:
+            if df.empty:
+                self.prediction_mode = False
+            else:
+                self.dataframe = df
+                self.prediction_mode = True
+        except AttributeError:
+            self.prediction_mode = False
+        self.train_test_split(how=self.train_split_type)
+        self.automatic_type_detection_casting()
+        self.fill_infinite_values()
+        self.fill_nulls(how='static') # can only be here when "static"
+        self.datetime_converter(datetime_handling='all')
+        self.pos_tagging_pca(pca_pos_tags=True)
+        if preprocessing_type == 'nlp':
+            self.pos_tagging_pca(pca_pos_tags=False)
+            self.tfidf_vectorizer_to_pca(pca_pos_tags=True)
+            self.tfidf_naive_bayes_proba()
+        self.rare_feature_processor(threshold=0.005, mask_as='miscellaneous', rarity_cols=self.rarity_cols)
+        self.cardinality_remover(threshold=100)
+        self.onehot_pca()
+        self.category_encoding(algorithm='GLMM')
+        self.delete_high_null_cols(threshold=0.4)
+        self.fill_nulls(how='static', fill_with=-99)
+        self.data_binning(nb_bins=5)
+        self.skewness_removal()
+        self.outlier_care(method='isolation', how='append')
+        self.remove_collinearity(threshold=0.80)
+        try:
+            self.clustering_as_a_feature(algorithm='dbscan', eps=0.3, n_jobs=-1, min_samples=10)
+        except ValueError:
+            print("Clustering as a feature skipped due to ValueError.")
+        for nb_cluster in range(2, 10):
+            try:
+                self.clustering_as_a_feature(algorithm='GLMM', nb_clusters=nb_cluster)
+            except ValueError:
+                print("Clustering as a feature skipped due to ValueError.")
+        if self.low_memory_mode:
+            self.reduce_memory_footprint()
+        self.automated_feature_selection()
+        self.vowpal_bruteforce_feature_selection()
+        self.sort_columns_alphabetically()
+        self.data_scaling()
+        if self.class_problem == 'binary' or self.class_problem == 'multiclass':
+            self.smote_data()
+        else:
+            pass
+
+    def pp_bp04_std_preprocessing(self, df=None, preprocessing_type='full'):
+        """
+        Our recommended blueprint for model testing.
+        Runs a preprocessing blueprint only. This is useful for building custom pipelines.
+        :param df: Accepts a dataframe to run ml preprocessing on it.
+        :param preprocessing_type: Select the type of preprocessing pipeline. "Minimum" executes the least possible steps,
+        "full" the whole standard preprocessing and "nlp" adds functionality especially for NLP tasks.
+        :return: Updates class attributes.
+        """
+        logging.info('Start blueprint.')
+        self.runtime_warnings(warn_about="future_architecture_change")
+        try:
+            if df.empty:
+                self.prediction_mode = False
+            else:
+                self.dataframe = df
+                self.prediction_mode = True
+        except AttributeError:
+            self.prediction_mode = False
+        self.train_test_split(how=self.train_split_type)
+        self.automatic_type_detection_casting()
+        self.fill_infinite_values()
+        self.fill_nulls(how='static') # can only be here when "static"
+        self.datetime_converter(datetime_handling='all')
+        self.pos_tagging_pca(pca_pos_tags=True)
+        if preprocessing_type == 'nlp':
+            self.append_text_sentiment_score()
+            self.pos_tagging_pca(pca_pos_tags=False)
+            self.tfidf_vectorizer_to_pca(pca_pos_tags=True)
+            self.tfidf_naive_bayes_proba(analyzer="char_wb", ngram_range=(1, 2))
+            self.tfidf_naive_bayes_proba(analyzer="word", ngram_range=(1, 1))
+        self.rare_feature_processor(threshold=0.005, mask_as='miscellaneous', rarity_cols=self.rarity_cols)
+        self.cardinality_remover(threshold=100)
+        self.onehot_pca()
+        self.category_encoding(algorithm='target')
+        self.delete_high_null_cols(threshold=0.5)
+        self.fill_nulls(how='static')
+        self.data_binning(nb_bins=10)
+        #self.skewness_removal()
+        self.outlier_care(method='isolation', how='append')
+        self.remove_collinearity(threshold=0.8)
+        try:
+            self.clustering_as_a_feature(algorithm='dbscan', eps=0.3, n_jobs=-1, min_samples=10)
+        except ValueError:
+            print("Clustering as a feature skipped due to ValueError.")
+        for nb_cluster in range(2, 10):
+            try:
+                self.clustering_as_a_feature(algorithm='kmeans', nb_clusters=nb_cluster)
+            except ValueError:
+                print("Clustering as a feature skipped due to ValueError.")
+        if self.low_memory_mode:
+            self.reduce_memory_footprint()
+        self.automated_feature_selection()
+        self.vowpal_bruteforce_feature_selection()
+        self.sort_columns_alphabetically()
+
+
+    def pp_bp10_nlp_preprocessing(self, df):
+        logging.info('Start blueprint.')
+        self.runtime_warnings(warn_about="future_architecture_change")
+        try:
+            if df.empty:
+                self.prediction_mode = False
+            else:
+                self.dataframe = df
+                self.prediction_mode = True
+        except AttributeError:
+            self.prediction_mode = False
+        self.train_test_split(how=self.train_split_type)
+        self.rare_feature_processor(threshold=0.005, mask_as='miscellaneous', rarity_cols=self.rarity_cols)
+        self.sort_columns_alphabetically()
+        self.check_max_sentence_length()
+        self.import_transformer_model_tokenizer(transformer_chosen=self.transformer_chosen)
+
+    def pp_bp11_nlp_preprocessing(self, df):
+        logging.info('Start blueprint.')
+        self.runtime_warnings(warn_about="future_architecture_change")
+        try:
+            if df.empty:
+                self.prediction_mode = False
+            else:
+                self.dataframe = df
+                self.prediction_mode = True
+        except AttributeError:
+            self.prediction_mode = False
+        self.train_test_split(how=self.train_split_type)
+        self.rare_feature_processor(threshold=0.005, mask_as='miscellaneous', rarity_cols=self.rarity_cols)
+        self.regex_clean_text_data()
+        self.sort_columns_alphabetically()
+        self.check_max_sentence_length()
+        self.import_transformer_model_tokenizer(transformer_chosen=self.transformer_chosen)
+
+    def pp_bp12_nlp_preprocessing(self, df):
+        logging.info('Start blueprint.')
+        self.runtime_warnings(warn_about="future_architecture_change")
+        try:
+            if df.empty:
+                self.prediction_mode = False
+            else:
+                self.dataframe = df
+                self.prediction_mode = True
+        except AttributeError:
+            self.prediction_mode = False
+        self.train_test_split(how=self.train_split_type)
+        self.oversample_train_data()
+        self.rare_feature_processor(threshold=0.005, mask_as='miscellaneous', rarity_cols=self.rarity_cols)
+        self.sort_columns_alphabetically()
+        self.check_max_sentence_length()
+        self.import_transformer_model_tokenizer(transformer_chosen=self.transformer_chosen)
+
+    def pp_bp13_nlp_preprocessing(self, df):
+        logging.info('Start blueprint.')
+        self.runtime_warnings(warn_about="future_architecture_change")
+        try:
+            if df.empty:
+                self.prediction_mode = False
+            else:
+                self.dataframe = df
+                self.prediction_mode = True
+        except AttributeError:
+            self.prediction_mode = False
+        self.train_test_split(how=self.train_split_type)
+        self.replace_synonyms_to_df_copy(words_to_replace=3, mode='auto')
+        #self.oversample_train_data()
+        self.rare_feature_processor(threshold=0.005, mask_as='miscellaneous', rarity_cols=self.rarity_cols)
+        self.sort_columns_alphabetically()
+        self.check_max_sentence_length()
+        self.import_transformer_model_tokenizer(transformer_chosen=self.transformer_chosen)
---
+++ packages/e2eml/e2eml-1.8.1-py3-none-any.whl/e2eml/test/classification_blueprints_test.py
@@ -0,0 +1,207 @@
+from e2eml.classification import classification_blueprints as cb
+import pandas as pd
+from sklearn.metrics import matthews_corrcoef
+from sklearn.metrics import classification_report
+from sklearn.utils import shuffle
+import re
+
+
+def load_titanic_data():
+    """
+    Load & preprocess Titanic dataset. The feature engineering simulates the business knowledge part.
+    The code has been taken from:
+    https://towardsdatascience.com/predicting-the-survival-of-titanic-passengers-30870ccc7e8
+    :return: Several dataframes and series to be processed by blueprint.
+    """
+    data = pd.read_csv("titanic_train.csv")
+    print('Create additional features and modify existing ones.')
+    deck = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "U": 8}
+    data['Cabin'] = data['Cabin'].fillna("U0")
+    data['Deck'] = data['Cabin'].map(lambda x: re.compile("([a-zA-Z]+)").search(x).group())
+    data['Deck'] = data['Deck'].map(deck)
+    data['Deck'] = data['Deck'].fillna(0)
+    data['Deck'] = data['Deck'].astype(int)
+
+    data.loc[data['Fare'] <= 7.91, 'Fare'] = 0
+    data.loc[(data['Fare'] > 7.91) & (data['Fare'] <= 14.454), 'Fare'] = 1
+    data.loc[(data['Fare'] > 14.454) & (data['Fare'] <= 31), 'Fare'] = 2
+    data.loc[(data['Fare'] > 31) & (data['Fare'] <= 99), 'Fare'] = 3
+    data.loc[(data['Fare'] > 99) & (data['Fare'] <= 250), 'Fare'] = 4
+    data.loc[data['Fare'] > 250, 'Fare'] = 5
+
+    data['Age'].fillna(0, inplace=True)
+    data['Age'] = data['Age'].astype(int)
+    data.loc[data['Age'] <= 11, 'Age'] = 0
+    data.loc[(data['Age'] > 11) & (data['Age'] <= 18), 'Age'] = 1
+    data.loc[(data['Age'] > 18) & (data['Age'] <= 22), 'Age'] = 2
+    data.loc[(data['Age'] > 22) & (data['Age'] <= 27), 'Age'] = 3
+    data.loc[(data['Age'] > 27) & (data['Age'] <= 33), 'Age'] = 4
+    data.loc[(data['Age'] > 33) & (data['Age'] <= 40), 'Age'] = 5
+    data.loc[(data['Age'] > 40) & (data['Age'] <= 66), 'Age'] = 6
+    data.loc[data['Age'] > 66, 'Age'] = 6
+
+    titles = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+    # extract titles
+    data['Title'] = data.Name.str.extract(' ([A-Za-z]+)\.', expand=False)
+    # replace titles with a more common title or as Rare
+    data['Title'] = data['Title'].replace(['Lady', 'Countess','Capt', 'Col','Don', 'Dr',
+                                                 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+    data['Title'] = data['Title'].replace('Mlle', 'Miss')
+    data['Title'] = data['Title'].replace('Ms', 'Miss')
+    data['Title'] = data['Title'].replace('Mme', 'Mrs')
+    # convert titles into numbers
+    data['Title'] = data['Title'].map(titles)
+    # filling NaN with 0, to get safe
+    data['Title'] = data['Title'].fillna(0)
+
+    data['relatives'] = data['SibSp'] + data['Parch']
+    data.loc[data['relatives'] > 0, 'not_alone'] = 0
+    data.loc[data['relatives'] == 0, 'not_alone'] = 1
+    data['not_alone'] = data['not_alone'].astype(int)
+
+    data['Fare'] = data['Fare'].astype(int)
+    data['Age_Class'] = data['Age']*data['Pclass']
+    data['Fare_Per_Person'] = data['Fare']/(data['relatives']+1)
+    data['Fare_Per_Person'] = data['Fare_Per_Person'].astype(int)
+    print('Do dataframe splits.')
+    test_df = data.head(800).copy()
+    val_df = data.tail(91).copy()
+    val_df_target = val_df["Survived"].copy()
+    del val_df["Survived"]
+    test_target = "Survived"
+    test_categorical_cols = ["Pclass", "Name", 'Sex', 'PassengerId']
+    return test_df, test_target, val_df, val_df_target, test_categorical_cols
+
+
+def steel_fault_multiclass_data():
+    data = pd.read_csv("faults.csv")
+    data = shuffle(data)
+    #from sklearn.preprocessing import LabelEncoder
+    #le = LabelEncoder()
+    #X = data.drop('target', axis=1)
+    #Y = le.fit_transform(data['target'])
+
+    #X['target'] = Y
+    test_df = data.head(1500).copy()
+    val_df = data.tail(441).copy()
+    val_df_target = val_df["target"].copy()
+    del val_df["target"]
+    test_target = "target"
+    test_categorical_cols = None
+    return test_df, test_target, val_df, val_df_target, test_categorical_cols
+
+
+def nlp_multiclass_data():
+    data = pd.read_csv("Corona_NLP_train.csv", encoding='latin-1')
+    test_df = data.head(100).copy()
+    print(test_df[["OriginalTweet"]])
+    val_df = data.tail(499).copy()
+    val_df_target = val_df["Sentiment"].copy()
+    del val_df["Sentiment"]
+    test_target = "Sentiment"
+    test_categorical_cols = ["Location", "OriginalTweet"]
+    return test_df, test_target, val_df, val_df_target, test_categorical_cols
+
+
+def blueprint_binary_test_titanic(blueprint='logistic_regression', dataset='titanic'):
+    if dataset == 'titanic':
+        test_df, test_target, val_df, val_df_target, test_categorical_cols = load_titanic_data()
+        titanic_auto_ml = cb.ClassificationBluePrint(datasource=test_df,
+                                       target_variable=test_target,
+                                       categorical_columns=test_categorical_cols,
+                                                     preferred_training_mode='auto',
+                                                     tune_mode='accurate')
+    elif dataset == 'synthetic_multiclass':
+        test_df, test_target, val_df, val_df_target, test_categorical_cols = steel_fault_multiclass_data()
+        titanic_auto_ml = cb.ClassificationBluePrint(datasource=test_df,
+                                                     target_variable=test_target,
+                                                     categorical_columns=test_categorical_cols)
+    elif dataset == 'corona_tweet':
+        test_df, test_target, val_df, val_df_target, test_categorical_cols = nlp_multiclass_data()
+        titanic_auto_ml = cb.ClassificationBluePrint(datasource=test_df,
+                                                     target_variable=test_target,
+                                                     categorical_columns=test_categorical_cols,
+                                                     preferred_training_mode='auto',
+                                                     tune_mode='accurate',
+                                                     nlp_transformer_columns='OriginalTweet'
+                                                     )
+    if blueprint == 'lgbm':
+        titanic_auto_ml.ml_bp02_multiclass_full_processing_lgbm_prob(preprocessing_type='nlp', preprocess_bp='bp_01')
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_bp02_multiclass_full_processing_lgbm_prob(val_df, preprocessing_type='nlp', preprocess_bp='bp_01')
+        val_df_target = titanic_auto_ml.label_encoder_decoder(val_df_target, mode='transform')
+        val_y_hat = titanic_auto_ml.predicted_classes['lgbm']
+        # nlp+dynamic scale_pos: matth 0.60, no scale_pos: 0.54
+        # no nlp, scale_pos: 0.49, no scale_pos: 0.54
+    elif blueprint == 'xgboost':
+        titanic_auto_ml.ml_bp01_multiclass_full_processing_xgb_prob(preprocessing_type='nlp')
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_bp01_multiclass_full_processing_xgb_prob(val_df, preprocessing_type='nlp')
+        # label encode targets
+        val_df_target = titanic_auto_ml.label_encoder_decoder(val_df_target, mode='transform')
+        val_y_hat = titanic_auto_ml.predicted_classes['xgboost']
+    elif blueprint == 'sklearn_ensemble':
+        titanic_auto_ml.ml_bp03_multiclass_full_processing_sklearn_stacking_ensemble()
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_bp03_multiclass_full_processing_sklearn_stacking_ensemble(val_df)
+        val_y_hat = titanic_auto_ml.predicted_classes['sklearn_ensemble']
+    elif blueprint == 'logistic_regression':
+        titanic_auto_ml.ml_bp00_train_test_binary_full_processing_log_reg_prob()
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_bp00_train_test_binary_full_processing_log_reg_prob(val_df)
+        val_y_hat = titanic_auto_ml.predicted_classes['logistic_regression']
+    elif blueprint == 'ngboost':
+        titanic_auto_ml.ml_bp04_multiclass_full_processing_ngboost(preprocessing_type='nlp')
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_bp04_multiclass_full_processing_ngboost(val_df, preprocessing_type='nlp')
+        val_y_hat = titanic_auto_ml.predicted_classes['ngboost']
+    elif blueprint == 'vowpal_wabbit':
+        titanic_auto_ml.ml_bp05_multiclass_full_processing_vowpal_wabbit(preprocessing_type='full')
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_bp05_multiclass_full_processing_vowpal_wabbit(val_df, preprocessing_type='full')
+        # label encode targets
+        #val_df_target = titanic_auto_ml.label_encoder_decoder(val_df_target, mode='transform')
+        val_y_hat = titanic_auto_ml.predicted_classes['vowpal_wabbit']
+    elif blueprint == 'max_voting':
+        titanic_auto_ml.ml_special_multiclass_full_processing_multimodel_max_voting()
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_special_multiclass_full_processing_multimodel_max_voting(val_df)
+        val_y_hat = titanic_auto_ml.predicted_classes['blended_preds']
+    elif blueprint == 'autoselect':
+        titanic_auto_ml.ml_special_multiclass_auto_model_exploration(preprocessing_type='full')
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_special_multiclass_auto_model_exploration(val_df, preprocessing_type='full')
+        val_y_hat = titanic_auto_ml.predicted_classes[titanic_auto_ml.best_model]
+    elif blueprint == 'nlp_transformer':
+        titanic_auto_ml.ml_bp06_multiclass_full_processing_bert_transformer()
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_bp06_multiclass_full_processing_bert_transformer(val_df)
+        # label encode targets
+        val_df_target = titanic_auto_ml.label_encoder_decoder(val_df_target, mode='transform')
+        val_y_hat = titanic_auto_ml.predicted_classes['nlp_transformer']
+    elif blueprint == 'tabnet':
+        titanic_auto_ml.ml_bp07_multiclass_full_processing_tabnet()
+        print("Start prediction on holdout dataset")
+        titanic_auto_ml.ml_bp07_multiclass_full_processing_tabnet(val_df)
+        # label encode targets
+        val_df_target = titanic_auto_ml.label_encoder_decoder(val_df_target, mode='transform')
+        val_y_hat = titanic_auto_ml.predicted_classes['tabnet']
+    else:
+        pass
+
+    print(classification_report(val_df_target, val_y_hat))
+    try:
+        matthews = matthews_corrcoef(val_df_target, val_y_hat)
+    except Exception:
+        print("Matthew failed.")
+        matthews = 0
+    print(matthews)
+
+    if matthews > 0:
+        return print('The test ran successfully.')
+    else:
+        return print('The test failed. Please investigate.')
+
+
+if __name__ == "__main__":
+    blueprint_binary_test_titanic(blueprint='vowpal_wabbit', dataset='titanic') # corona_tweet
