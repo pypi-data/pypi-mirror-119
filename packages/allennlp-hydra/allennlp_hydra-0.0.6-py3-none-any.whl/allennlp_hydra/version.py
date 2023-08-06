43541d3d68c54e03f0aabbe07446c1fca31c9809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:08:53 +0000

    Add package allennlp-hydra/0.0.6/allennlp_hydra-0.0.6-py3-none-any.whl

---
+++ packages/allennlp-hydra/allennlp_hydra-0.0.6-py3-none-any.whl/allennlp_hydra/version.py
@@ -0,0 +1,11 @@
+"""
+Taken from
+https://github.com/allenai/allennlp-models/blob/main/allennlp_models/version.py
+"""
+_MAJOR = "0"
+_MINOR = "0"
+_PATCH = "6"
+
+VERSION_SHORT = "{0}.{1}".format(_MAJOR, _MINOR)
+VERSION = "{0}.{1}.{2}".format(_MAJOR, _MINOR, _PATCH)
+__version__ = VERSION
