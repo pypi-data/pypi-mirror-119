bd4447c43ac621fc024cc1c185eb3b63da79a26e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:39 +0000

    Add package robotframework-OpenShiftCLI/1.0.0/robotframework_OpenShiftCLI-1.0-py3-none-any.whl

---
+++ packages/robotframework-OpenShiftCLI/robotframework_OpenShiftCLI-1.0-py3-none-any.whl/OpenShiftCLI/keywords/__init__.py
@@ -0,0 +1,15 @@
+
+from .clusterrolebindings import ClusterrolebindingKeywords
+from .clusterroles import ClusterroleKeywords
+from .configmaps import ConfigmapKeywords
+from .crds import CRDKeywords
+from .groups import GroupKeywords
+from .kfdefs import KFDEFKeywords
+from .lists import ListKeywords
+from .pods import PodKeywords
+from .projects import ProjectKeywords
+from .roles import RoleKeywords
+from .rolebindings import RolebindingKeywords
+from .secrets import SecretKeywords
+from .services import ServiceKeywords
+from .users import UserKeywords
