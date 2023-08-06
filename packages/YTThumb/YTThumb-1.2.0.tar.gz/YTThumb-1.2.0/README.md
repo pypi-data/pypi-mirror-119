321b2c26cffd966ffbf6b182e55ceefd5bd683be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:59 +0000

    Add package YTThumb/1.2.0/YTThumb-1.2.0.tar.gz

---
+++ packages/YTThumb/YTThumb-1.2.0.tar.gz/YTThumb-1.2.0/README.md
@@ -0,0 +1,36 @@
+# Youtube-Video-Thumbnail
+
+A youtube video link or id to video thumbnail python package.
+
+---
+
+```
+Made with Python3
+(C) @FayasNoushad
+Copyright permission under MIT License
+License -> https://github.com/FayasNoushad/Youtube-Video-Thumbnail/blob/main/LICENSE
+```
+
+---
+
+## Installation
+
+```
+pip3 install ytthumb
+```
+
+---
+
+## Usage
+
+```python
+import ytthumb
+
+ytthumb.thumbnail("https://youtu.be/rokGy0huYEA")
+```
+
+---
+
+## Credits
+
+- [Fayas Noushad](https://github.com/FayasNoushad)
---
+++ packages/YTThumb/YTThumb-1.2.0.tar.gz/YTThumb-1.2.0/setup.py
@@ -0,0 +1,32 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/Youtube-Video-Thumbnail/blob/main/LICENSE
+
+import pathlib
+from setuptools import setup, find_packages
+
+
+file = pathlib.Path(__file__).parent
+
+README = (file / "README.md").read_text()
+
+setup(
+    name="YTThumb",
+    version="1.2.0",
+    author="FayasNoushad",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    description="A youtube video link or id to video thumbnail python package.",
+    license="MIT",
+    url="https://github.com/FayasNoushad/Youtube-Video-Thumbnail",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6"
+)
